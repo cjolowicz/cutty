8b9750a0158b072e740b8d940ee5f4864cdeeac0
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-12 20:53:28 +0200

    Add tags module

---
+++ src/cutty/tags.py
@@ -0,0 +1,39 @@
+"""Determining the versions tagged in a Git repository."""
+from __future__ import annotations
+
+import contextlib
+from dataclasses import dataclass
+from typing import Iterator
+
+from packaging.version import InvalidVersion
+from packaging.version import Version
+
+from . import git
+
+
+@dataclass
+class VersionTag:
+    """Git tag for a version according to PEP 440."""
+
+    name: str
+    version: Version
+
+    @classmethod
+    def create(cls, name: str) -> VersionTag:
+        """Parse the tag name as a PEP 440 version."""
+        version = Version(name[1:] if name.startswith("v") else name)
+        return cls(name, version)
+
+
+def load(repository: git.Repository) -> Iterator[VersionTag]:
+    """Load versions tagged in the repository."""
+    for tag in repository.tags():
+        with contextlib.suppress(InvalidVersion):
+            yield VersionTag.create(tag)
+
+
+def find_latest(repository: git.Repository) -> str:
+    """Return the Git tag for the latest version."""
+    tags = load(repository)
+    latest = max(tags, key=lambda tag: tag.version)
+    return latest.name
