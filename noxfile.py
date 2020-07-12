c1422d5d219d1e5a67823b6d3ea9036288e288ae
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-12 21:05:42 +0200

    Drop Python 3.6

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,7 @@ from nox.sessions import Session
 
 
 package = "cutty"
-python_versions = ["3.8", "3.7", "3.6"]
+python_versions = ["3.8", "3.7"]
 nox.options.sessions = "pre-commit", "safety", "mypy", "tests", "typeguard"
 
 
