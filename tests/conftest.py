2bea0dcc5b357336f7b2c15240ee613135a47c2d
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-17 15:53:12 +0200

    Move user_cache_dir fixture to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,9 @@
 from pathlib import Path
 
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 
+from cutty import cache
 from cutty import git
 
 
@@ -12,3 +14,12 @@ def repository(tmp_path: Path) -> git.Repository:
     path = tmp_path / "repository"
     path.mkdir()
     return git.Repository.init(path)
+
+
+@pytest.fixture
+def user_cache_dir(monkeypatch: MonkeyPatch, tmp_path: Path) -> Path:
+    """Replace the application cache directory by a temporary directory."""
+    path = tmp_path / ".cache" / cache.appname
+    path.mkdir(parents=True, exist_ok=True)
+    monkeypatch.setattr("appdirs.user_cache_dir", lambda *args, **kwargs: path)
+    return path
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,21 +1,9 @@
 """Tests for the cache module."""
 from pathlib import Path
 
-import pytest
-from _pytest.monkeypatch import MonkeyPatch
-
 from .utils import commit
 from cutty import cache
 from cutty import git
-
-
-@pytest.fixture
-def user_cache_dir(monkeypatch: MonkeyPatch, tmp_path: Path) -> Path:
-    """Replace the application cache directory by a temporary directory."""
-    path = tmp_path / ".cache" / cache.appname
-    path.mkdir(parents=True, exist_ok=True)
-    monkeypatch.setattr("appdirs.user_cache_dir", lambda *args, **kwargs: path)
-    return path
 
 
 def test_repository_clones(user_cache_dir: Path, repository: git.Repository) -> None:
