6e2c3b965c679d0093af9c2932ae77d3b64018e9
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-17 16:48:57 +0200

    Add extra_context command-line argument

--- src/cutty/create/console.py
+++ src/cutty/create/console.py
@@ -1,11 +1,15 @@
 """Command-line interface."""
+from typing import Tuple
+
 import click
+from cookiecutter.cli import validate_extra_context
 
 from .core import create
 
 
 @click.command()
 @click.argument("template")
-def create(template: str) -> None:
+@click.argument("extra_context", nargs=-1, callback=validate_extra_context)
+def create(template: str, extra_context: Tuple[str]) -> None:
     """Create a project from a Cookiecutter template."""
-    create(template)
+    create(template, extra_context)
--- src/cutty/create/core.py
+++ src/cutty/create/core.py
@@ -1,4 +1,6 @@
 """Create a project."""
+from typing import Tuple
+
 from cookiecutter.config import get_user_config
 from cookiecutter.generate import generate_context
 from cookiecutter.generate import generate_files
@@ -9,7 +11,7 @@ from .. import cache
 from .. import tags
 
 
-def create(template: str) -> None:
+def create(template: str, extra_context: Tuple[str]) -> None:
     """Create a project from a Cookiecutter template."""
     config = get_user_config()
     template = expand_abbreviations(
@@ -20,7 +22,9 @@ def create(template: str) -> None:
     with cache.worktree(template, tag) as worktree:
         context_file = worktree.path / "cookiecutter.json"
         context = generate_context(
-            context_file=context_file, default_context=config["default_context"]
+            context_file=context_file,
+            default_context=config["default_context"],
+            extra_context=extra_context,
         )
         context["cookiecutter"] = prompt_for_config(context)
         context["cookiecutter"]["_template"] = template
