898d862bd5faac9087c3a02726895f22b91ddb3c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-18 18:37:44 +0200

    Prevent tests from writing to $HOME

--- tests/create/test_main.py
+++ tests/create/test_main.py
@@ -50,12 +50,32 @@ def template(repository: git.Repository) -> git.Repository:
     return repository
 
 
+@pytest.fixture
+def user_config_file(tmp_path: Path) -> Path:
+    """Configure cookiecutter to write to temporary directories."""
+    path = tmp_path / ".cookiecutterrc"
+    cookiecutters_dir = tmp_path / "cookiecutters_dir"
+    replay_dir = tmp_path / "replay_dir"
+    config = f"""\
+    cookiecutters_dir: {cookiecutters_dir}
+    replay_dir: {replay_dir}
+    """
+    path.write_text(dedent(config))
+    return path
+
+
 def test_create(
-    runner: CliRunner, user_cache_dir: Path, template: git.Repository
+    runner: CliRunner,
+    user_cache_dir: Path,
+    user_config_file: Path,
+    template: git.Repository,
 ) -> None:
     """It generates a project from the template."""
     result = runner.invoke(
-        create, [str(template.path)], input="example", catch_exceptions=False
+        create,
+        [str(template.path), f"--config-file={user_config_file}"],
+        input="example",
+        catch_exceptions=False,
     )
     assert result.exit_code == 0
 
@@ -67,7 +87,10 @@ def _replace(path: Path, old: str, new: str) -> None:
 
 
 def test_undefined_variable(
-    runner: CliRunner, user_cache_dir: Path, template: git.Repository
+    runner: CliRunner,
+    user_cache_dir: Path,
+    user_config_file: Path,
+    template: git.Repository,
 ) -> None:
     """It displays an informative error message if a variable is undefined."""
     _replace(template.path / "cookiecutter.json", "project", "XXproject")
@@ -77,27 +100,39 @@ def test_undefined_variable(
     template.git("tag", "v1.0.1")
 
     result = runner.invoke(
-        create, [str(template.path)], input="example", catch_exceptions=False
+        create,
+        [str(template.path), f"--config-file={user_config_file}"],
+        input="example",
+        catch_exceptions=False,
     )
 
     assert "has no attribute 'project'" in result.output
 
 
 def test_output_directory_exists(
-    runner: CliRunner, user_cache_dir: Path, template: git.Repository
+    runner: CliRunner,
+    user_cache_dir: Path,
+    user_config_file: Path,
+    template: git.Repository,
 ) -> None:
     """It displays an informative error message if the output directory exists."""
     Path("example").mkdir()
 
     result = runner.invoke(
-        create, [str(template.path)], input="example", catch_exceptions=False
+        create,
+        [str(template.path), f"--config-file={user_config_file}"],
+        input="example",
+        catch_exceptions=False,
     )
 
     assert "already exists" in result.output
 
 
 def test_checkout(
-    runner: CliRunner, user_cache_dir: Path, template: git.Repository
+    runner: CliRunner,
+    user_cache_dir: Path,
+    user_config_file: Path,
+    template: git.Repository,
 ) -> None:
     """It checks out the specified revision."""
     _replace(
@@ -111,7 +146,7 @@ def test_checkout(
 
     runner.invoke(
         create,
-        [str(template.path), "--checkout=master"],
+        [str(template.path), f"--config-file={user_config_file}", "--checkout=master"],
         input="example",
         catch_exceptions=False,
     )
