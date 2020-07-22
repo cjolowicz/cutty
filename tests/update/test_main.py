906db6c1ed34bc3bba7fdda2517f9b3b5b4e34bd
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-22 22:00:32 +0200

    Add test for --interactive

--- tests/update/test_main.py
+++ tests/update/test_main.py
@@ -43,3 +43,35 @@ def test_update(
 
     result = runner.invoke(update, [f"--config-file={user_config_file}"])
     assert result.exit_code == 0
+
+
+def test_interactive(
+    runner: CliRunner,
+    user_cache_dir: Path,
+    user_config_file: Path,
+    template: git.Repository,
+) -> None:
+    """It generates a project from the template."""
+    runner.invoke(
+        create,
+        [str(template.path), f"--config-file={user_config_file}"],
+        input="example",
+        catch_exceptions=False,
+    )
+
+    instance = git.Repository.init(Path("example"))
+    instance.git("add", "--all")
+    instance.git("commit", "--message=Initial")
+    instance.git("branch", "template")
+
+    os.chdir(instance.path)
+
+    (template.path / "{{cookiecutter.project}}" / "LICENSE").touch()
+    template.git("add", ".")
+    template.git("commit", "--message=Add LICENSE")
+    template.git("tag", "v1.1.0")
+
+    result = runner.invoke(
+        update, [f"--config-file={user_config_file}", "--interactive"], input="example",
+    )
+    assert result.exit_code == 0
