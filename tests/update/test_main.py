cjolowicz/cookiecutter-tools

Add test for --help

tests/update/test_main.py
@@ -1,0 +1,10 @@
+"""Test cases for the __main__ module."""
+from click.testing import CliRunner
+
+from cookiecutter_tools.update import __main__
+
+
+def test_help_succeeds(runner: CliRunner) -> None:
+    """It exits with a status code of zero."""
+    result = runner.invoke(__main__.main, ["--help"])
+    assert result.exit_code == 0

cjolowicz/cutty

tests/update/test_main.py
@@ -1,0 +1,10 @@
+"""Test cases for the console module."""
+from click.testing import CliRunner
+
+from cutty.update import console
+
+
+def test_help_succeeds(runner: CliRunner) -> None:
+    """It exits with a status code of zero."""
+    result = runner.invoke(console.update, ["--help"])
+    assert result.exit_code == 0