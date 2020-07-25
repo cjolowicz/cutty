cjolowicz/cookiecutter-tools

Add repository fixture to conftest.py

tests/conftest.py
@@ -1,0 +1,14 @@
+"""Test fixtures."""
+from pathlib import Path
+
+import pytest
+
+from cookiecutter_tools import git
+
+
+@pytest.fixture
+def repository(tmp_path: Path) -> git.Repository:
+    """Initialize repository in a temporary directory."""
+    path = tmp_path / "repository"
+    path.mkdir()
+    return git.Repository.init(path)

cjolowicz/cutty

tests/conftest.py
@@ -1,0 +1,14 @@
+"""Test fixtures."""
+from pathlib import Path
+
+import pytest
+
+from cutty import git
+
+
+@pytest.fixture
+def repository(tmp_path: Path) -> git.Repository:
+    """Initialize repository in a temporary directory."""
+    path = tmp_path / "repository"
+    path.mkdir()
+    return git.Repository.init(path)
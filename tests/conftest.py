756c83c94ec0adb474837a2719ab294f3af73d3f
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-16 10:31:04 +0200

    Add repository fixture to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+"""Test fixtures."""
+from pathlib import Path
+
+import pytest
+
+from cutty import git
+
+
+@pytest.fixture
+def repository(tmp_path: Path) -> git.Repository:
+    """Initialize repository in a temporary directory."""
+    path = tmp_path / "repository"
+    path.mkdir()
+    return git.Repository.init(path)
