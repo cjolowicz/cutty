cjolowicz/cookiecutter-tools

Add test utility to create commits

tests/utils.py
@@ -1,0 +1,8 @@
+"""Test utilities."""
+from cookiecutter_tools import git
+
+
+def commit(repository: git.Repository) -> str:
+    """Create an empty commit and return the SHA1 hash."""
+    repository.git("commit", "--allow-empty", "--allow-empty-message", "--message=")
+    return repository.rev_parse("HEAD")

cjolowicz/cutty

tests/utils.py
@@ -1,0 +1,8 @@
+"""Test utilities."""
+from cutty import git
+
+
+def commit(repository: git.Repository) -> str:
+    """Create an empty commit and return the SHA1 hash."""
+    repository.git("commit", "--allow-empty", "--allow-empty-message", "--message=")
+    return repository.rev_parse("HEAD")