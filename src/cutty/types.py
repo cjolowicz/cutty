30219edec2da5453fc368a58a2e486c9c669fca5
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-26 14:08:35 +0200

    Add types.StrPath

--- src/cutty/types.py
+++ src/cutty/types.py
@@ -1,13 +1,18 @@
 """Types."""
+import os
 import subprocess  # noqa: S404
 from typing import Any
 from typing import Mapping
 from typing import TYPE_CHECKING
+from typing import Union
 
 
-if TYPE_CHECKING:
-    CompletedProcess = subprocess.CompletedProcess[str]  # pragma: no cover
+if TYPE_CHECKING:  # pragma: no cover
+    CompletedProcess = subprocess.CompletedProcess[str]
+    PathLike = os.PathLike[str]
 else:
     CompletedProcess = subprocess.CompletedProcess
+    PathLike = os.PathLike
 
 StrMapping = Mapping[str, Any]
+StrPath = Union[str, PathLike]
