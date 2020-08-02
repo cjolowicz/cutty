907d26c8da22aa2b1523e6f20f77dcf63754fbb7
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-02 23:27:32 +0200

    Remove unused as_optional_str utility

--- src/cutty/utils.py
+++ src/cutty/utils.py
@@ -1,14 +1,6 @@
 """General-purpose utilities."""
 from pathlib import Path
 from typing import Optional
-from typing import TypeVar
-
-T = TypeVar("T")
-
-
-def as_optional_str(argument: Optional[T]) -> Optional[str]:
-    """Convert the argument to a string if it is not None."""
-    return str(argument) if argument is not None else None
 
 
 def as_optional_path(argument: Optional[str]) -> Optional[Path]:
