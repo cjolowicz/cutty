6fb7ce09297bc83ccdd2bd8ab41eed470ca3a073
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-03 16:42:08 +0200

    Add locations.config

--- src/cutty/locations.py
+++ src/cutty/locations.py
@@ -6,3 +6,4 @@ import appdirs
 
 name = "cutty"
 cache = Path(appdirs.user_cache_dir(appname=name, appauthor=name))
+config = Path(appdirs.user_config_dir(appname=name, appauthor=name)) / f"{name}.yml"
