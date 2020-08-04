d2a7b8cc0d3d02e245007663110baa47af2edb19
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-04 22:20:06 +0200

    Copy render_variable

--- src/cutty/prompt.py
+++ src/cutty/prompt.py
@@ -2,15 +2,52 @@
 from typing import cast
 from typing import List
 
+import six
 from cookiecutter.environment import StrictEnvironment
 from cookiecutter.exceptions import UndefinedVariableInTemplate
 from cookiecutter.prompt import read_user_choice
 from cookiecutter.prompt import read_user_dict
 from cookiecutter.prompt import read_user_variable
-from cookiecutter.prompt import render_variable
 from jinja2.exceptions import UndefinedError
 
 from .types import StrMapping
+
+
+def render_variable(env, raw, cookiecutter_dict):
+    """Render the next variable to be displayed in the user prompt.
+
+    Inside the prompting taken from the cookiecutter.json file, this renders
+    the next variable. For example, if a project_name is "Peanut Butter
+    Cookie", the repo_name could be be rendered with:
+
+        `{{ cookiecutter.project_name.replace(" ", "_") }}`.
+
+    This is then presented to the user as the default.
+
+    :param Environment env: A Jinja2 Environment object.
+    :param raw: The next value to be prompted for by the user.
+    :param dict cookiecutter_dict: The current context as it's gradually
+        being populated with variables.
+    :return: The rendered value for the default variable.
+    """
+    if raw is None:
+        return None
+    elif isinstance(raw, dict):
+        return {
+            render_variable(env, k, cookiecutter_dict): render_variable(
+                env, v, cookiecutter_dict
+            )
+            for k, v in raw.items()
+        }
+    elif isinstance(raw, list):
+        return [render_variable(env, v, cookiecutter_dict) for v in raw]
+    elif not isinstance(raw, six.string_types):
+        raw = str(raw)
+
+    template = env.from_string(raw)
+
+    rendered_template = template.render(cookiecutter=cookiecutter_dict)
+    return rendered_template
 
 
 def prompt_choice_for_config(
