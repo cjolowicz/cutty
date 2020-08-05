7174cb8d6e8a38dbd0ff7f0626e248701683e703
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-05 19:41:13 +0200

    Add environment module with StrictEnvironment

---
+++ src/cutty/environment.py
@@ -0,0 +1,36 @@
+"""Jinja2 environment and extensions loading."""
+from typing import Any
+from typing import Optional
+
+import jinja2
+from cookiecutter.exceptions import UnknownExtension
+
+from .types import StrMapping
+
+
+DEFAULT_EXTENSIONS = [
+    "cookiecutter.extensions.JsonifyExtension",
+    "cookiecutter.extensions.RandomStringExtension",
+    "cookiecutter.extensions.SlugifyExtension",
+    "jinja2_time.TimeExtension",
+]
+
+
+class StrictEnvironment(jinja2.Environment):
+    """Strict Jinja2 environment."""
+
+    def __init__(self, context: Optional[StrMapping] = None, **kwargs: Any) -> None:
+        """Initialize."""
+        if context is None:
+            context = {}
+
+        extensions = context.get("cookiecutter", {}).get("_extensions", [])
+        extensions = [str(extension) for extension in extensions]
+        extensions = DEFAULT_EXTENSIONS + extensions
+
+        try:
+            super().__init__(
+                undefined=jinja2.StrictUndefined, extensions=extensions, **kwargs
+            )
+        except ImportError as error:
+            raise UnknownExtension(f"Unable to load extension: {error}")
