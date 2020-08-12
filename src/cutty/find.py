77ffdaa8c824dce17cd59cb67d60c39493138853
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-12 17:50:07 +0200

    Add type annotations

--- src/cutty/find.py
+++ src/cutty/find.py
@@ -7,7 +7,7 @@ from cookiecutter.exceptions import NonTemplatedInputDirException
 logger = logging.getLogger(__name__)
 
 
-def find_template(repo_dir):
+def find_template(repo_dir: str) -> str:
     """Determine which child directory of `repo_dir` is the project template.
 
     :param repo_dir: Local directory of newly cloned repo.
