4b00d83dc537fa03d720302ea4864c251d0f8469
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-12 19:17:43 +0200

    Change parameter type of `repo_dir` to Path

--- src/cutty/generate.py
+++ src/cutty/generate.py
@@ -51,14 +51,14 @@ def create_directory(directory: Path, overwrite_if_exists: bool = False) -> bool
 
 
 def _run_hook_from_repo_dir(
-    repo_dir: str,
+    repo_dir: Path,
     hook_name: str,
     project_dir: str,
     context: StrMapping,
     delete_project_on_failure: bool,
 ) -> None:
     """Run hook from repo directory, clean project directory if hook fails."""
-    with work_in(repo_dir):
+    with work_in(str(repo_dir)):
         try:
             run_hook(hook_name, project_dir, context)
         except FailedHookException:
@@ -100,7 +100,7 @@ def generate_files(  # noqa: C901
     delete_project_on_failure = output_directory_created
 
     _run_hook_from_repo_dir(
-        str(repo_dir),
+        repo_dir,
         "pre_gen_project",
         str(project_dir),
         context,
@@ -170,7 +170,7 @@ def generate_files(  # noqa: C901
                     raise UndefinedVariableInTemplate(msg, err, context)
 
     _run_hook_from_repo_dir(
-        str(repo_dir),
+        repo_dir,
         "post_gen_project",
         str(project_dir),
         context,
