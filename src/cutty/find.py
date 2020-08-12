dd315f0c749b0f3011ea5d32fc95b1360fa128a1
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-12 17:51:02 +0200

    Remove logging

--- src/cutty/find.py
+++ src/cutty/find.py
@@ -1,16 +1,11 @@
 """Functions for finding Cookiecutter templates and other components."""
-import logging
 import os
 
 from cookiecutter.exceptions import NonTemplatedInputDirException
 
-logger = logging.getLogger(__name__)
-
 
 def find_template(repo_dir: str) -> str:
     """Determine which child directory of `repo_dir` is the project template."""
-    logger.debug("Searching %s for the project template.", repo_dir)
-
     repo_dir_contents = os.listdir(repo_dir)
 
     project_template = None
@@ -21,7 +16,6 @@ def find_template(repo_dir: str) -> str:
 
     if project_template:
         project_template = os.path.join(repo_dir, project_template)
-        logger.debug("The project template appears to be %s", project_template)
         return project_template
     else:
         raise NonTemplatedInputDirException
