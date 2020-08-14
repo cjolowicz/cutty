e50061e72816d2a09e15901d829edead3e7a2283
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-14 22:47:53 +0200

    Eliminate get_file_name

--- src/cutty/replay.py
+++ src/cutty/replay.py
@@ -5,12 +5,6 @@ from pathlib import Path
 from .types import StrMapping
 
 
-def get_file_name(replay_dir: Path, template_name: str) -> Path:
-    """Get the name of file."""
-    file_name = "{}.json".format(template_name)
-    return replay_dir / file_name
-
-
 def dump(replay_dir: Path, template_name: str, context: StrMapping) -> None:
     """Write json data to file."""
     replay_dir.mkdir(parents=True, exist_ok=True)
@@ -18,7 +12,7 @@ def dump(replay_dir: Path, template_name: str, context: StrMapping) -> None:
     if "cookiecutter" not in context:
         raise ValueError("Context is required to contain a cookiecutter key")
 
-    replay_file = get_file_name(replay_dir, template_name)
+    replay_file = replay_dir / f"{template_name}.json"
 
     with replay_file.open("w") as outfile:
         json.dump(context, outfile, indent=2)
@@ -26,7 +20,7 @@ def dump(replay_dir: Path, template_name: str, context: StrMapping) -> None:
 
 def load(replay_dir: Path, template_name: str) -> StrMapping:
     """Read json data from file."""
-    replay_file = get_file_name(replay_dir, template_name)
+    replay_file = replay_dir / f"{template_name}.json"
 
     with replay_file.open("r") as infile:
         context = json.load(infile)
