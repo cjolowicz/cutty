5d75be671ac766d415b0efedbfd85d94598a0699
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-14 22:51:18 +0200

    Clean up local variable names

--- src/cutty/replay.py
+++ src/cutty/replay.py
@@ -7,17 +7,17 @@ from .types import StrMapping
 
 def dump(replay_dir: Path, template_name: str, context: StrMapping) -> None:
     """Write json data to file."""
-    replay_file = replay_dir / f"{template_name}.json"
+    path = replay_dir / f"{template_name}.json"
 
-    with replay_file.open("w") as outfile:
-        json.dump(context, outfile, indent=2)
+    with path.open("w") as io:
+        json.dump(context, io, indent=2)
 
 
 def load(replay_dir: Path, template_name: str) -> StrMapping:
     """Read json data from file."""
-    replay_file = replay_dir / f"{template_name}.json"
+    path = replay_dir / f"{template_name}.json"
 
-    with replay_file.open("r") as infile:
-        context = json.load(infile)
+    with path.open("r") as io:
+        context = json.load(io)
 
     return context
