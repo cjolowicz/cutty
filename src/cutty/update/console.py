400a418e030c22dd69c101d98d3dcc8e4c8f6d59
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-14 23:28:15 +0200

    Remove logging and --verbose and --debug-file options

--- src/cutty/update/console.py
+++ src/cutty/update/console.py
@@ -2,7 +2,6 @@
 from typing import Optional
 
 import click
-from cookiecutter.log import configure_logger
 
 from . import core
 from ..create.console import validate_extra_context
@@ -30,9 +29,6 @@ from ..utils import as_optional_path
     ),
 )
 @click.option(
-    "-v", "--verbose", is_flag=True, help="Print debug information", default=False
-)
-@click.option(
     "--config-file",
     type=click.Path(),
     default=None,
@@ -44,25 +40,15 @@ from ..utils import as_optional_path
     is_flag=True,
     help="Do not load a config file. Use the defaults instead",
 )
-@click.option(
-    "--debug-file",
-    type=click.Path(),
-    default=None,
-    help="File to be used as a stream for DEBUG logging",
-)
 def update(
     extra_context: StrMapping,
     interactive: bool,
     checkout: Optional[str],
     directory: Optional[str],
-    verbose: bool,
     config_file: Optional[str],
     default_config: bool,
-    debug_file: Optional[str],
 ) -> None:
     """Update a project from a Cookiecutter template."""
-    configure_logger(stream_level="DEBUG" if verbose else "INFO", debug_file=debug_file)
-
     core.update(
         extra_context,
         interactive=interactive,
