3eb911b5b2771243b75f3fd6332549a9c36d683c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-15 17:53:39 +0200

    Reformat

--- src/cutty/exceptions.py
+++ src/cutty/exceptions.py
@@ -1,19 +1,15 @@
-# -*- coding: utf-8 -*-
-
 """All exceptions used in the Cookiecutter code base are defined here."""
 
 
 class CookiecutterException(Exception):
-    """
-    Base exception class.
+    """Base exception class.
 
     All Cookiecutter-specific exceptions should subclass this class.
     """
 
 
 class NonTemplatedInputDirException(CookiecutterException):
-    """
-    Exception for when a project's input dir is not templated.
+    """Exception for when a project's input dir is not templated.
 
     The name of the input directory should always contain a string that is
     rendered to something else, so that input_dir != output_dir.
@@ -21,8 +17,7 @@ class NonTemplatedInputDirException(CookiecutterException):
 
 
 class UnknownTemplateDirException(CookiecutterException):
-    """
-    Exception for ambiguous project template directory.
+    """Exception for ambiguous project template directory.
 
     Raised when Cookiecutter cannot determine which directory is the project
     template, e.g. more than one dir appears to be a template dir.
@@ -32,8 +27,7 @@ class UnknownTemplateDirException(CookiecutterException):
 
 
 class MissingProjectDir(CookiecutterException):
-    """
-    Exception for missing generated project directory.
+    """Exception for missing generated project directory.
 
     Raised during cleanup when remove_repo() can't find a generated project
     directory inside of a repo.
@@ -43,8 +37,7 @@ class MissingProjectDir(CookiecutterException):
 
 
 class ConfigDoesNotExistException(CookiecutterException):
-    """
-    Exception for missing config file.
+    """Exception for missing config file.
 
     Raised when get_config() is passed a path to a config file, but no file
     is found at that path.
@@ -52,8 +45,7 @@ class ConfigDoesNotExistException(CookiecutterException):
 
 
 class InvalidConfiguration(CookiecutterException):
-    """
-    Exception for invalid configuration file.
+    """Exception for invalid configuration file.
 
     Raised if the global configuration file is not valid YAML or is
     badly constructed.
@@ -61,40 +53,35 @@ class InvalidConfiguration(CookiecutterException):
 
 
 class UnknownRepoType(CookiecutterException):
-    """
-    Exception for unknown repo types.
+    """Exception for unknown repo types.
 
     Raised if a repo's type cannot be determined.
     """
 
 
 class VCSNotInstalled(CookiecutterException):
-    """
-    Exception when version control is unavailable.
+    """Exception when version control is unavailable.
 
     Raised if the version control system (git or hg) is not installed.
     """
 
 
 class ContextDecodingException(CookiecutterException):
-    """
-    Exception for failed JSON decoding.
+    """Exception for failed JSON decoding.
 
     Raised when a project's JSON context file can not be decoded.
     """
 
 
 class OutputDirExistsException(CookiecutterException):
-    """
-    Exception for existing output directory.
+    """Exception for existing output directory.
 
     Raised when the output directory of the project exists already.
     """
 
 
 class InvalidModeException(CookiecutterException):
-    """
-    Exception for incompatible modes.
+    """Exception for incompatible modes.
 
     Raised when cookiecutter is called with both `no_input==True` and
     `replay==True` at the same time.
@@ -102,16 +89,14 @@ class InvalidModeException(CookiecutterException):
 
 
 class FailedHookException(CookiecutterException):
-    """
-    Exception for hook failures.
+    """Exception for hook failures.
 
     Raised when a hook script fails.
     """
 
 
 class UndefinedVariableInTemplate(CookiecutterException):
-    """
-    Exception for out-of-scope variables.
+    """Exception for out-of-scope variables.
 
     Raised when a template uses a variable which is not defined in the
     context.
@@ -133,32 +118,28 @@ class UndefinedVariableInTemplate(CookiecutterException):
 
 
 class UnknownExtension(CookiecutterException):
-    """
-    Exception for un-importable extention.
+    """Exception for un-importable extention.
 
     Raised when an environment is unable to import a required extension.
     """
 
 
 class RepositoryNotFound(CookiecutterException):
-    """
-    Exception for missing repo.
+    """Exception for missing repo.
 
     Raised when the specified cookiecutter repository doesn't exist.
     """
 
 
 class RepositoryCloneFailed(CookiecutterException):
-    """
-    Exception for un-cloneable repo.
+    """Exception for un-cloneable repo.
 
     Raised when a cookiecutter template can't be cloned.
     """
 
 
 class InvalidZipRepository(CookiecutterException):
-    """
-    Exception for bad zip repo.
+    """Exception for bad zip repo.
 
     Raised when the specified cookiecutter repository isn't a valid
     Zip archive.
