3856a46ec4764397c551dfcddd8b16d5a7e0bed9
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-16 21:48:04 +0200

    Adapt tests to changes in cache module

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -2,54 +2,51 @@
 from pathlib import Path
 
 from .utils import commit
-from cutty import cache
 from cutty import git
+from cutty.cache import Cache
 
 
 def test_repository_clones(user_cache_dir: Path, repository: git.Repository) -> None:
     """It clones the repository if it does not exist."""
     commit(repository)
     location = str(repository.path)
-    mirror = cache.Entry(location).repository
-    origin = mirror.get_remote_url("origin")
-    assert origin == location
-
-
-def test_repository_uses_unique_path(
-    user_cache_dir: Path, repository: git.Repository
-) -> None:
-    """It returns the same path when called multiple times."""
-    commit(repository)
-    location = str(repository.path)
-    mirror1 = cache.Entry(location).repository
-    mirror2 = cache.Entry(location).repository
-    assert mirror1.path == mirror2.path
+    with Cache.load(location) as cache:
+        mirror = git.Repository(cache.repository)
+        origin = mirror.get_remote_url("origin")
+        assert origin == location
 
 
 def test_repository_updates(user_cache_dir: Path, repository: git.Repository) -> None:
     """It updates the repository if it exists."""
     commit(repository)
     location = str(repository.path)
-    cache.Entry(location)
+
+    with Cache.load(location) as cache:
+        pass
+
     head = commit(repository)
-    mirror = cache.Entry(location).repository
-    assert mirror.rev_parse("HEAD") == head
+
+    with Cache.load(location) as cache:
+        mirror = git.Repository(cache.repository)
+        assert mirror.rev_parse("HEAD") == head
 
 
 def test_worktree_creates(user_cache_dir: Path, repository: git.Repository) -> None:
     """It creates a worktree."""
     head = commit(repository)
     location = str(repository.path)
-    entry = cache.Entry(location, revision="HEAD")
-    with entry.checkout() as path:
-        assert git.Repository(path).rev_parse("HEAD") == head
+
+    with Cache.load(location, revision="HEAD") as cache:
+        mirror = git.Repository(cache.repository)
+        assert mirror.rev_parse("HEAD") == head
 
 
 def test_worktree_removes(user_cache_dir: Path, repository: git.Repository) -> None:
     """It removes the worktree after use."""
     commit(repository)
     location = str(repository.path)
-    entry = cache.Entry(location, revision="HEAD")
-    with entry.checkout() as path:
+
+    with Cache.load(location, revision="HEAD") as cache:
         pass
-    assert not path.exists()
+
+    assert not cache.repository.exists()
