8e48edac3e5c86b0fbd248389bc1cc882e535ee4
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-26 22:16:50 +0200

    create: Remove replay support from API

--- src/cutty/create/core.py
+++ src/cutty/create/core.py
@@ -2,7 +2,6 @@
 from pathlib import Path
 from typing import Optional
 
-from .. import exceptions
 from ..cache import Cache
 from ..config import Config
 from ..context import override_context
@@ -17,7 +16,6 @@ def create(
     no_input: bool = False,
     checkout: Optional[str] = None,
     directory: Optional[Path] = None,
-    replay: bool = False,
     overwrite_if_exists: bool = False,
     skip_if_file_exists: bool = False,
     output_dir: str = ".",
@@ -25,23 +23,14 @@ def create(
     default_config: bool = False,
 ) -> None:
     """Create a project from a Cookiecutter template."""
-    if replay and no_input:
-        raise exceptions.InvalidModeException(
-            "You can not use both replay and no_input at the same time."
-        )
-
     config = Config.load(config_file, ignore_config=default_config)
     template = config.abbreviations.expand(template)
 
     with Cache.load(template, directory=directory, revision=checkout) as cache:
-        if replay:
-            context = cache.context.load()
-        else:
-            context = Store(cache.repository / "cookiecutter.json").load()
-            context = override_context(context, config.default_context)
-            context = prompt_for_config(context, no_input=no_input)
-            context = {**context, "_template": template}
-            cache.context.dump(context)
+        context = Store(cache.repository / "cookiecutter.json").load()
+        context = override_context(context, config.default_context)
+        context = prompt_for_config(context, no_input=no_input)
+        context = {**context, "_template": template}
 
         generate_files(
             repo_dir=cache.repository,
