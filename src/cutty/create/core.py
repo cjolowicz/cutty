4691be67c1838f710c2cd1871540265b00bd4453
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-26 22:10:46 +0200

    create: Remove API support for specifying extra context

--- src/cutty/create/core.py
+++ src/cutty/create/core.py
@@ -9,12 +9,10 @@ from ..context import override_context
 from ..context import Store
 from ..generate import generate_files
 from ..prompt import prompt_for_config
-from ..types import Context
 
 
 def create(
     template: str,
-    extra_context: Context,
     *,
     no_input: bool = False,
     checkout: Optional[str] = None,
@@ -27,10 +25,9 @@ def create(
     default_config: bool = False,
 ) -> None:
     """Create a project from a Cookiecutter template."""
-    if replay and (no_input or extra_context):
+    if replay and no_input:
         raise exceptions.InvalidModeException(
-            "You can not use both replay and no_input or extra_context "
-            "at the same time."
+            "You can not use both replay and no_input at the same time."
         )
 
     config = Config.load(config_file, ignore_config=default_config)
@@ -41,9 +38,7 @@ def create(
             context = cache.context.load()
         else:
             context = Store(cache.repository / "cookiecutter.json").load()
-            context = override_context(
-                context, {**config.default_context, **extra_context}
-            )
+            context = override_context(context, config.default_context)
             context = prompt_for_config(context, no_input=no_input)
             context = {**context, "_template": template}
             cache.context.dump(context)
