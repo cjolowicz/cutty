bd0fadf8662f0f024d4a1b7b567e0459f1c1e3e6
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-30 13:34:09 +0200

    Add template.Config

--- src/cutty/api/template.py
+++ src/cutty/api/template.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 
 import json
 from dataclasses import dataclass
+from dataclasses import replace
 from pathlib import Path
 from typing import Any
 from typing import List
+from typing import Optional
 
 
 DEFAULT_EXTENSIONS = [
@@ -34,24 +36,65 @@ class Variable:
 
 
 @dataclass(frozen=True)
-class Template:
-    """Template."""
+class Config:
+    """Template configuration."""
 
-    root: Path
+    location: str
     variables: List[Variable]
     extensions: List[str]
     copy_without_render: List[str]
 
     @classmethod
-    def load(cls, path: Path, *, location: str) -> Template:
-        """Load the template variables."""
-        with (path / "cookiecutter.json").open() as io:
+    def load(cls, path: Path, *, location: Optional[str] = None) -> Config:
+        """Load the template configuration."""
+        with path.open() as io:
             data = json.load(io)
-            data.setdefault("_template", location)
+            location = data.setdefault("_template", location)
+            assert location is not None  # noqa: S101  # TODO: raise
 
-        root = find_template(path)
         variables = [Variable(name, value) for name, value in data.items()]
         extensions = DEFAULT_EXTENSIONS + data.get("_extensions", [])
         copy_without_render = data.get("_copy_without_render", [])
 
-        return cls(root, variables, extensions, copy_without_render)
+        return cls(location, variables, extensions, copy_without_render)
+
+    def override(self, other: Config) -> Config:
+        """Override variables from another configuration."""
+
+        def _override(variable: Variable) -> Variable:
+            if variable.name.startswith("_"):
+                return variable
+
+            for other_variable in other.variables:
+                if variable.name == other_variable.name and (
+                    not isinstance(variable.value, list)
+                    or other_variable.value in variable.value
+                ):
+                    return other_variable
+
+            # TODO: If variables are missing, we should prompt later.
+            return variable
+
+        return replace(
+            self, variables=[_override(variable) for variable in self.variables]
+        )
+
+
+@dataclass(frozen=True)
+class Template:
+    """Template."""
+
+    root: Path
+    config: Config
+
+    @classmethod
+    def load(
+        cls, path: Path, *, location: str, overrides: Optional[Config] = None
+    ) -> Template:
+        """Load the template variables."""
+        root = find_template(path)
+        config = Config.load(path / "cookiecutter.json")
+        if overrides is not None:
+            config = config.override(overrides)
+
+        return cls(root, config)
