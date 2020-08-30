416a16a838804735a91ff9fda65639fade257aa1
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-08-30 13:34:28 +0200

    create: Add

---
+++ src/cutty/api/create.py
@@ -0,0 +1,27 @@
+"""Create a project."""
+from pathlib import Path
+from typing import Optional
+
+from ..cache import Cache
+from ..config import Config
+from .engine import Engine
+from .template import Template
+
+
+def create(
+    location: str,
+    *,
+    interactive: bool = True,
+    revision: Optional[str] = None,
+    directory: Optional[Path] = None,
+    output_dir: Optional[Path] = None,
+    config_file: Optional[Path] = None,
+) -> None:
+    """Create a project from a Cookiecutter template."""
+    config = Config.load(config_file)
+    location = config.abbreviations.expand(location)
+
+    with Cache.load(location, directory=directory, revision=revision) as cache:
+        template = Template.load(cache.repository, location=location)
+        engine = Engine(template, interactive=interactive)
+        engine.generate(output_dir or Path())
