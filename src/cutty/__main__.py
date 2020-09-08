f9331c5c1dd56a3f4c538eec468ffc65aab16f2e
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-09-08 09:18:41 +0200

    Switch CLI to cutty.cli

--- src/cutty/__main__.py
+++ src/cutty/__main__.py
@@ -1,18 +1,5 @@
 """Command-line interface."""
-import click
-
-from .legacy.create.console import create
-from .legacy.update.console import update
-
-
-@click.group()
-@click.version_option(None, "-V", "--version")
-def main() -> None:
-    """Cutty."""
-
-
-main.add_command(create)
-main.add_command(update)
+from .cli.main import main
 
 
 if __name__ == "__main__":
