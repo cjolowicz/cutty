1888857926cb46e57993bdc97754b49f3361d492
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-09-10 21:07:29 +0200

    Rework exception handling

--- src/cutty/common/exceptions.py
+++ src/cutty/common/exceptions.py
@@ -1,13 +1,39 @@
 """Exceptions."""
+from typing import Iterator
+from typing import Type
+
+from .compat import contextmanager
 
 
 class CuttyException(Exception):
     """Base class for cutty exceptions."""
 
+    def __str__(self) -> str:
+        """Convert the exception to a string."""
+        assert self.__doc__ is not None  # noqa: S101
+        return self.__doc__.format(*self.args)
 
-class ConfigurationDoesNotExist(CuttyException):
-    """The configuration file does not exist."""
+    @contextmanager
+    def when(self, *exception_types: Type[Exception]) -> Iterator[None]:
+        """Context manager handling exceptions by raising self."""
+        if not exception_types:
+            exception_types = (Exception,)
+
+        try:
+            yield
+        except CuttyException:
+            raise
+        except exception_types as exception:
+            raise self from exception
 
 
-class InvalidConfiguration(CuttyException):
-    """The configuration file is not valid."""
+class ConfigurationError(CuttyException):
+    """Configuration file {} cannot be loaded."""
+
+
+class ConfigurationDoesNotExist(ConfigurationError):
+    """Configuration file {} not found."""
+
+
+class InvalidConfiguration(ConfigurationError):
+    """The configuration file {} is not valid."""
