4a48e8fb9b55e5e320098920edbda9acf4f11d0d
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-09-12 16:41:49 +0200

    Add `overwrite` parameter to Engine constructor

--- src/cutty/api/engine.py
+++ src/cutty/api/engine.py
@@ -10,13 +10,16 @@ from .template import Template
 class Engine:
     """Engine."""
 
-    def __init__(self, template: Template, *, interactive: bool = True) -> None:
+    def __init__(
+        self, template: Template, *, interactive: bool = True, overwrite: bool = False
+    ) -> None:
         """Initialize."""
         self.renderer = Renderer(template)
         self.survey = Survey(template.config.variables, interactive=interactive)
         self.generator = Generator(template=template, renderer=self.renderer)
+        self.overwrite = overwrite
 
     def generate(self, output_dir: Path) -> None:
         """Generate the project."""
         self.survey.run(self.renderer)
-        self.generator.generate(output_dir)
+        self.generator.generate(output_dir, overwrite=self.overwrite)
