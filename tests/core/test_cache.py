7ab160e2a31d7a4752bad5bffd3b1cbe20af9419
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-09-13 21:15:45 +0200

    Adapt cache test

--- tests/core/test_cache.py
+++ tests/core/test_cache.py
@@ -6,47 +6,45 @@ from cutty.core import cache
 from cutty.core import git
 
 
-def test_repository_clones(user_cache_dir: Path, repository: git.Repository) -> None:
+def test_repository_clones(user_cache_dir: Path, template: git.Repository) -> None:
     """It clones the repository if it does not exist."""
-    commit(repository)
-    location = str(repository.path)
-    with cache.load(location) as template:
-        mirror = git.Repository(template.repository)
+    location = str(template.path)
+    with cache.load(location) as loaded_template:
+        mirror = git.Repository(loaded_template.repository)
         origin = mirror.get_remote_url("origin")
         assert origin == location
 
 
-def test_repository_updates(user_cache_dir: Path, repository: git.Repository) -> None:
+def test_repository_updates(user_cache_dir: Path, template: git.Repository) -> None:
     """It updates the repository if it exists."""
-    commit(repository)
-    location = str(repository.path)
+    location = str(template.path)
 
-    with cache.load(location) as template:
+    with cache.load(location):
         pass
 
-    head = commit(repository)
+    head = commit(template)
+    template.tag("v1.0.1")
 
-    with cache.load(location) as template:
-        mirror = git.Repository(template.repository)
+    with cache.load(location) as loaded_template:
+        mirror = git.Repository(loaded_template.repository)
         assert mirror.rev_parse("HEAD") == head
 
 
-def test_worktree_creates(user_cache_dir: Path, repository: git.Repository) -> None:
+def test_worktree_creates(user_cache_dir: Path, template: git.Repository) -> None:
     """It creates a worktree."""
-    head = commit(repository)
-    location = str(repository.path)
+    head = template.rev_parse("HEAD")
+    location = str(template.path)
 
-    with cache.load(location, revision="HEAD") as template:
-        mirror = git.Repository(template.repository)
+    with cache.load(location, revision="HEAD") as loaded_template:
+        mirror = git.Repository(loaded_template.repository)
         assert mirror.rev_parse("HEAD") == head
 
 
-def test_worktree_removes(user_cache_dir: Path, repository: git.Repository) -> None:
+def test_worktree_removes(user_cache_dir: Path, template: git.Repository) -> None:
     """It removes the worktree after use."""
-    commit(repository)
-    location = str(repository.path)
+    location = str(template.path)
 
-    with cache.load(location, revision="HEAD") as template:
+    with cache.load(location, revision="HEAD") as loaded_template:
         pass
 
-    assert not template.repository.exists()
+    assert not loaded_template.repository.exists()
