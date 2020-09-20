4f680d7a87e039f16cd50f6477652289a65ba5fc
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-09-20 17:12:31 +0200

    Use Project in api.update

--- src/cutty/api/update.py
+++ src/cutty/api/update.py
@@ -2,16 +2,8 @@
 from pathlib import Path
 from typing import Optional
 
-from ..core import git
 from ..core.application import Application
-
-
-def _ensure_branch_exists(repository: git.Repository, branch: str) -> None:
-    try:
-        repository.rev_parse(branch, verify=True, quiet=True)
-    except git.Error:
-        (firstref,) = repository.rev_list(max_count=1, max_parents=0)
-        repository.branch(branch, firstref)
+from ..core.project import Project
 
 
 def update(
@@ -23,30 +15,17 @@ def update(
 ) -> None:
     """Update a project from a Cookiecutter template."""
     application = Application.create(config_file=config_file)
-    instance = git.Repository()
+    project = Project.load(Path.cwd())
 
     with application.load_template_from_instance(
-        instance.path, directory=directory, revision=revision
+        project.repository.path, directory=directory, revision=revision
     ) as template:
-        _ensure_branch_exists(instance, "template")
-
-        with instance.worktree(
-            instance.path / ".git" / "cutty" / instance.path.name,
-            "template",
-            checkout=False,
-            force_remove=True,
-        ) as project:
+        with project.worktree(
+            commit_message=f"Update template to {template.version}"
+        ) as worktree:
             application.generate_project(
                 template,
-                output_dir=project.path.parent,
+                output_dir=worktree.parent,
                 interactive=interactive,
                 overwrite=True,
             )
-
-            project.add(all=True)
-            project.commit(
-                message=f"Update template to {template.version}", verify=False
-            )
-
-            commit = project.rev_parse("HEAD")
-            instance.cherrypick(commit)
