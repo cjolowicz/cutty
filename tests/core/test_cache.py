4afc7d7659f261bf49dee33e9d9548e017e69250
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-09-20 15:03:03 +0200

    Add cache fixture

--- tests/core/test_cache.py
+++ tests/core/test_cache.py
@@ -1,14 +1,11 @@
 """Tests for the cache module."""
-from pathlib import Path
-
 from ..utils import commit
 from cutty.core import git
 from cutty.core.cache import Cache
 
 
-def test_repository_clones(user_cache_dir: Path, template: git.Repository) -> None:
+def test_repository_clones(cache: Cache, template: git.Repository) -> None:
     """It clones the repository if it does not exist."""
-    cache = Cache(user_cache_dir)
     location = str(template.path)
     with cache.load(location) as loaded_template:
         mirror = git.Repository(loaded_template.repository)
@@ -16,9 +13,8 @@ def test_repository_clones(user_cache_dir: Path, template: git.Repository) -> No
         assert origin == location
 
 
-def test_repository_updates(user_cache_dir: Path, template: git.Repository) -> None:
+def test_repository_updates(cache: Cache, template: git.Repository) -> None:
     """It updates the repository if it exists."""
-    cache = Cache(user_cache_dir)
     location = str(template.path)
 
     with cache.load(location):
@@ -32,9 +28,8 @@ def test_repository_updates(user_cache_dir: Path, template: git.Repository) -> N
         assert mirror.rev_parse("HEAD") == head
 
 
-def test_worktree_creates(user_cache_dir: Path, template: git.Repository) -> None:
+def test_worktree_creates(cache: Cache, template: git.Repository) -> None:
     """It creates a worktree."""
-    cache = Cache(user_cache_dir)
     head = template.rev_parse("HEAD")
     location = str(template.path)
 
@@ -43,9 +38,8 @@ def test_worktree_creates(user_cache_dir: Path, template: git.Repository) -> Non
         assert mirror.rev_parse("HEAD") == head
 
 
-def test_worktree_removes(user_cache_dir: Path, template: git.Repository) -> None:
+def test_worktree_removes(cache: Cache, template: git.Repository) -> None:
     """It removes the worktree after use."""
-    cache = Cache(user_cache_dir)
     location = str(template.path)
 
     with cache.load(location, revision="HEAD") as loaded_template:
