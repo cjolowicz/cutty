0794d8655eaa7664414484c4ccdf6d17a350450c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-09-24 21:27:16 +0200

    Make Template.root a property

--- src/cutty/core/template.py
+++ src/cutty/core/template.py
@@ -3,32 +3,17 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional
 
 from . import exceptions
+from .compat import cached_property
 from .hooks import Hooks
 from .variables import Variables
-
-
-def find_template(path: Path) -> Optional[Path]:
-    """Determine which child directory is the project template."""
-    for item in path.iterdir():
-        if (
-            item.is_dir()
-            and "cookiecutter" in item.name
-            and "{{" in item.name
-            and "}}" in item.name
-        ):
-            return item
-
-    return None
 
 
 @dataclass(frozen=True)
 class Template:
     """Template."""
 
-    root: Path
     hooks: Hooks
     repository: Path
     version: str
@@ -37,20 +22,24 @@ class Template:
     @classmethod
     def load(cls, path: Path, *, version: str, location: str) -> Template:
         """Load the template variables."""
-        root = find_template(path)
-        if root is None:
-            raise exceptions.TemplateDirectoryNotFound(location)
-
         hooks = Hooks.load(path / "hooks")
         variables = Variables.load(path / "cookiecutter.json", location=location)
 
-        return cls(
-            root=root,
-            hooks=hooks,
-            repository=path,
-            version=version,
-            variables=variables,
-        )
+        return cls(hooks=hooks, repository=path, version=version, variables=variables)
+
+    @cached_property
+    def root(self) -> Path:
+        """Return the template directory."""
+        for path in self.repository.iterdir():
+            if (
+                path.is_dir()
+                and "cookiecutter" in path.name
+                and "{{" in path.name
+                and "}}" in path.name
+            ):
+                return path
+
+        raise exceptions.TemplateDirectoryNotFound(self.variables.location)
 
     @property
     def name(self) -> str:
