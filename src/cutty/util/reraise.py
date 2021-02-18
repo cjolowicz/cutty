01b0203c0c7f6bfec49f46e4337d122df10f2d21
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-18 15:18:52 +0100

    feat(util): Add module reraise

---
+++ src/cutty/util/reraise.py
@@ -0,0 +1,23 @@
+"""Utility for handling an exception by raising another one."""
+from typing import NoReturn
+
+from cutty.util.exceptionhandlers import ExceptionHandler
+from cutty.util.exceptionhandlers import exceptionhandler
+
+
+def reraise(
+    exception: BaseException,
+    *,
+    when: type[BaseException] | tuple[type[BaseException], ...] = ()
+) -> ExceptionHandler:
+    """Raise the given exception when any of the specified exceptions is caught."""
+    if not isinstance(when, tuple):
+        when = (when,)
+    elif not when:
+        when = (Exception,)
+
+    @exceptionhandler(*when)
+    def _(other: BaseException) -> NoReturn:
+        raise exception
+
+    return _
