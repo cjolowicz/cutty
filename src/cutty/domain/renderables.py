0a9143cffad8d962886c15ad1466e6bfbc872589
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-18 11:44:26 +0100

    feat(domain): Add module renderables

---
+++ src/cutty/domain/renderables.py
@@ -0,0 +1,87 @@
+"""Rendering."""
+import abc
+from collections.abc import Iterable
+from collections.abc import Sequence
+from typing import Generic
+
+from cutty.domain.paths import Path
+from cutty.domain.variables import Value
+from cutty.domain.variables import ValueT_co
+from cutty.domain.variables import Variable
+
+
+class Renderable(abc.ABC, Generic[ValueT_co]):
+    """Something that can be rendered using variables."""
+
+    @abc.abstractmethod
+    def render(self, variables: Sequence[Variable[Value]]) -> ValueT_co:
+        """Render the object."""
+
+
+class TrivialRenderable(Renderable[ValueT_co]):
+    """A renderable that just returns its value."""
+
+    def __init__(self, value: ValueT_co) -> None:
+        """Initialize."""
+        self.value = value
+
+    def render(self, variables: Sequence[Variable[Value]]) -> ValueT_co:
+        """Return the value."""
+        return self.value
+
+
+class RenderableList(Renderable[list[Value]]):
+    """A renderable list."""
+
+    def __init__(self, renderables: Iterable[Renderable[Value]]) -> None:
+        """Initialize."""
+        self.renderables = list(renderables)
+
+    def render(self, variables: Sequence[Variable[Value]]) -> list[Value]:
+        """Return a list of rendered items."""
+        return [renderable.render(variables) for renderable in self.renderables]
+
+
+class RenderableDict(Renderable[dict[str, Value]]):
+    """A renderable dictionary."""
+
+    def __init__(
+        self, renderables: Iterable[tuple[Renderable[str], Renderable[Value]]]
+    ) -> None:
+        """Initialize."""
+        self.renderables = list(renderables)
+
+    def render(self, variables: Sequence[Variable[Value]]) -> dict[str, Value]:
+        """Return a dictionary of rendered key-value pairs."""
+        return {
+            key.render(variables): value.render(variables)
+            for key, value in self.renderables
+        }
+
+
+class RenderableLoader(abc.ABC):
+    """Load renderables."""
+
+    def load(self, value: Value) -> Renderable[Value]:
+        """Load renderable."""
+        if isinstance(value, str):
+            return self.loadtext(value)
+
+        if isinstance(value, list):
+            return RenderableList(self.load(item) for item in value)
+
+        if isinstance(value, dict):
+            assert all(isinstance(key, str) for key in value)  # noqa: S101
+            return RenderableDict(
+                (self.loadtext(key), self.load(item)) for key, item in value.items()
+            )
+
+        return TrivialRenderable(str(value) if value is not None else None)
+
+    @abc.abstractmethod
+    def loadtext(self, text: str) -> Renderable[str]:
+        """Load renderable from text."""
+
+    @abc.abstractmethod
+    def get(self, path: Path) -> Renderable[str]:
+        """Get renderable by path."""
