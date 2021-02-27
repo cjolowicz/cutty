cb537d70eba009f8e82bd94b0fb23c400314dc46
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-27 23:01:20 +0100

    test(application): Remove fixture from cookiecutter.templates test

--- tests/unit/application/cookiecutter/test_templates.py
+++ tests/unit/application/cookiecutter/test_templates.py
@@ -1,30 +1,8 @@
 """Unit tests for cutty.application.cookiecutter.templates."""
 import pathlib
 
-import pytest
-
 from cutty.application.cookiecutter.templates import load
 from cutty.domain.varspecs import DefaultVariableBuilder
-
-
-def create(path: pathlib.Path, text: str) -> None:
-    """Create a file with the given path and contents."""
-    path.parent.mkdir(parents=True, exist_ok=True)
-    path.write_text(text)
-
-
-@pytest.fixture
-def template_directory(tmp_path: pathlib.Path) -> pathlib.Path:
-    """Fixture for a template directory."""
-    create(
-        tmp_path / "cookiecutter.json",
-        '{"project": "example", "license": ["MIT", "GPL-3.0", "Apache-2.0"]}',
-    )
-    create(
-        tmp_path / "{{ cookiecutter.project }}" / "README.md",
-        "# {{ cookiecutter.project }}\n",
-    )
-    return tmp_path
 
 
 def test_load(template_directory: pathlib.Path) -> None:
