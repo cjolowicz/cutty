37e80f496706097f02a7baa899a926e41901eb54
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-27 23:00:53 +0100

    test(conftest): Add top-level conftest.py with template_directory fixture

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+"""Common fixtures."""
+import pathlib
+
+import pytest
+
+
+def create(path: pathlib.Path, text: str) -> None:
+    """Create a file with the given path and contents."""
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(text)
+
+
+@pytest.fixture
+def template_directory(tmp_path: pathlib.Path) -> pathlib.Path:
+    """Fixture for a template directory."""
+    create(
+        tmp_path / "cookiecutter.json",
+        '{"project": "example", "license": ["MIT", "GPL-3.0", "Apache-2.0"]}',
+    )
+    create(
+        tmp_path / "{{ cookiecutter.project }}" / "README.md",
+        "# {{ cookiecutter.project }}\n",
+    )
+    return tmp_path
