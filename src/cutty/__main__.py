0ade4decdde2147dbf5bde5df3c971e60d62ef6c
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-27 22:53:27 +0100

    feat(__main__): Implement using cutty.application.cookiecutter.main
    This is a basic functional implementation that can create projects from
    Cookiecutter templates on the local filesystem.

--- src/cutty/__main__.py
+++ src/cutty/__main__.py
@@ -1,11 +1,17 @@
 """Command-line interface."""
+from pathlib import Path
+
 import click
+
+import cutty.application.cookiecutter.main
 
 
 @click.command()
+@click.argument("directory")
 @click.version_option()
-def main() -> None:
+def main(directory: str) -> None:
     """cutty."""
+    cutty.application.cookiecutter.main.main(Path(directory))
 
 
 if __name__ == "__main__":
