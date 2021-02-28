28a284901b2174c07f81d5ca263ee6b35d17544c
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-28 17:36:35 +0100

    refactor(domain): Add method Template.render

--- src/cutty/domain/templates.py
+++ src/cutty/domain/templates.py
@@ -1,9 +1,14 @@
 """Templates."""
+import contextlib
 from collections.abc import Iterable
+from collections.abc import Iterator
 
+from cutty.domain.files import File
 from cutty.domain.files import RenderableFile
+from cutty.domain.paths import EmptyPathComponent
 from cutty.domain.renderables import Renderable
 from cutty.domain.variables import Value
+from cutty.domain.varspecs import VariableBuilder
 from cutty.domain.varspecs import VariableSpecification
 
 
@@ -19,3 +24,10 @@ class Template:
         """Initialize."""
         self.variables = tuple(variables)
         self.files = tuple(files)
+
+    def render(self, builder: VariableBuilder) -> Iterator[File]:
+        """Render the template."""
+        variables = builder.build(self.variables)
+        for file in self.files:
+            with contextlib.suppress(EmptyPathComponent):
+                yield file.render(variables)
