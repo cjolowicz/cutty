ee28bd81a277d64f65a16aacae9e9bb7e5a57a60
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-28 17:38:33 +0100

    refactor(application): Use Template.render in cookiecutter.main

--- src/cutty/application/cookiecutter/main.py
+++ src/cutty/application/cookiecutter/main.py
@@ -1,11 +1,9 @@
 """Main entry point for the Cookiecutter compatibility layer."""
-import contextlib
 from pathlib import Path
 
 from cutty.adapters.click.prompts import ClickPromptFactory
 from cutty.adapters.filesystem.files import FilesystemFileStorage
 from cutty.application.cookiecutter import templates
-from cutty.domain.paths import EmptyPathComponent
 from cutty.domain.prompts import PromptVariableBuilder
 
 
@@ -13,9 +11,6 @@ def main(directory: Path) -> None:
     """Generate a project from a Cookiecutter template."""
     template = templates.load(directory)
     builder = PromptVariableBuilder(ClickPromptFactory())
-    variables = builder.build(template.variables)
     storage = FilesystemFileStorage(Path.cwd())
-    for renderable in template.files:
-        with contextlib.suppress(EmptyPathComponent):
-            file = renderable.render(variables)
-            storage.store(file)
+    for file in template.render(builder):
+        storage.store(file)
