cd536d0c737d78b7b0de75cce1e0cf1ccfafa729
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-28 21:25:04 +0100

    test(domain): Use Template.render API

--- tests/unit/domain/test_templates.py
+++ tests/unit/domain/test_templates.py
@@ -1,9 +1,10 @@
 """Unit tests for cutty.domain.templates."""
+from cutty.domain.renderables import RenderableLoader
 from cutty.domain.templates import Template
+from cutty.domain.varspecs import DefaultVariableBuilder
 
 
-def test_template() -> None:
-    """It has variables and files."""
-    template = Template(variables=[], files=[])
-    assert template.variables == ()
-    assert template.files == ()
+def test_template(renderable_loader: RenderableLoader) -> None:
+    """It can be rendered."""
+    template = Template(loader=renderable_loader, variables=[], paths=[])
+    template.render(DefaultVariableBuilder())
