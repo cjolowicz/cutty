218b2529a5e3269d02ba7846e1f203264f22c902
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-28 22:14:04 +0100

    refactor(application): Use cookiecutter.variables in ~.templates

--- src/cutty/application/cookiecutter/templates.py
+++ src/cutty/application/cookiecutter/templates.py
@@ -2,75 +2,11 @@
 import json
 import pathlib
 from collections.abc import Iterator
-from typing import Any
 
 from cutty.adapters.jinja.renderables import JinjaRenderableLoader
+from cutty.application.cookiecutter import variables
 from cutty.domain.paths import Path
-from cutty.domain.renderables import Renderable
-from cutty.domain.renderables import RenderableLoader
-from cutty.domain.renderables import TrivialRenderable
 from cutty.domain.templates import Template
-from cutty.domain.variables import Value
-from cutty.domain.varspecs import VariableSpecification
-from cutty.domain.varspecs import VariableType
-
-
-def _get_variable_type(value: Any) -> VariableType:
-    mapping = {
-        type(None): VariableType.NULL,
-        bool: VariableType.BOOLEAN,
-        float: VariableType.NUMBER,
-        str: VariableType.STRING,
-        list: VariableType.ARRAY,
-        dict: VariableType.OBJECT,
-    }
-
-    for value_type, variable_type in mapping.items():
-        if isinstance(value, value_type):
-            return variable_type
-
-    raise RuntimeError(f"unsupported variable type {type(value)}")  # pragma: no cover
-
-
-def _load_variable(
-    loader: RenderableLoader, name: str, value: Any
-) -> VariableSpecification[Renderable[Value]]:
-    if name.startswith("_"):
-        return VariableSpecification(
-            name,
-            name,
-            _get_variable_type(value),
-            TrivialRenderable(value),
-            choices=(),
-            interactive=False,
-        )
-
-    if isinstance(value, list):
-        [variable_type] = set(_get_variable_type(choice) for choice in value)
-        choices = tuple(loader.load(choice) for choice in value)
-        return VariableSpecification(
-            name,
-            name,
-            variable_type,
-            choices[0],
-            choices,
-            interactive=True,
-        )
-
-    return VariableSpecification(
-        name,
-        name,
-        _get_variable_type(value),
-        loader.load(value),
-        choices=(),
-        interactive=True,
-    )
-
-
-def _load_variables(
-    loader: RenderableLoader, data: dict[str, Any]
-) -> list[VariableSpecification[Renderable[Value]]]:
-    return [_load_variable(loader, name, value) for name, value in data.items()]
 
 
 def _walk_files(path: pathlib.Path) -> Iterator[pathlib.Path]:
@@ -105,6 +41,9 @@ def load(path: pathlib.Path) -> Template:
     loader = JinjaRenderableLoader.create(
         path, context_prefix="cookiecutter", extra_extensions=extensions
     )
-    variables = _load_variables(loader, data)
     paths = _load_paths(path)
-    return Template(loader=loader, variables=variables, paths=paths)
+    return Template(
+        loader=loader,
+        variables=variables.load(loader, data),
+        paths=paths,
+    )
