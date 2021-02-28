ca6c96eaa55cb75bab12149a3a3aaa8e3c23cbd0
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-28 21:23:44 +0100

    test(application): Use Template.render with cookiecutter.templates

--- tests/unit/application/cookiecutter/test_templates.py
+++ tests/unit/application/cookiecutter/test_templates.py
@@ -9,7 +9,6 @@ def test_load(template_directory: pathlib.Path) -> None:
     """It loads a template."""
     template = load(template_directory)
     builder = DefaultVariableBuilder()
-    variables = builder.build(template.variables)
-    [readme] = [file.render(variables) for file in template.files]
+    [readme] = template.render(builder)
     assert readme.path.parts == ("example", "README.md")
     assert readme.blob == "# example\n"
