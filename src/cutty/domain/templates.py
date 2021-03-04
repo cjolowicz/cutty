eae8aaa567472c050dee823e28e0a4ec37394e65
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-04 15:15:29 +0100

    refactor(domain): Use RenderableFileRepository in Template

--- src/cutty/domain/templates.py
+++ src/cutty/domain/templates.py
@@ -4,7 +4,7 @@ from collections.abc import Iterable
 from collections.abc import Iterator
 
 from cutty.domain.files import File
-from cutty.domain.files import RenderableFileLoader
+from cutty.domain.files import RenderableFileRepository
 from cutty.domain.paths import EmptyPathComponent
 from cutty.domain.renderables import Renderable
 from cutty.domain.variables import Value
@@ -18,17 +18,17 @@ class Template:
     def __init__(
         self,
         *,
-        loader: RenderableFileLoader,
+        files: RenderableFileRepository,
         variables: Iterable[VariableSpecification[Renderable[Value]]],
     ) -> None:
         """Initialize."""
-        self.loader = loader
+        self.files = files
         self.variables = tuple(variables)
 
     def render(self, builder: VariableBuilder) -> Iterator[File]:
         """Render the template."""
         variables = builder.build(self.variables)
 
-        for file in self.loader.load():
+        for file in self.files.load():
             with contextlib.suppress(EmptyPathComponent):
                 yield file.render(variables)
