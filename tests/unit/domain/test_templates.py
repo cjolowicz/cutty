d8588af8a6421c6a1d408987a7c67f485228a571
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-04 22:42:30 +0100

    test(domain): Adapt Template test to RenderableRepository

--- tests/unit/domain/test_templates.py
+++ tests/unit/domain/test_templates.py
@@ -1,12 +1,16 @@
 """Unit tests for cutty.domain.templates."""
 from cutty.domain.files import RenderableFileLoader
 from cutty.domain.renderables import RenderableLoader
+from cutty.domain.renderables import RenderableRepository
 from cutty.domain.templates import Template
 from cutty.domain.varspecs import DefaultVariableBuilder
 
 
-def test_template(renderable_loader: RenderableLoader) -> None:
+def test_template(
+    renderable_loader: RenderableLoader,
+    renderable_repository: RenderableRepository,
+) -> None:
     """It can be rendered."""
-    loader = RenderableFileLoader(renderable_loader, paths=[])
+    loader = RenderableFileLoader(renderable_loader, renderable_repository, paths=[])
     template = Template(files=loader, variables=[])
     template.render(DefaultVariableBuilder())
