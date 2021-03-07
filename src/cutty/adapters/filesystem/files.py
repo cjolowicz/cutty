c77a899bf1dadc2e3a4d04768b4e1555ffdc334a
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-07 21:37:30 +0100

    feat(adapters): Support executable files

--- src/cutty/adapters/filesystem/files.py
+++ src/cutty/adapters/filesystem/files.py
@@ -1,10 +1,12 @@
 """Filesystem implementation of the cutty.domain.files abstractions."""
+import os
 import pathlib
 from collections.abc import Iterator
 
 from cutty.domain.files import File
 from cutty.domain.files import FileRepository
 from cutty.domain.files import FileStorage
+from cutty.domain.files import Mode
 from cutty.domain.paths import Path
 
 
@@ -31,8 +33,9 @@ class FilesystemFileRepository(FileRepository):
         """Iterate over the files in the filesystem."""
         for path in walkfiles(self.path):
             blob = path.read_text()
+            mode = Mode.EXECUTABLE if os.access(path, os.X_OK) else Mode.DEFAULT
             path = path.relative_to(self.root)
-            yield File(Path(path.parts), blob)
+            yield File(Path(path.parts), mode, blob)
 
 
 class FilesystemFileStorage(FileStorage):
@@ -47,3 +50,5 @@ class FilesystemFileStorage(FileStorage):
         path = self.root.joinpath(*file.path.parts)
         path.parent.mkdir(parents=True, exist_ok=True)
         path.write_text(file.blob)
+        if file.mode & Mode.EXECUTABLE:
+            path.chmod(path.stat().st_mode | 0o111)
--- tests/unit/adapters/filesystem/test_files.py
+++ tests/unit/adapters/filesystem/test_files.py
@@ -1,8 +1,13 @@
 """Unit tests for cutty.adapters.filesystem.files."""
 import pathlib
+import platform
 
+import pytest
+
+from cutty.adapters.filesystem.files import FilesystemFileRepository
 from cutty.adapters.filesystem.files import FilesystemFileStorage
 from cutty.domain.files import File
+from cutty.domain.files import Mode
 from cutty.domain.paths import Path
 
 
@@ -10,9 +15,27 @@ def test_storage(tmp_path: pathlib.Path) -> None:
     """It stores the file."""
     path = Path(["example", "README.md"])
     blob = "# example\n"
-    file = File(path, blob)
+    file = File(path, Mode.DEFAULT, blob)
 
     storage = FilesystemFileStorage(tmp_path)
     storage.store(file)
 
     assert blob == (tmp_path / "example" / "README.md").read_text()
+
+
+@pytest.mark.skipif(
+    platform.system() == "Windows",
+    reason="Path.chmod ignores executable bits on Windows.",
+)
+def test_executable(tmp_path: pathlib.Path) -> None:
+    """It stores the file."""
+    path = Path(["main.py"])
+    blob = "#!/usr/bin/env python"
+    file = File(path, Mode.EXECUTABLE, blob)
+
+    storage = FilesystemFileStorage(tmp_path)
+    storage.store(file)
+
+    repository = FilesystemFileRepository(tmp_path / "main.py", relative_to=tmp_path)
+    [file] = repository.load()
+    assert file.mode & Mode.EXECUTABLE
