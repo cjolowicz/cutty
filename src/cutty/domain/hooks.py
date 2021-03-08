a65dd1c979d8b3c58bd1da372705ee75c37c0766
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-08 23:03:07 +0100

    feat(domain): Add module `hooks`

---
+++ src/cutty/domain/hooks.py
@@ -0,0 +1,56 @@
+"""Hooks."""
+import abc
+from collections.abc import Iterable
+from dataclasses import dataclass
+
+from cutty.domain.files import File
+from cutty.util.bus import Bus
+from cutty.util.bus import Event
+
+
+class PreGenerateProject(Event):
+    """This event is published before the project is generated."""
+
+
+class PostGenerateProject(Event):
+    """This event is published after the project is generated."""
+
+
+@dataclass
+class Hook:
+    """Hook."""
+
+    file: File
+    event: type[Event]
+
+
+class HookExecutor(abc.ABC):
+    """Something that can execute a hook."""
+
+    @abc.abstractmethod
+    def execute(self, hook: Hook) -> None:
+        """Execute the hook."""
+
+
+class HookManager:
+    """Hook manager."""
+
+    def __init__(self, hooks: Iterable[Hook], executor: HookExecutor) -> None:
+        """Initialize."""
+        self.hooks = tuple(hooks)
+        self.executor = executor
+
+    def subscribe(self, bus: Bus) -> None:
+        """Subscribe the hooks."""
+        for hook in self.hooks:
+            if hook.event is PreGenerateProject:
+
+                @bus.events.subscribe
+                def _(event: PreGenerateProject) -> None:
+                    self.executor.execute(hook)
+
+            elif hook.event is PostGenerateProject:
+
+                @bus.events.subscribe
+                def _(event: PostGenerateProject) -> None:
+                    self.executor.execute(hook)
