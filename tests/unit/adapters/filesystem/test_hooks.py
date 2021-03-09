f7e954dc9fa5382ef44f458cc4f0328353f5fac5
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-09 13:56:59 +0100

    test(adapters): Add unit test for FilesystemHookExecutor

---
+++ tests/unit/adapters/filesystem/test_hooks.py
@@ -0,0 +1,24 @@
+"""Unit tests for cutty.adapters.filesystem.hooks."""
+import pathlib
+
+import pytest
+
+from cutty.adapters.filesystem.hooks import FilesystemHookExecutor
+from cutty.domain.files import File
+from cutty.domain.files import Mode
+from cutty.domain.files import Path
+from cutty.domain.hooks import Hook
+from cutty.domain.hooks import PostGenerateProject
+
+
+def test_executor(tmp_path: pathlib.Path, capfd: pytest.CaptureFixture[str]) -> None:
+    """It executes the script."""
+    file = File(Path(["hook.py"]), Mode.EXECUTABLE, 'print("hello")\n')
+    hook = Hook(file=file, event=PostGenerateProject)
+
+    executor = FilesystemHookExecutor(cwd=tmp_path)
+    executor.execute(hook)
+
+    captured = capfd.readouterr()
+    assert captured.out == "hello\n"
+    assert captured.err == ""
