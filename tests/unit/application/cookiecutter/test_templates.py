7903c8b52bb617a06e79c36ac69b41034722d18a
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-09 16:29:12 +0100

    refactor(domain): Add TemplateRenderer

--- tests/unit/application/cookiecutter/test_templates.py
+++ tests/unit/application/cookiecutter/test_templates.py
@@ -2,13 +2,14 @@
 import pathlib
 
 from cutty.application.cookiecutter.templates import load
+from cutty.domain.templates import TemplateRenderer
 from cutty.domain.varspecs import DefaultVariableBuilder
 
 
 def test_load(template_directory: pathlib.Path) -> None:
     """It loads a template."""
     template = load(template_directory)
-    builder = DefaultVariableBuilder()
-    [readme] = template.render(builder)
+    renderer = TemplateRenderer(builder=DefaultVariableBuilder())
+    [readme] = renderer.render(template)
     assert readme.path.parts == ("example", "README.md")
     assert readme.blob == "# example\n"
