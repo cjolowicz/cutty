03dbd24cc5b6a32dee5f7860bea5318f0d22ab09
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-11 12:01:11 +0100

    feat(domain): Add TemplateRenderer

--- src/cutty/application/cookiecutter/main.py
+++ src/cutty/application/cookiecutter/main.py
@@ -4,15 +4,15 @@ from pathlib import Path
 from cutty.adapters.click.prompts import ClickPromptFactory
 from cutty.adapters.filesystem.files import FilesystemFileStorage
 from cutty.application.cookiecutter import templates
-from cutty.domain.files import renderfiles
 from cutty.domain.prompts import PromptVariableBuilder
+from cutty.domain.templates import TemplateRenderer
 
 
 def main(directory: Path) -> None:
     """Generate a project from a Cookiecutter template."""
+    renderer = TemplateRenderer(
+        builder=PromptVariableBuilder(ClickPromptFactory()),
+        storage=FilesystemFileStorage(Path.cwd()),
+    )
     template = templates.load(directory)
-    builder = PromptVariableBuilder(ClickPromptFactory())
-    storage = FilesystemFileStorage(Path.cwd())
-    variables = builder.build(template.variables)
-    for file in renderfiles(template.files, variables):
-        storage.store(file)
+    renderer.render(template)
