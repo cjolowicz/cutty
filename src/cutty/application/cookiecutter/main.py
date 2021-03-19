9f37cfb58def95995eebfb5f90f1f2135a11d04a
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-19 11:41:18 +0100

    refactor(application): Use cookiecutter.loader in cookiecutter.main

--- src/cutty/application/cookiecutter/main.py
+++ src/cutty/application/cookiecutter/main.py
@@ -1,18 +1,30 @@
 """Main entry point for the Cookiecutter compatibility layer."""
-from pathlib import Path
+import pathlib
 
 from cutty.adapters.click.prompts import ClickPromptFactory
 from cutty.adapters.filesystem.files import FilesystemFileStorage
-from cutty.application.cookiecutter import templates
+from cutty.adapters.filesystem.filesystem import DiskFilesystem
+from cutty.application.cookiecutter.loader import CookiecutterFileLoader
+from cutty.application.cookiecutter.loader import CookiecutterRenderableLoaderFactory
+from cutty.application.cookiecutter.loader import (
+    CookiecutterVariableSpecificationLoader,
+)
+from cutty.domain.loader import TemplateLoader
 from cutty.domain.prompts import PromptVariableBuilder
 from cutty.domain.templates import TemplateRenderer
 
 
-def main(directory: Path) -> None:
+def main(directory: pathlib.Path) -> None:
     """Generate a project from a Cookiecutter template."""
+    filesystem = DiskFilesystem(directory)
+    loader = TemplateLoader(
+        loaderfactory=CookiecutterRenderableLoaderFactory(),
+        varloader=CookiecutterVariableSpecificationLoader(),
+        fileloader=CookiecutterFileLoader(),
+    )
     renderer = TemplateRenderer(
         builder=PromptVariableBuilder(ClickPromptFactory()),
-        storage=FilesystemFileStorage(Path.cwd()),
+        storage=FilesystemFileStorage(pathlib.Path.cwd()),
     )
-    template = templates.load(directory)
+    template = loader.load(filesystem.root)
     renderer.render(template)
