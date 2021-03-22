091611cc86e4c2598269ed74275553962a4ad59c
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-22 13:41:46 +0100

    refactor(domain): Move TemplateConfig to loader

--- src/cutty/domain/loader.py
+++ src/cutty/domain/loader.py
@@ -1,11 +1,20 @@
 """Loading project templates from the filesystem."""
 import abc
 from collections.abc import Sequence
+from dataclasses import dataclass
 
 from cutty.domain.bindings import Binding
 from cutty.domain.render import Renderer
-from cutty.domain.templates import TemplateConfig
+from cutty.domain.variables import Variable
 from cutty.filesystem.path import Path
+
+
+@dataclass
+class TemplateConfig:
+    """Template configuration."""
+
+    settings: tuple[Binding, ...]
+    variables: tuple[Variable, ...]
 
 
 class TemplateConfigLoader(abc.ABC):
