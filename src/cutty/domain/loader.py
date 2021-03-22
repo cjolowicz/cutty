f0b4a2c18a5401bc40f5b97428c8dbaf41f16a95
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 00:03:26 +0100

    refactor(domain): Move type aliases from loader to services

--- src/cutty/domain/loader.py
+++ src/cutty/domain/loader.py
@@ -1,12 +1,8 @@
 """Loading project templates from the filesystem."""
-from collections.abc import Callable
-from collections.abc import Sequence
 from dataclasses import dataclass
 
 from cutty.domain.bindings import Binding
-from cutty.domain.render import Renderer
 from cutty.domain.variables import Variable
-from cutty.filesystem.path import Path
 
 
 @dataclass
@@ -15,7 +11,3 @@ class TemplateConfig:
 
     settings: tuple[Binding, ...]
     variables: tuple[Variable, ...]
-
-
-TemplateConfigLoader = Callable[[Path], TemplateConfig]
-RendererLoader = Callable[[Path, Sequence[Binding]], Renderer]
