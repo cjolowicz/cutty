44c627a36ad6bb3c81d2631b631df159aee750b9
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-22 13:51:11 +0100

    refactor(domain): Remove module templates

--- src/cutty/domain/render.py
+++ src/cutty/domain/render.py
@@ -1,6 +1,9 @@
 """Rendering."""
+import contextlib
 import functools
 from collections.abc import Callable
+from collections.abc import Iterable
+from collections.abc import Iterator
 from collections.abc import Sequence
 from typing import get_type_hints
 from typing import Optional
@@ -133,3 +136,38 @@ class Renderer:
             )
 
         return render
+
+
+class EmptyPathComponent(Exception):
+    """The rendered path has an empty component."""
+
+
+class InvalidPathComponent(Exception):
+    """The rendered path has an invalid component."""
+
+
+def renderfiles(
+    files: Iterable[File], *, render: Renderer, bindings: Sequence[Binding]
+) -> Iterator[File]:
+    """Render the template."""
+    for file in files:
+        with contextlib.suppress(EmptyPathComponent):
+            yield renderfile(file, render=render, bindings=bindings)
+
+
+def renderfile(file: File, *, render: Renderer, bindings: Sequence[Binding]) -> File:
+    """Render the file."""
+    file = render(file, bindings)
+
+    if not all(file.path.parts):
+        # FIXME: Shouldn't have rendered the blob at all.
+        # FIXME: Can we avoid traversing that directory?
+        raise EmptyPathComponent(str(file.path))
+
+    if any(
+        "/" in part or "\\" in part or part == "." or part == ".."
+        for part in file.path.parts
+    ):
+        raise InvalidPathComponent(str(file.path))
+
+    return file
