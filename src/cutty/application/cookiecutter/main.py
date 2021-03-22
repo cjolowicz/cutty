d350e1176ffaadaf24e8a89d6f4468298bfd3677
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 00:09:37 +0100

    refactor(domain): Use function instead of class for render service

--- src/cutty/application/cookiecutter/main.py
+++ src/cutty/application/cookiecutter/main.py
@@ -7,7 +7,7 @@ from cutty.application.cookiecutter.loader import loadconfig
 from cutty.application.cookiecutter.loader import loadpaths
 from cutty.application.cookiecutter.loader import loadrenderer
 from cutty.domain.binders import renderbindwith
-from cutty.domain.services import RenderService
+from cutty.domain.services import render
 from cutty.filesystem.disk import DiskFilesystem
 from cutty.filesystem.path import Path
 
@@ -16,11 +16,11 @@ def main(directory: pathlib.Path) -> None:
     """Generate a project from a Cookiecutter template."""
     path = Path(filesystem=DiskFilesystem(directory))
     storage = FilesystemFileStorage(pathlib.Path.cwd())
-    service = RenderService(
+    render(
+        path,
         loadconfig=loadconfig,
         loadrenderer=loadrenderer,
         loadpaths=loadpaths,
         renderbind=renderbindwith(prompt),
         storefile=storage.store,
     )
-    service.render(path)
