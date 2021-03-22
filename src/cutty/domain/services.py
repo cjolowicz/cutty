e61c464b404e113cb33fe8aa392b7fac580fa827
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-22 13:36:34 +0100

    refactor(domain): Remove Template

--- src/cutty/domain/services.py
+++ src/cutty/domain/services.py
@@ -5,7 +5,6 @@ from cutty.domain.files import FileStorage
 from cutty.domain.loader import RendererFactory
 from cutty.domain.loader import TemplateConfigLoader
 from cutty.domain.templates import renderfiles
-from cutty.domain.templates import Template
 from cutty.filesystem.path import Path
 
 
@@ -34,7 +33,6 @@ class RenderService:
         renderer = self.rendererfactory.create(path, settings=config.settings)
         bindings = self.binder(config.variables, render=renderer)
         files = self.fileloader.load(path)
-        template = Template(config=config, files=files, renderer=renderer)
 
-        for file in renderfiles(template.files, render=renderer, bindings=bindings):
+        for file in renderfiles(files, render=renderer, bindings=bindings):
             self.storage.store(file)
