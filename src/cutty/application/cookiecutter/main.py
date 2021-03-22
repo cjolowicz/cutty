9cd5982800cf0f66b4203f923d0e4a87eb7455cc
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-22 11:21:39 +0100

    refactor(application): Use domain.services.RenderService

--- src/cutty/application/cookiecutter/main.py
+++ src/cutty/application/cookiecutter/main.py
@@ -6,24 +6,20 @@ from cutty.adapters.filesystem.files import FilesystemFileStorage
 from cutty.application.cookiecutter.loader import CookiecutterFileLoader
 from cutty.application.cookiecutter.loader import CookiecutterRendererFactory
 from cutty.application.cookiecutter.loader import CookiecutterTemplateConfigLoader
-from cutty.domain.loader import TemplateLoader
 from cutty.domain.prompts import PromptBinder
-from cutty.domain.templates import TemplateRenderer
+from cutty.domain.services import RenderService
 from cutty.filesystem.disk import DiskFilesystem
 from cutty.filesystem.path import Path
 
 
 def main(directory: pathlib.Path) -> None:
     """Generate a project from a Cookiecutter template."""
-    path = Path(filesystem=DiskFilesystem(directory))
-    loader = TemplateLoader(
+    service = RenderService(
         configloader=CookiecutterTemplateConfigLoader(),
         rendererfactory=CookiecutterRendererFactory(),
         fileloader=CookiecutterFileLoader(),
-    )
-    renderer = TemplateRenderer(
         binder=PromptBinder(ClickPromptFactory()),
         storage=FilesystemFileStorage(pathlib.Path.cwd()),
     )
-    template = loader.load(path)
-    renderer.render(template)
+    path = Path(filesystem=DiskFilesystem(directory))
+    service.render(path)
