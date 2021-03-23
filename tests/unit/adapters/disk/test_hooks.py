e5c200d65597491b8be2f81aa40b7ca3c0d797d5
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 11:00:35 +0100

    refactor(domain): Use function instead of class for HookExecutor

--- tests/unit/adapters/disk/test_hooks.py
+++ tests/unit/adapters/disk/test_hooks.py
@@ -3,7 +3,7 @@ import pathlib
 
 import pytest
 
-from cutty.adapters.disk.hooks import DiskHookExecutor
+from cutty.adapters.disk.hooks import executehook
 from cutty.domain.files import File
 from cutty.domain.files import Mode
 from cutty.domain.hooks import Hook
@@ -18,8 +18,7 @@ def test_executor(tmp_path: pathlib.Path, capfd: pytest.CaptureFixture[str]) -> 
     file = File(PurePath("hook.py"), Mode.EXECUTABLE, 'print("hello")\n')
     hook = Hook(file=file, event=PostGenerateProject)
 
-    executor = DiskHookExecutor()
-    executor.execute(hook, Path(filesystem=DiskFilesystem(tmp_path)))
+    executehook(hook, Path(filesystem=DiskFilesystem(tmp_path)))
 
     captured = capfd.readouterr()
     assert captured.out == "hello\n"
