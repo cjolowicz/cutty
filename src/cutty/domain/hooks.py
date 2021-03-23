5c15f3f7599cadd74394986aad799d0e00914483
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 12:03:57 +0100

    feat(domain): Render hooks before execution

--- src/cutty/domain/hooks.py
+++ src/cutty/domain/hooks.py
@@ -1,8 +1,12 @@
 """Hooks."""
 from collections.abc import Callable
+from collections.abc import Sequence
 from dataclasses import dataclass
+from typing import Union
 
+from cutty.domain.bindings import Binding
 from cutty.domain.files import File
+from cutty.domain.render import Renderer
 from cutty.filesystem.path import Path
 from cutty.util.bus import Bus
 from cutty.util.bus import Event
@@ -26,23 +30,36 @@ class PostGenerateProject(Event):
 class Hook:
     """Hook."""
 
-    file: File
+    path: Path
     event: type[Event]
 
 
-HookExecutor = Callable[[Hook, Path], None]
+HookExecutor = Callable[[File, Path], None]
 
 
-def registerhook(hook: Hook, executehook: HookExecutor, bus: Bus) -> None:
+def registerhook(
+    hook: Hook,
+    render: Renderer,
+    bindings: Sequence[Binding],
+    executehook: HookExecutor,
+    bus: Bus,
+) -> None:
     """Subscribe the hook to its event."""
+
+    def runhook(event: Union[PreGenerateProject, PostGenerateProject]) -> None:
+        """Render and execute the hook."""
+        hookfile = File.load(hook.path)
+        hookfile = render(hookfile, bindings)
+        executehook(hookfile, event.project)
+
     if hook.event is PreGenerateProject:
 
         @bus.events.subscribe
         def _(event: PreGenerateProject) -> None:
-            executehook(hook, event.project)
+            runhook(event)
 
     elif hook.event is PostGenerateProject:
 
         @bus.events.subscribe
         def _(event: PostGenerateProject) -> None:
-            executehook(hook, event.project)
+            runhook(event)
