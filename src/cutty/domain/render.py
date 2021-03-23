0b1227a39cc6cb66a3627d94fe8a1bf1b0026e62
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 15:38:45 +0100

    refactor(domain): Spin off module `renderfiles` from `render`

--- src/cutty/domain/render.py
+++ src/cutty/domain/render.py
@@ -1,8 +1,6 @@
 """Rendering."""
 import functools
 from collections.abc import Callable
-from collections.abc import Iterable
-from collections.abc import Iterator
 from collections.abc import Sequence
 from typing import get_type_hints
 from typing import Optional
@@ -14,9 +12,7 @@ from cutty.domain.bindings import Binding
 from cutty.domain.files import File
 from cutty.domain.variables import GenericVariable
 from cutty.domain.variables import Variable
-from cutty.filesystem.path import Path
 from cutty.filesystem.pure import PurePath
-from cutty.util.bus import Bus
 
 
 T = TypeVar("T")
@@ -24,10 +20,6 @@ U = TypeVar("U")
 RenderFunction = Callable[[T, Sequence[Binding]], T]
 RenderContinuation = Callable[[T, Sequence[Binding], RenderFunction[U]], T]
 RenderDecorator = Callable[[RenderContinuation[T, U]], RenderContinuation[T, U]]
-
-
-class InvalidPathComponent(Exception):
-    """The rendered path has an invalid component."""
 
 
 class Renderer:
@@ -141,44 +133,3 @@ class Renderer:
             )
 
         return render
-
-
-def withevents(paths: Iterable[Path], bus: Optional[Bus]) -> Iterator[Path]:
-    """Publish the PreGenerateProject and PostGenerateProject events."""
-    if bus is None:
-        yield from paths
-    else:
-        from cutty.domain.hooks import PreGenerateProject
-        from cutty.domain.hooks import PostGenerateProject
-
-        paths = iter(paths)
-        project = next(paths)
-        bus.events.publish(PreGenerateProject(project))
-
-        yield project
-        yield from paths
-
-        bus.events.publish(PostGenerateProject(project))
-
-
-def renderfiles(
-    paths: Iterable[Path],
-    render: Renderer,
-    bindings: Sequence[Binding],
-    bus: Optional[Bus] = None,
-) -> Iterator[File]:
-    """Render the files."""
-    for path in withevents(paths, bus):
-        name = render(path, bindings).name
-        if not name:
-            continue
-
-        if "/" in name or "\\" in name or name in (".", ".."):
-            raise InvalidPathComponent(str(path), name)
-
-        if path.is_file():
-            yield render(File.load(path), bindings)
-        elif path.is_dir():
-            yield from renderfiles(path.iterdir(), render, bindings)
-        else:  # pragma: no cover
-            raise RuntimeError(f"{path}: not a regular file or directory")
