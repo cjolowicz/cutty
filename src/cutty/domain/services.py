704c69d11e4ddb108ff851d36e1f64dbcbd65967
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 10:11:04 +0100

    refactor(domain): Pass TemplateConfig to loadrenderer

--- src/cutty/domain/services.py
+++ src/cutty/domain/services.py
@@ -1,10 +1,8 @@
 """Domain services."""
 from collections.abc import Callable
 from collections.abc import Iterator
-from collections.abc import Sequence
 
 from cutty.domain.binders import RenderBinder
-from cutty.domain.bindings import Binding
 from cutty.domain.config import TemplateConfig
 from cutty.domain.files import FileStorage
 from cutty.domain.render import Renderer
@@ -14,7 +12,7 @@ from cutty.filesystem.path import Path
 
 TemplateConfigLoader = Callable[[Path], TemplateConfig]
 PathLoader = Callable[[Path, TemplateConfig], Iterator[Path]]
-RendererLoader = Callable[[Path, Sequence[Binding]], Renderer]
+RendererLoader = Callable[[Path, TemplateConfig], Renderer]
 
 
 def render(
@@ -28,7 +26,7 @@ def render(
 ) -> None:
     """Render the template at the given path."""
     config = loadconfig(path)
-    render = loadrenderer(path, config.settings)
+    render = loadrenderer(path, config)
     bindings = renderbind(render, config.variables)
     paths = loadpaths(path, config)
     for file in renderfiles(paths, render, bindings):
