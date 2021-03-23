682233d83ef31f67420a2158f75982b839b4d3ba
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 10:48:55 +0100

    refactor(domain): Pass project path to hooks via event

--- tests/unit/adapters/disk/test_hooks.py
+++ tests/unit/adapters/disk/test_hooks.py
@@ -8,6 +8,8 @@ from cutty.domain.files import File
 from cutty.domain.files import Mode
 from cutty.domain.hooks import Hook
 from cutty.domain.hooks import PostGenerateProject
+from cutty.filesystem.disk import DiskFilesystem
+from cutty.filesystem.path import Path
 from cutty.filesystem.pure import PurePath
 
 
@@ -16,8 +18,8 @@ def test_executor(tmp_path: pathlib.Path, capfd: pytest.CaptureFixture[str]) -> 
     file = File(PurePath("hook.py"), Mode.EXECUTABLE, 'print("hello")\n')
     hook = Hook(file=file, event=PostGenerateProject)
 
-    executor = DiskHookExecutor(cwd=tmp_path)
-    executor.execute(hook)
+    executor = DiskHookExecutor()
+    executor.execute(hook, Path(filesystem=DiskFilesystem(tmp_path)))
 
     captured = capfd.readouterr()
     assert captured.out == "hello\n"
