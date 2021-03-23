4a8d4d9644d3b56c73b30e0bbd8aeb7fa356b933
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 11:04:17 +0100

    refactor(domain): Use function registerhook instead of class HookManager

--- tests/unit/domain/test_hooks.py
+++ tests/unit/domain/test_hooks.py
@@ -4,9 +4,9 @@ import pytest
 from cutty.domain.files import File
 from cutty.domain.files import Mode
 from cutty.domain.hooks import Hook
-from cutty.domain.hooks import HookManager
 from cutty.domain.hooks import PostGenerateProject
 from cutty.domain.hooks import PreGenerateProject
+from cutty.domain.hooks import registerhook
 from cutty.filesystem.dict import DictFilesystem
 from cutty.filesystem.path import Path
 from cutty.filesystem.pure import PurePath
@@ -23,8 +23,8 @@ def test_manager(bus: Bus, eventtype: type[Event]) -> None:
     """It subscribes the hooks."""
     file = File(PurePath(), Mode.DEFAULT, "")
     hook = Hook(file=file, event=eventtype)
-    manager = HookManager([hook], executehook)
-    manager.subscribe(bus)
+
+    registerhook(hook, executehook, bus)
 
     project = Path(filesystem=DictFilesystem({}))
     event = (
