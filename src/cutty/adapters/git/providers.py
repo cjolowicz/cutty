756ffbebb61fd7a02eaf5525d4202f9509eb43cb
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 17:05:30 +0100

    feat(adapters): Derive repository directory from URL

--- src/cutty/adapters/git/providers.py
+++ src/cutty/adapters/git/providers.py
@@ -1,4 +1,7 @@
 """Git-based provider."""
+import pathlib
+from urllib.parse import urlparse
+
 import pygit2
 
 from cutty.adapters.disk.hooks import resolve
@@ -10,6 +13,13 @@ def _createremote(repository: pygit2.Repository, name: str, url: str) -> pygit2.
     return repository.remotes.create(name, url, "+refs/*:refs/*")
 
 
+def _getrepositoryname(url: str) -> str:
+    path = urlparse(url).path
+    name = pathlib.PurePosixPath(path).stem
+    return f"{name}.git"
+
+
 def gitdownload(url: str, path: Path) -> None:
     """Download the URL to the given path."""
-    pygit2.clone_repository(url, resolve(path), bare=True, remote=_createremote)
+    location = resolve(path) / _getrepositoryname(url)
+    pygit2.clone_repository(url, location, bare=True, remote=_createremote)
