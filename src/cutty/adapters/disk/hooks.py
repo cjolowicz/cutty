5d33be6f54ebb644d0d844f4cc239b76d3a41cc6
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 11:00:35 +0100

    refactor(domain): Use function instead of class for HookExecutor

--- src/cutty/adapters/disk/hooks.py
+++ src/cutty/adapters/disk/hooks.py
@@ -8,35 +8,40 @@ from collections.abc import Iterator
 from cutty.adapters.disk.files import DiskFileStorage
 from cutty.compat.contextlib import contextmanager
 from cutty.domain.hooks import Hook
-from cutty.domain.hooks import HookExecutor
 from cutty.filesystem.disk import DiskFilesystem
 from cutty.filesystem.path import Path
 
 
-class DiskHookExecutor(HookExecutor):
+def executehook(hook: Hook, project: Path) -> None:
     """Disk-based hook executor."""
+    with store(hook) as path:
+        run(path, resolve(project))
 
-    def execute(self, hook: Hook, project: Path) -> None:
-        """Execute the hook."""
-        filesystem = project._filesystem
-        assert isinstance(filesystem, DiskFilesystem)  # noqa: S101
-        with self.store(hook) as path:
-            self.run(path, filesystem.resolve(project))
 
-    @contextmanager
-    def store(self, hook: Hook) -> Iterator[pathlib.Path]:
-        """Store the hook on disk."""
-        with DiskFileStorage.temporary() as storage:
-            storage.store(hook.file)
-            yield storage.resolve(hook.file.path)
+def resolve(path: Path) -> pathlib.Path:
+    """Resolve a disk-based path."""
+    # NOTE: This function assumes that the project is written to disk
+    # (precondition). In other words, disk-based hooks _must_ be used with
+    # disk-based project storage.
 
-    def run(self, path: pathlib.Path, project: pathlib.Path) -> None:
-        """Run the hook from disk."""
-        project.mkdir(parents=True, exist_ok=True)
+    filesystem = path._filesystem
+    assert isinstance(filesystem, DiskFilesystem)  # noqa: S101
+    return filesystem.resolve(path)
 
-        command = (
-            [pathlib.Path(sys.executable), path] if path.suffix == ".py" else [path]
-        )
-        shell = platform.system() == "Windows"
 
-        subprocess.run(command, shell=shell, cwd=project, check=True)  # noqa: S602
+@contextmanager
+def store(hook: Hook) -> Iterator[pathlib.Path]:
+    """Store the hook on disk."""
+    with DiskFileStorage.temporary() as storage:
+        storage.store(hook.file)
+        yield storage.resolve(hook.file.path)
+
+
+def run(path: pathlib.Path, project: pathlib.Path) -> None:
+    """Run the hook from disk."""
+    project.mkdir(parents=True, exist_ok=True)
+
+    command = [pathlib.Path(sys.executable), path] if path.suffix == ".py" else [path]
+    shell = platform.system() == "Windows"
+
+    subprocess.run(command, shell=shell, cwd=project, check=True)  # noqa: S602
