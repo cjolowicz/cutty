bc6503f3c96b3c12dd38fda6acea1cbf34958a97
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-23 16:00:02 +0100

    refactor: Import future feature `annotations`

--- src/cutty/util/exceptionhandlers.py
+++ src/cutty/util/exceptionhandlers.py
@@ -97,6 +97,8 @@ associative, this makes exception handlers with composition a simple monoid_.
 
 .. _monoid: https://en.wikipedia.org/wiki/Monoid
 """
+from __future__ import annotations
+
 import contextlib
 from collections.abc import Callable
 from types import TracebackType
