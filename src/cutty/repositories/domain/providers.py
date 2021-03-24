9dd897d57559865aa8f926e8b693e682f7c016d1
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-24 20:21:04 +0100

    refactor(repositories): Store url and path in Provider instance

--- src/cutty/repositories/domain/providers.py
+++ src/cutty/repositories/domain/providers.py
@@ -1,4 +1,6 @@
 """Repository providers."""
+from __future__ import annotations
+
 import abc
 import pathlib
 import shutil
@@ -14,23 +16,29 @@ class Provider(abc.ABC):
 
     name: str
 
+    def __init__(self, url: URL, path: pathlib.Path) -> None:
+        """Initialize."""
+        self.url = url
+        self.path = path
+
+    @classmethod
     @abc.abstractmethod
-    def matches(self, url: URL) -> bool:
+    def matches(cls, url: URL) -> bool:
         """Return True if the provider handles the given URL."""
 
     @abc.abstractmethod
-    def exists(self, url: URL, path: pathlib.Path) -> bool:
+    def exists(self) -> bool:
         """Return True if the repository exists."""
 
     @abc.abstractmethod
-    def download(self, url: URL, path: pathlib.Path) -> None:
+    def download(self) -> None:
         """Download the repository to the given path."""
 
-    def update(self, url: URL, path: pathlib.Path) -> None:
+    def update(self) -> None:
         """Update the repository at the given path."""
-        shutil.rmtree(path)
-        self.download(url, path)
+        shutil.rmtree(self.path)
+        self.download()
 
     @abc.abstractmethod
-    def resolve(self, url: URL, path: pathlib.Path, revision: Optional[str]) -> Path:
+    def resolve(self, revision: Optional[str]) -> Path:
         """Return a filesystem tree for the given revision."""
