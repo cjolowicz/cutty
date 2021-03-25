6b2fb71d5c9edc841bd361b7573d9c3274e3112d
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-26 00:18:01 +0100

    :sparkles: [repositories] Add LocalDirectoryRepository

---
+++ src/cutty/repositories/adapters/directory.py
@@ -0,0 +1,26 @@
+"""Local directory."""
+import pathlib
+from typing import Optional
+
+from cutty.filesystem.adapters.disk import DiskFilesystem
+from cutty.filesystem.domain.path import Path
+from cutty.repositories.domain.repositories import LocalRepository
+
+
+class LocalDirectoryRepository(LocalRepository):
+    """Local directory."""
+
+    type: str = "dir-local"
+
+    @classmethod
+    def supports(cls, path: pathlib.Path) -> bool:
+        """Return True if the implementation supports the given path."""
+        return path.exists()
+
+    def resolve(self, revision: Optional[str]) -> Path:
+        """Return a filesystem tree for the given revision."""
+        if revision is not None:
+            raise RuntimeError(
+                f"{self.type} repository does not support revisions, got {revision}"
+            )
+        return Path(filesystem=DiskFilesystem(self.path))
