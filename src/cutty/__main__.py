2717506f3d154923a04a3c4df292d4493449f877
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-25 12:55:54 +0100

    feat(application): Add option --no-interactive

--- src/cutty/__main__.py
+++ src/cutty/__main__.py
@@ -6,10 +6,16 @@ import cutty.application.cookiecutter.main
 
 @click.command()
 @click.argument("url")
+@click.option(
+    "--interactive / --no-interactive",
+    default=True,
+    show_default=True,
+    help="Prompt for template variables.",
+)
 @click.version_option()
-def main(url: str) -> None:
+def main(url: str, interactive: bool) -> None:
     """cutty."""
-    cutty.application.cookiecutter.main.main(url)
+    cutty.application.cookiecutter.main.main(url, interactive=interactive)
 
 
 if __name__ == "__main__":
