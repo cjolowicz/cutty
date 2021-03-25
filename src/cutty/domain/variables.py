077cbde425e0de230c20cb8253775f1d7dd8e940
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-25 16:17:43 +0100

    feat(domain): Add variables.validate

--- src/cutty/domain/variables.py
+++ src/cutty/domain/variables.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 from typing import Generic
 
+from cutty.domain.values import getvaluetype
 from cutty.domain.values import Value
 from cutty.domain.values import ValueT_co
 from cutty.domain.values import ValueType
@@ -20,3 +21,14 @@ class GenericVariable(Generic[ValueT_co]):
 
 
 Variable = GenericVariable[Value]
+
+
+def validate(value: Value, variable: Variable) -> None:
+    """Raise ValueError if the value is not compatible with the variable."""
+    if getvaluetype(value) != variable.type:
+        raise ValueError(f"{variable.name} has type {variable.type}, got {value!r}")
+
+    if variable.choices and value not in variable.choices:
+        raise ValueError(
+            f"{variable.name} has choices {variable.choices}, got {value!r}"
+        )
