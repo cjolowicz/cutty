74bb2253480f9109989b8a44833073be5ec5b84d
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-03-28 01:59:39 +0100

    :white_check_mark: [templates] Work around mypy "untyped decorator" error

--- tests/unit/templates/domain/test_values.py
+++ tests/unit/templates/domain/test_values.py
@@ -32,42 +32,42 @@ def test_getvaluetype_string(value: str) -> None:
 
 
 @given(value=infer)
-@settings(max_examples=10)
+@settings(max_examples=10)  # type: ignore[misc]
 def test_getvaluetype_array_simple(value: list[Scalar]) -> None:
     """It is ARRAY."""
     assert getvaluetype(value) is ValueType.ARRAY
 
 
 @given(value=infer)
-@settings(max_examples=10)
+@settings(max_examples=10)  # type: ignore[misc]
 def test_getvaluetype_array_nested(value: list[list[Scalar]]) -> None:
     """It is ARRAY."""
     assert getvaluetype(value) is ValueType.ARRAY
 
 
 @given(value=infer)
-@settings(max_examples=10)
+@settings(max_examples=10)  # type: ignore[misc]
 def test_getvaluetype_array_mixed(value: list[dict[str, Scalar]]) -> None:
     """It is ARRAY."""
     assert getvaluetype(value) is ValueType.ARRAY
 
 
 @given(value=infer)
-@settings(max_examples=10)
+@settings(max_examples=10)  # type: ignore[misc]
 def test_getvaluetype_object_simple(value: dict[str, Scalar]) -> None:
     """It is OBJECT."""
     assert getvaluetype(value) is ValueType.OBJECT
 
 
 @given(value=infer)
-@settings(max_examples=10)
+@settings(max_examples=10)  # type: ignore[misc]
 def test_getvaluetype_object_nested(value: dict[str, dict[str, Scalar]]) -> None:
     """It is OBJECT."""
     assert getvaluetype(value) is ValueType.OBJECT
 
 
 @given(value=infer)
-@settings(max_examples=10)
+@settings(max_examples=10)  # type: ignore[misc]
 def test_getvaluetype_object_mixed(value: dict[str, list[Scalar]]) -> None:
     """It is OBJECT."""
     assert getvaluetype(value) is ValueType.OBJECT
