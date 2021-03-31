ba06639d4f87af734b0275e0c18d6e9db3d6c146
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-01 00:23:25 +0200

    :white_check_mark: [templates] Add unit tests for binders module

--- tests/unit/templates/domain/test_binders.py
+++ tests/unit/templates/domain/test_binders.py
@@ -1,6 +1,10 @@
 """Unit tests for cutty.templates.domain.binders."""
+import pytest
+
 from cutty.templates.domain.binders import binddefault
+from cutty.templates.domain.binders import override
 from cutty.templates.domain.binders import renderbindwith
+from cutty.templates.domain.bindings import Binding
 from cutty.templates.domain.render import Renderer
 from cutty.templates.domain.variables import GenericVariable
 
@@ -15,3 +19,21 @@ def test_renderbind_with_binddefault(
 
     assert binding.name == "project"
     assert binding.value == "example"
+
+
+@pytest.mark.parametrize(
+    "given,expected",
+    [
+        (Binding("project", "foobar"), Binding("project", "foobar")),
+        (Binding("unrelated", "foobar"), Binding("project", "example")),
+    ],
+)
+def test_renderbind_with_override(
+    variable: GenericVariable[str], render: Renderer, given: Binding, expected: Binding
+) -> None:
+    """It binds variables using defaults, unless overridden."""
+    renderbind = renderbindwith(override(binddefault, [given]))
+
+    [binding] = renderbind(render, [variable])
+
+    assert binding == expected
