1b1f2a563c151565ff79851317bba10569c6ef85
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-01 00:04:09 +0200

    :white_check_mark: [templates] Add unit tests for render module

--- tests/unit/templates/domain/test_render.py
+++ tests/unit/templates/domain/test_render.py
@@ -3,6 +3,8 @@ import pytest
 
 from cutty.filesystems.domain.purepath import PurePath
 from cutty.templates.domain.bindings import Binding
+from cutty.templates.domain.files import File
+from cutty.templates.domain.files import Mode
 from cutty.templates.domain.render import Renderer
 from cutty.templates.domain.variables import GenericVariable
 
@@ -10,9 +12,14 @@ from cutty.templates.domain.variables import GenericVariable
 @pytest.mark.parametrize(
     "template,expected",
     [
+        (None, None),
         (["green-{x}"], ["green-teapot"]),
         ({"key": "{x}"}, {"key": "teapot"}),
         (PurePath("src", "{x}"), PurePath("src", "teapot")),
+        (
+            File(PurePath("{x}"), Mode.DEFAULT, "{x}"),
+            File(PurePath("teapot"), Mode.DEFAULT, "teapot"),
+        ),
     ],
 )
 def test_render(render: Renderer, template: object, expected: object) -> None:
@@ -25,3 +32,12 @@ def test_render(render: Renderer, template: object, expected: object) -> None:
 def test_render_variable(render: Renderer, variable: GenericVariable[str]) -> None:
     """It renders the variable."""
     assert variable == render(variable, [])
+
+
+def test_render_register_unannotated(render: Renderer) -> None:
+    """It raises an exception if the type is not provided."""
+    with pytest.raises(Exception):
+
+        @render.register
+        def render(value, bindings, render):  # type: ignore[no-untyped-def]
+            ...
