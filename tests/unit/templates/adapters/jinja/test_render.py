1510d73ce42367918db7420c9ed7f75e44adad2b
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-01 12:18:04 +0200

    :white_check_mark: [templates] Add unit tests for Jinja adapter

--- tests/unit/templates/adapters/jinja/test_render.py
+++ tests/unit/templates/adapters/jinja/test_render.py
@@ -1,12 +1,15 @@
 """Unit tests for cutty.templates.adapters.jinja.render."""
 import pathlib
 
+import jinja2
 import pytest
 
+from cutty.filesystems.adapters.dict import DictFilesystem
 from cutty.filesystems.adapters.disk import DiskFilesystem
 from cutty.filesystems.domain.path import Path
 from cutty.templates.adapters.jinja.render import import_object
 from cutty.templates.adapters.jinja.render import JinjaRenderer
+from cutty.templates.adapters.jinja.render import load_extension
 from cutty.templates.domain.bindings import Binding
 from cutty.templates.domain.render import Renderer
 
@@ -24,6 +27,59 @@ from cutty.templates.domain.render import Renderer
 def test_import_object(import_path: str) -> None:
     """It imports the object."""
     assert import_object(import_path)
+
+
+@pytest.mark.parametrize(
+    "import_path",
+    [
+        "jinja2_time.TimeExtension",
+    ],
+)
+def test_load_extension_valid(import_path: str) -> None:
+    """It loads the extension."""
+    extension = load_extension(import_path)
+    assert issubclass(extension, jinja2.ext.Extension)
+
+
+@pytest.mark.parametrize(
+    "import_path",
+    [
+        "json",
+    ],
+)
+def test_load_extension_invalid(import_path: str) -> None:
+    """It raises an exception."""
+    with pytest.raises(Exception):
+        load_extension(import_path)
+
+
+def test_loader_valid(render: Renderer) -> None:
+    """It loads from the filesystem."""
+    binding = Binding("x", "teapot")
+    root = Path(filesystem=DictFilesystem({"template": "{{ x }}"}))
+    render_ = JinjaRenderer.create(searchpath=[root])
+    # Render it twice to trigger Jinja2's caching logic.
+    for _ in range(2):
+        text = render_("{% include 'template' %}", [binding], render)
+    assert text == "teapot"
+
+
+def test_loader_invalid(render: Renderer) -> None:
+    """It raises an exception."""
+    binding = Binding("x", "teapot")
+    root = Path(filesystem=DictFilesystem({"template": "{{ x }}", "dir": {}}))
+    render_ = JinjaRenderer.create(searchpath=[root])
+    with pytest.raises(Exception):
+        render_("{% include 'dir/../template' %}", [binding], render)
+
+
+def test_loader_not_found(render: Renderer) -> None:
+    """It raises an exception."""
+    binding = Binding("x", "teapot")
+    root = Path(filesystem=DictFilesystem({}))
+    render_ = JinjaRenderer.create(searchpath=[root])
+    with pytest.raises(Exception):
+        render_("{% include 'template' %}", [binding], render)
 
 
 @pytest.fixture
