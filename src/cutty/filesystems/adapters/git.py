17f365988d0990f1a8448477ae956a35f3f031fd
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-02 11:45:07 +0200

    :sparkles: [filesystems] Improve error handling in GitFilesystem

--- src/cutty/filesystems/adapters/git.py
+++ src/cutty/filesystems/adapters/git.py
@@ -28,28 +28,39 @@ class GitFilesystem(Filesystem):
     def is_dir(self, path: PurePath) -> bool:
         """Return True if this is a directory."""
         try:
-            return isinstance(self.resolve(path), pygit2.Tree)
+            tree = self.resolve(path)
         except FileNotFoundError:
             return False
+        else:
+            return isinstance(tree, pygit2.Tree)
 
     def iterdir(self, path: PurePath) -> Iterator[PurePath]:
         """Iterate over the files in this directory."""
         tree = self.resolve(path)
-        assert isinstance(tree, pygit2.Tree)  # noqa: S101
+        if not isinstance(tree, pygit2.Tree):
+            raise NotADirectoryError(f"not a directory: {path}")
+
         for entry in tree:
             yield path / entry.name
 
     def is_file(self, path: PurePath) -> bool:
         """Return True if this is a regular file (or a symlink to one)."""
         try:
-            return isinstance(self.resolve(path), pygit2.Blob)
+            blob = self.resolve(path)
         except FileNotFoundError:
             return False
+        else:
+            return isinstance(blob, pygit2.Blob)
 
     def read_text(self, path: PurePath) -> str:
         """Return the contents of this file."""
         blob = self.resolve(path)
-        assert isinstance(blob, pygit2.Blob)  # noqa: S101
+        if isinstance(blob, pygit2.Tree):
+            raise IsADirectoryError(f"is a directory: {path}")
+
+        if not isinstance(blob, pygit2.Blob):  # pragma: no cover
+            raise RuntimeError(f"unexpected object type: {path} ({blob})")
+
         text: str = blob.data.decode()
         return text
 
@@ -59,14 +70,27 @@ class GitFilesystem(Filesystem):
             blob = self.resolve(path)
         except FileNotFoundError:
             return False
-
-        return (
-            isinstance(blob, pygit2.Blob) and blob.filemode == pygit2.GIT_FILEMODE_LINK
-        )
+        else:
+            return (
+                isinstance(blob, pygit2.Blob)
+                and blob.filemode == pygit2.GIT_FILEMODE_LINK
+            )
 
     def readlink(self, path: PurePath) -> PurePath:
         """Return the target of a symbolic link."""
-        target = self.read_text(path)
+        blob = self.resolve(path)
+        if isinstance(blob, pygit2.Tree):
+            raise IsADirectoryError(f"is a directory: {path}")
+
+        if not (
+            isinstance(blob, pygit2.Blob) and blob.filemode == pygit2.GIT_FILEMODE_LINK
+        ):
+            raise RuntimeError(f"not a symlink: {path}")
+
+        target: str = blob.data.decode(errors="surrogateescape")
+
+        # TODO: Parse the path.
+        # TODO: Handle absolute paths in some way.
         return PurePath(*target.split("/"))
 
     def access(self, path: PurePath, mode: Access) -> bool:
