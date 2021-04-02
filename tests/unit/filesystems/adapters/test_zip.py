55b794506317e77bc22cc3a91fd5e9b6d509f629
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-02 17:35:18 +0200

    :white_check_mark: [filesystems] Add unit tests for ZipFilesystem

---
+++ tests/unit/filesystems/adapters/test_zip.py
@@ -0,0 +1,140 @@
+"""Unit tests for cutty.filesystems.adapters.zip."""
+import shutil
+from pathlib import Path
+
+import pytest
+
+from cutty.filesystems.adapters.zip import ZipFilesystem
+from cutty.filesystems.domain.filesystem import Access
+from cutty.filesystems.domain.purepath import PurePath
+
+
+@pytest.fixture
+def filesystem(tmp_path: Path) -> ZipFilesystem:
+    """Fixture for a zip filesystem."""
+    path = tmp_path / "archive"
+    path.mkdir()
+
+    (path / "file").write_text("lorem ipsum dolor\n")
+    (path / "dir").mkdir()
+    (path / "dir" / "script.py").write_text("#!/usr/bin/env python\n")
+    (path / "dir" / "script.py").chmod(0o755)
+    (path / "dir" / "subdir").mkdir()
+    (path / "dir" / "subdir" / ".keep").touch()
+
+    shutil.make_archive(str(path), "zip", str(path))
+
+    return ZipFilesystem(path.with_suffix(".zip"))
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        PurePath(),
+        PurePath("dir"),
+        PurePath("dir", "subdir"),
+        PurePath("."),
+        PurePath(".."),
+        PurePath("..", ".."),
+        PurePath("dir", "."),
+        PurePath("dir", ".."),
+        PurePath(".", "dir"),
+        PurePath("..", "dir"),
+        PurePath("dir", ".", "subdir"),
+        PurePath("dir", "subdir", ".."),
+    ],
+    ids=str,
+)
+def test_is_dir_true(filesystem: ZipFilesystem, path: PurePath) -> None:
+    """It returns True."""
+    assert filesystem.is_dir(path)
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        PurePath("file"),
+        PurePath("dir", "script.py"),
+        PurePath("dir", "subdir", ".keep"),
+    ],
+    ids=str,
+)
+def test_is_dir_false(filesystem: ZipFilesystem, path: PurePath) -> None:
+    """It returns False."""
+    assert not filesystem.is_dir(path)
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        PurePath("file"),
+        PurePath("dir", "script.py"),
+        PurePath("dir", "subdir", ".keep"),
+        PurePath(".", "file"),
+        PurePath("dir", "..", "file"),
+        PurePath("dir", "script.py"),
+        PurePath("dir", "subdir", ".keep"),
+    ],
+    ids=str,
+)
+def test_is_file_true(filesystem: ZipFilesystem, path: PurePath) -> None:
+    """It returns True."""
+    assert filesystem.is_file(path)
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        PurePath(),
+        PurePath("dir"),
+        PurePath("dir", "subdir"),
+        PurePath("."),
+        PurePath(".."),
+        PurePath("..", ".."),
+        PurePath("dir", "."),
+        PurePath("dir", ".."),
+        PurePath(".", "dir"),
+        PurePath("..", "dir"),
+        PurePath("dir", ".", "subdir"),
+        PurePath("dir", "subdir", ".."),
+        PurePath("no such file"),
+        PurePath("dir", "no such file"),
+    ],
+    ids=str,
+)
+def test_is_file_false(filesystem: ZipFilesystem, path: PurePath) -> None:
+    """It returns False."""
+    assert not filesystem.is_file(path)
+
+
+def test_read_text(filesystem: ZipFilesystem) -> None:
+    """It returns the file contents."""
+    assert filesystem.read_text(PurePath("file")) == "lorem ipsum dolor\n"
+
+
+def test_readlink(filesystem: ZipFilesystem) -> None:
+    """It raises an exception."""
+    with pytest.raises(Exception):
+        filesystem.readlink(PurePath("file"))
+
+
+@pytest.mark.parametrize(
+    "path,entries",
+    [
+        (PurePath(), {"dir", "file"}),
+        (PurePath("."), {"dir", "file"}),
+        (PurePath(".."), {"dir", "file"}),
+        (PurePath("dir", ".."), {"dir", "file"}),
+        (PurePath("dir"), {"subdir", "script.py"}),
+        (PurePath("dir", "subdir"), {".keep"}),
+    ],
+    ids=str,
+)
+def test_iterdir(filesystem: ZipFilesystem, path: PurePath, entries: set[str]) -> None:
+    """It iterates over the directory entries."""
+    assert set(entry.name for entry in filesystem.iterdir(path)) == entries
+
+
+def test_access(filesystem: ZipFilesystem) -> None:
+    """It returns True if the file can be accessed."""
+    assert filesystem.access(PurePath("dir", "script.py"), Access.EXECUTE)
