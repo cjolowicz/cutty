24f0f9e3adfa232e54223eb96c1e7ee9ca584b23
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-09 13:10:27 +0200

    :sparkles: [repositories2] Add function parseurl in domain.urls

--- src/cutty/repositories2/domain/urls.py
+++ src/cutty/repositories2/domain/urls.py
@@ -7,3 +7,9 @@ from yarl import URL
 def asurl(path: pathlib.Path) -> URL:
     """Convert filesystem path to URL."""
     return URL(path.resolve().as_uri())
+
+
+def parseurl(location: str) -> URL:
+    """Construct a URL from a string."""
+    path = pathlib.Path(location)
+    return asurl(path) if path.exists() else URL(location)
