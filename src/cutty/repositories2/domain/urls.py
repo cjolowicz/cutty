739533cb458f6e5cdfb4e5704bbec850a81b83ef
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-09 21:16:38 +0200

    :sparkles: [repositories2] Add function aspath in domain.urls

--- src/cutty/repositories2/domain/urls.py
+++ src/cutty/repositories2/domain/urls.py
@@ -1,7 +1,26 @@
 """Utilities for URLs and paths."""
 import pathlib
+import platform
 
 from yarl import URL
+
+
+def aspath(url: URL) -> pathlib.Path:
+    """Convert URL to filesystem path."""
+    if any(
+        (
+            url.scheme and url.scheme != "file",
+            url.host and platform.system() != "Windows",
+            url.user,
+            url.password,
+            url.port,
+            url.query_string,
+            url.fragment,
+        )
+    ):
+        raise ValueError(f"not a path: {url}")
+
+    return pathlib.Path(f"//{url.host}{url.path}" if url.host else url.path)
 
 
 def asurl(path: pathlib.Path) -> URL:
