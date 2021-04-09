cc2aa9bf90ea2c4ecd47259db0dac85d69116301
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-09 11:51:06 +0200

    :sparkles: [repositories2] Add function asurl in domain.urls

---
+++ src/cutty/repositories2/domain/urls.py
@@ -0,0 +1,9 @@
+"""Utilities for URLs and paths."""
+import pathlib
+
+from yarl import URL
+
+
+def asurl(path: pathlib.Path) -> URL:
+    """Convert filesystem path to URL."""
+    return URL(path.as_uri())
