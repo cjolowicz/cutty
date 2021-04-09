a988cde1043bc3e7cbca93710a4e97745704ec72
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-09 23:04:24 +0200

    :white_check_mark: [repositories2] Add tests for scheme in domain.matchers

---
+++ tests/unit/repositories2/domain/test_matchers.py
@@ -0,0 +1,31 @@
+"""Unit tests for cutty.repositories2.domain.matchers."""
+import pytest
+from yarl import URL
+
+from cutty.repositories2.domain.matchers import scheme
+
+
+@pytest.mark.parametrize(
+    ("schemes", "url"),
+    [
+        (("https", "http"), URL("https://example.com/")),
+        (("file",), URL("/home/user/repository")),
+    ],
+)
+def test_scheme_pass(url: URL, schemes: tuple[str]) -> None:
+    """It matches the URL."""
+    match = scheme(*schemes)
+    assert match(url)
+
+
+@pytest.mark.parametrize(
+    ("schemes", "url"),
+    [
+        (("http", "ftp"), URL("https://example.com/")),
+        (("https", "http", "ftp"), URL("/home/user/repository")),
+    ],
+)
+def test_scheme_fail(url: URL, schemes: tuple[str]) -> None:
+    """It does not match the URL."""
+    match = scheme(*schemes)
+    assert not match(url)
