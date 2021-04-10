36a00b2e900b7ae9970ce8f91ebd2aecec9acc03
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-10 22:52:02 +0200

    :recycle: [repositories2] Move store fixtures to conftest.py

--- tests/unit/repositories2/conftest.py
+++ tests/unit/repositories2/conftest.py
@@ -1,9 +1,32 @@
 """Fixtures for testing cutty.repositories2."""
+import pathlib
+
 import pytest
 from yarl import URL
+
+from cutty.repositories2.domain.providers import ProviderStore
+from cutty.repositories2.domain.stores import Store
 
 
 @pytest.fixture
 def url() -> URL:
     """Fixture for a URL."""
     return URL("https://example.com/repository")
+
+
+@pytest.fixture
+def providerstore(tmp_path: pathlib.Path) -> ProviderStore:
+    """Fixture for a simple provider store."""
+    path = tmp_path / "providerstore"
+    path.mkdir()
+
+    def _providerstore(providername: str):
+        return lambda url: path
+
+    return _providerstore
+
+
+@pytest.fixture
+def store(providerstore: ProviderStore) -> Store:
+    """Fixture for a simple store."""
+    return providerstore("default")
