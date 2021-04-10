e96b70443ad00246322671dd4e23a3eed4a35f09
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-11 01:21:06 +0200

    :beetle: [repositories2] Do not unlink destination when HTTP fails

--- src/cutty/repositories2/adapters/fetchers/http.py
+++ src/cutty/repositories2/adapters/fetchers/http.py
@@ -13,11 +13,9 @@ from cutty.repositories2.domain.revisions import Revision
 @fetcher(match=scheme("http", "https"))
 def httpfetcher(url: URL, destination: Path, revision: Optional[Revision]) -> None:
     """Fetch via HTTP."""
-    if destination.exists():
-        destination.unlink()
-
     with httpx.stream("GET", str(url)) as response:
         response.raise_for_status()
+
         with destination.open(mode="wb") as io:
             for data in response.iter_bytes():
                 io.write(data)
