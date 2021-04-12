a50c0abf47479fb7909ced726d1a0722fa543e31
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-12 22:52:12 +0200

    :white_check_mark: [repositories2] Add tests for provider registry

---
+++ tests/unit/repositories2/adapters/test_registry.py
@@ -0,0 +1,27 @@
+"""Unit tests for cutty.repositories2.adapters.registry."""
+from yarl import URL
+
+from cutty.repositories2.adapters.registry import defaultproviderregistry
+from cutty.repositories2.domain.fetchers import FetchMode
+from cutty.repositories2.domain.providers import ProviderStore
+
+
+def test_defaultproviderregistry_non_empty():
+    """It is not empty."""
+    assert defaultproviderregistry
+
+
+def test_defaultproviderregistry_strings():
+    """Its keys are strings."""
+    assert all(
+        isinstance(providername, str) for providername in defaultproviderregistry
+    )
+
+
+def test_defaultproviderregistry_providerfactories(store: ProviderStore):
+    """Its values are provider factories."""
+    url = URL("mailto:you@example.com")
+    for providerfactory in defaultproviderregistry.values():
+        provider = providerfactory(store, FetchMode)
+        filesystem = provider(url, revision=None)
+        assert filesystem is None
