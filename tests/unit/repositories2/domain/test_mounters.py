5054f4ae3ea89afca653e0f151cbf45e4a1a49e7
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-13 00:18:07 +0200

    :label: [repositories2] Fix mounter invocation with keyword argument

--- tests/unit/repositories2/domain/test_mounters.py
+++ tests/unit/repositories2/domain/test_mounters.py
@@ -10,7 +10,7 @@ from cutty.repositories2.domain.mounters import unversioned_mounter
 def test_unversioned_mounter_pass(tmp_path: Path) -> None:
     """It mounts the filesystem."""
     mount = unversioned_mounter(DiskFilesystem)
-    filesystem = mount(tmp_path, revision=None)
+    filesystem = mount(tmp_path, None)
     assert isinstance(filesystem, DiskFilesystem)
 
 
@@ -18,4 +18,4 @@ def test_unversioned_mounter_fail(tmp_path: Path) -> None:
     """It raises an exception."""
     mount = unversioned_mounter(DiskFilesystem)
     with pytest.raises(Exception):
-        mount(tmp_path, revision="v1.0.0")
+        mount(tmp_path, "v1.0.0")
