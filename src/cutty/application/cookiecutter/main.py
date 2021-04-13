7747a2b365b90e79d5e2c99390e48a7799b46567
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-13 11:58:08 +0200

    :building_construction: [cookiecutter] Move loadrenderer to module `render`

--- src/cutty/application/cookiecutter/main.py
+++ src/cutty/application/cookiecutter/main.py
@@ -11,8 +11,8 @@ import appdirs
 from cutty.application.cookiecutter.files import CookiecutterFileStorage
 from cutty.application.cookiecutter.loader import loadconfig
 from cutty.application.cookiecutter.loader import loadpaths
-from cutty.application.cookiecutter.loader import loadrenderer
 from cutty.application.cookiecutter.prompts import prompt
+from cutty.application.cookiecutter.render import loadrenderer
 from cutty.repositories.adapters.registry import defaultproviderregistry
 from cutty.repositories.adapters.storage import asproviderstore
 from cutty.repositories.adapters.storage import RepositoryStorage
