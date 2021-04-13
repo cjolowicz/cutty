1032e123cf8ae138fb2e9331ee329d4e6cb08264
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-13 14:36:56 +0200

    :building_construction: [templates] Move DiskFileStorage to Cookiecutter component

--- src/cutty/application/cookiecutter/files.py
+++ src/cutty/application/cookiecutter/files.py
@@ -12,8 +12,34 @@ from collections.abc import Mapping
 from typing import Optional
 
 from cutty.compat.contextlib import contextmanager
-from cutty.templates.adapters.diskfilestorage import DiskFileStorage
+from cutty.filesystems.domain.purepath import PurePath
 from cutty.templates.domain.files import File
+from cutty.templates.domain.files import Mode
+
+
+class DiskFileStorage:
+    """Disk-based store for files."""
+
+    def __init__(self, root: pathlib.Path) -> None:
+        """Initialize."""
+        self.root = root
+
+    def store(self, files: Iterable[File]) -> None:
+        """Commit the files to storage."""
+        for file in files:
+            path = self.resolve(file.path)
+            self.storefile(file, path)
+
+    def storefile(self, file: File, path: pathlib.Path) -> None:
+        """Commit a file to storage."""
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.write_text(file.blob)
+        if file.mode & Mode.EXECUTABLE:
+            path.chmod(path.stat().st_mode | 0o111)
+
+    def resolve(self, path: PurePath) -> pathlib.Path:
+        """Resolve the path to a filesystem location."""
+        return self.root.joinpath(*path.parts)
 
 
 class CookiecutterFileStorage(DiskFileStorage):
