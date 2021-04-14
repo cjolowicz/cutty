58551d2aba1b53a6be8ee1a1a4bf8fb3e9e440a5
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-14 03:11:52 +0200

    :green_heart: [repositories] Provide HGUSER when testing on Windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,6 +47,12 @@ def template_directory(tmp_path: pathlib.Path) -> pathlib.Path:
     return tmp_path
 
 
+@pytest.fixture(autouse=platform.system() == "Windows")
+def set_hg_user(monkeypatch: pytest.MonkeyPatch) -> None:
+    """Provide author information to ``hg commit``."""
+    monkeypatch.setenv("HGUSER", "you@example.com")
+
+
 @pytest.fixture(autouse=True)
 def set_user_cache_dir(monkeypatch: pytest.MonkeyPatch, tmp_path: pathlib.Path) -> None:
     """Replace the user cache directory by a temporary directory."""
