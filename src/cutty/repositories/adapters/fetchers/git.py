2823dbf469eff673cfd309beeb9f3bd3ca8da388
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-17 14:28:05 +0200

    :beetle: [repositories2] Work around libgit2 issue resulting in broken HEAD

--- src/cutty/repositories/adapters/fetchers/git.py
+++ src/cutty/repositories/adapters/fetchers/git.py
@@ -1,4 +1,5 @@
 """Fetcher for git repositories."""
+import contextlib
 import pathlib
 from typing import Optional
 
@@ -9,6 +10,27 @@ from cutty.repositories.domain.fetchers import fetcher
 from cutty.repositories.domain.matchers import scheme
 from cutty.repositories.domain.revisions import Revision
 from cutty.repositories.domain.stores import defaultstore
+
+
+def _fix_repository_head(repository: pygit2.Repository) -> pygit2.Reference:
+    """Work around a bug in libgit2 resulting in a bogus HEAD reference.
+
+    Cloning with a remote callback results in HEAD pointing to the user's
+    `init.defaultBranch` instead of the default branch of the cloned repository.
+    """
+    # https://github.com/libgit2/pygit2/issues/1073
+    head = repository.references["HEAD"]
+
+    with contextlib.suppress(KeyError):
+        return head.resolve()
+
+    for branch in ["main", "master"]:
+        ref = f"refs/heads/{branch}"
+        if ref in repository.references:
+            head.set_target(ref, message="repair broken HEAD after clone")
+            break
+
+    return head.resolve()
 
 
 @fetcher(
@@ -32,6 +54,8 @@ def gitfetcher(
         for remote in repository.remotes:
             remote.fetch(prune=pygit2.GIT_FETCH_PRUNE)
     else:
-        pygit2.clone_repository(
-            str(url), str(destination), bare=True, remote=_createremote
+        _fix_repository_head(
+            pygit2.clone_repository(
+                str(url), str(destination), bare=True, remote=_createremote
+            )
         )
