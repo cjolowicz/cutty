71c1f8f52aa6cb9a723547bb4e1ba6498e886f52
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-21 09:21:07 +0200

    :art: [templates] Rename Render{ => ing}Binder

--- src/cutty/templates/domain/services.py
+++ src/cutty/templates/domain/services.py
@@ -3,7 +3,7 @@ from collections.abc import Callable
 from collections.abc import Iterator
 
 from cutty.filesystems.domain.path import Path
-from cutty.templates.domain.binders import RenderBinder
+from cutty.templates.domain.binders import RenderingBinder
 from cutty.templates.domain.config import Config
 from cutty.templates.domain.files import File
 from cutty.templates.domain.render import createrenderer
@@ -20,7 +20,7 @@ RendererRegistrar = Callable[[Path, Config], RenderRegistry]
 def render(
     path: Path,
     *,
-    renderbind: RenderBinder,
+    renderbind: RenderingBinder,
     loadconfig: ConfigLoader,
     registerrenderers: RendererRegistrar,
     loadpaths: PathLoader,
