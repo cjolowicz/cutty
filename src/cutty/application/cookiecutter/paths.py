19feb7d21be00be22cb23b6459176e1f22991cd1
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-05-04 16:05:11 +0200

    :recycle: [cookiecutter] Move iterhooks to hooks module

--- src/cutty/application/cookiecutter/paths.py
+++ src/cutty/application/cookiecutter/paths.py
@@ -1,19 +1,9 @@
 """Paths in Cookiecutter templates."""
 from collections.abc import Iterator
 
+from cutty.application.cookiecutter.hooks import iterhooks
 from cutty.filesystems.domain.path import Path
 from cutty.templates.domain.config import Config
-
-
-def iterhooks(path: Path) -> Iterator[Path]:
-    """Load hooks in a Cookiecutter template."""
-    hookdir = path / "hooks"
-    hooks = {"pre_gen_project", "post_gen_project"}
-
-    if hookdir.is_dir():
-        for path in hookdir.iterdir():
-            if path.is_file() and not path.name.endswith("~") and path.stem in hooks:
-                yield path
 
 
 def iterpaths(path: Path, config: Config) -> Iterator[Path]:
