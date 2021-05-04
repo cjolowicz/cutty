00fb6f7ddc1200b1ab2e4bbd0eab2bf102148df0
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-05-04 23:07:40 +0200

    :sparkles: [filestorage] Add loadfile function

--- src/cutty/filestorage/domain/files.py
+++ src/cutty/filestorage/domain/files.py
@@ -1,6 +1,8 @@
 """Files."""
 from dataclasses import dataclass
 
+from cutty.filesystems.domain.filesystem import Access
+from cutty.filesystems.domain.path import Path
 from cutty.filesystems.domain.purepath import PurePath
 
 
@@ -28,3 +30,29 @@ class SymbolicLink(File):
     """A symbolic link."""
 
     target: PurePath
+
+
+def loadfile(
+    path: Path,
+    *,
+    follow_symlinks: bool = True,
+) -> File:
+    """Load file from path."""
+    if not follow_symlinks and path.is_symlink():
+        target = path.readlink()
+        return SymbolicLink(path, target)
+
+    if path.is_file():
+        cls = Executable if path.access(Access.EXECUTE) else RegularFile
+        return cls(path, path.read_bytes())
+
+    message = (
+        "broken symlink"
+        if path.is_symlink()
+        else "directory"
+        if path.is_dir()
+        else "special file"
+        if path.access()
+        else "no such file"
+    )
+    raise RuntimeError(f"{path}: {message}")
