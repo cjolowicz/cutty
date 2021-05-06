c200b77cf28a6c1d5d5ee9621d9e263f5769b7d1
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-05-06 21:03:05 +0200

    :sparkles: [filestorage] Implement rollback on error in disk adapter

--- src/cutty/filestorage/adapters/disk.py
+++ src/cutty/filestorage/adapters/disk.py
@@ -5,6 +5,7 @@ import enum
 import functools
 import pathlib
 import tempfile
+from collections.abc import Callable
 from collections.abc import Iterator
 
 from cutty.compat.contextlib import contextmanager
@@ -23,14 +24,16 @@ class FileExistsPolicy(enum.Enum):
     SKIP = enum.auto()
 
 
-def storefile(
+def storefile(  # noqa: C901
     file: File,
     *,
     root: pathlib.Path,
     fileexists: FileExistsPolicy,
+    undo: list[Callable[[], None]],
 ) -> None:
     """Store the file in a directory on disk."""
     path = root.joinpath(*file.path.parts)
+    overwrite = False
 
     if path.exists():
         if fileexists is FileExistsPolicy.RAISE:
@@ -39,16 +42,31 @@ def storefile(
         if fileexists is FileExistsPolicy.SKIP:
             return
 
-    path.parent.mkdir(parents=True, exist_ok=True)
+        overwrite = True
+
+    for parent in reversed(path.parents):
+        # A symbolic link in the path whose target is not a directory will
+        # result in FileExistsError. XXX Should we guard against symbolic links
+        # whose target is a directory here?
+        if not parent.is_dir():
+            parent.mkdir()
+            undo.append(parent.rmdir)
 
     if isinstance(file, RegularFile):
         path.write_bytes(file.blob)
+
+        if not overwrite:
+            undo.append(path.unlink)
+
         if isinstance(file, Executable):
             path.chmod(path.stat().st_mode | 0o111)
 
     elif isinstance(file, SymbolicLink):
         target = pathlib.Path(*file.target.parts)
         path.symlink_to(target)
+
+        if not overwrite:
+            undo.append(path.unlink)
 
     else:
         raise TypeError(f"cannot store file of type {type(file)}")
@@ -61,7 +79,14 @@ def diskfilestorage(
     fileexists: FileExistsPolicy = FileExistsPolicy.RAISE,
 ) -> Iterator[FileStore]:
     """Disk-based store for files."""
-    yield functools.partial(storefile, root=root, fileexists=fileexists)
+    undo: list[Callable[[], None]] = []
+
+    try:
+        yield functools.partial(storefile, root=root, fileexists=fileexists, undo=undo)
+    except BaseException:
+        for action in reversed(undo):
+            action()  # if this fails then so be it
+        raise
 
 
 @contextmanager
