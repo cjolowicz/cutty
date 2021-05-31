4c4832402d1da2c9af3f21ebdb3b8de08b5db2e3
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-05-31 22:58:51 +0200

    :white_check_mark: [repositories] Add unit tests for hooks

--- tests/unit/repositories/adapters/test_hooks.py
+++ tests/unit/repositories/adapters/test_hooks.py
@@ -1,8 +1,10 @@
 """Unit tests for cutty.repositories.adapters.hooks."""
+import pytest
 from yarl import URL
 
 from cutty.plugins.adapters.fake import FakeRegistry
 from cutty.repositories.adapters.hooks import getproviderstore
+from cutty.repositories.adapters.hooks import getrepositoryprovider
 
 
 def test_getrepositorystorage() -> None:
@@ -12,3 +14,12 @@ def test_getrepositorystorage() -> None:
     providerstore = getproviderstore(registry, "cutty")
     store = providerstore("git")
     assert store(url) == store(url)
+
+
+def test_getrepositoryprovider() -> None:
+    """It raises an exception for an invalid scheme."""
+    registry = FakeRegistry()
+    repositoryprovider = getrepositoryprovider(registry, "cutty")
+    url = URL("invalid-scheme://example.com/repository")
+    with pytest.raises(Exception):
+        repositoryprovider(url)
