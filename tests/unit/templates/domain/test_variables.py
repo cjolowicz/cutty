aec49b5471604a797d58ec5da24b0ca0f68621fe
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-03 15:37:39 +0200

    :white_check_mark: [templates] Do not use Hypothesis in variable tests

--- tests/unit/templates/domain/test_variables.py
+++ tests/unit/templates/domain/test_variables.py
@@ -1,62 +1,63 @@
 """Unit tests for cutty.templates.domain.variables."""
-from typing import Any
-
 import pytest
-from hypothesis import assume
-from hypothesis import given
-from hypothesis import strategies
 
 from cutty.templates.domain.variables import validate
 from cutty.templates.domain.variables import Variable
-from tests.unit.templates.domain.strategies import values
-from tests.unit.templates.domain.strategies import variables
 
 
-@given(
-    value=values(valuetype=int),
-    variable=variables(valuetype=int, choices=()),
-)
-def test_validate_type_valid(value: Any, variable: Variable) -> None:
+def test_validate_type_valid() -> None:
     """It does not raise if the value has the expected type."""
+    variable = Variable(
+        name="name",
+        description="description",
+        type=int,
+        default=0,
+        choices=(),
+        interactive=True,
+    )
+    value = 1
     validate(value, variable)
 
 
-@given(
-    value=values(valuetype=str),
-    variable=variables(valuetype=int, choices=()),
-)
-def test_validate_type_invalid(value: Any, variable: Variable) -> None:
+def test_validate_type_invalid() -> None:
     """It raises if the value does not have the expected type."""
+    variable = Variable(
+        name="name",
+        description="description",
+        type=int,
+        default=0,
+        choices=(),
+        interactive=True,
+    )
+    value = ""
     with pytest.raises(ValueError):
         validate(value, variable)
 
 
-@given(
-    value=values(valuetype=int),
-    variable=variables(valuetype=int, choices=()),
-)
-def test_validate_choices_empty(value: Any, variable: Variable) -> None:
-    """It does not raise if the variable does not specify choices."""
-    validate(value, variable)
-
-
-@given(variable=variables(), data=strategies.data())
-def test_validate_choice_valid(
-    variable: Variable,
-    data: strategies.DataObject,
-) -> None:
+def test_validate_choice_valid() -> None:
     """It does not raise if the value is contained in the choices."""
-    assume(variable.choices)
-    value = data.draw(strategies.sampled_from(variable.choices))
+    variable = Variable(
+        name="name",
+        description="description",
+        type=int,
+        default=0,
+        choices=(0, 1, 2),
+        interactive=True,
+    )
+    value = 1
     validate(value, variable)
 
 
-@given(
-    value=values(valuetype=int),
-    variable=variables(valuetype=int),
-)
-def test_validate_choice_invalid(value: Any, variable: Variable) -> None:
+def test_validate_choice_invalid() -> None:
     """It raises if the value is not contained in the choices."""
-    assume(variable.choices and value not in variable.choices)
+    variable = Variable(
+        name="name",
+        description="description",
+        type=int,
+        default=0,
+        choices=(0, 1, 2),
+        interactive=True,
+    )
+    value = 3
     with pytest.raises(ValueError):
         validate(value, variable)
