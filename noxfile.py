f04e47b299b5ed5cd25d4d4da2cc61e20b916d41
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-11 10:37:19 +0200

    :construction_worker: [nox] Install types-python-slugify in mypy session

--- noxfile.py
+++ noxfile.py
@@ -114,7 +114,7 @@ def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or ["src", "tests", "docs/conf.py"]
     session.install(".")
-    session.install("mypy", *dependencies["test"])
+    session.install("mypy", *dependencies["test"], "types-python-slugify")
     session.run("mypy", *args)
     if not session.posargs:
         session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
