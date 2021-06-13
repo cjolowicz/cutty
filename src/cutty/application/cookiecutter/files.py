b33acb886efbca051a60a08e11eb1718596a7379
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-13 21:38:32 +0200

    :fire: [cookiecutter] Remove class DiskFileStorage

--- src/cutty/application/cookiecutter/files.py
+++ src/cutty/application/cookiecutter/files.py
@@ -11,31 +11,6 @@ from cutty.templates.domain.files import File
 from cutty.templates.domain.files import Mode
 
 
-class DiskFileStorage:
-    """Disk-based store for files."""
-
-    def __init__(self, root: pathlib.Path) -> None:
-        """Initialize."""
-        self.root = root
-
-    def store(self, files: Iterable[File]) -> None:
-        """Commit the files to storage."""
-        for file in files:
-            path = self.resolve(file.path)
-            self.storefile(file, path)
-
-    def storefile(self, file: File, path: pathlib.Path) -> None:
-        """Commit a file to storage."""
-        path.parent.mkdir(parents=True, exist_ok=True)
-        path.write_bytes(file.blob)
-        if file.mode & Mode.EXECUTABLE:
-            path.chmod(path.stat().st_mode | 0o111)
-
-    def resolve(self, path: PurePath) -> pathlib.Path:
-        """Resolve the path to a filesystem location."""
-        return self.root.joinpath(*path.parts)
-
-
 def _convert_file_representation(file: File) -> BaseFile:
     if Mode.EXECUTABLE in file.mode:
         return Executable(file.path, file.blob)
