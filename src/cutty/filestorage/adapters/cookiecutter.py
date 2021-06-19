7f04dc5ae8fd77bf46bc5a22f6760a03d56ab390
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-19 21:55:25 +0200

    :recycle: [filestorage] Add Cookiecutter-style disk storage

--- src/cutty/filestorage/adapters/cookiecutter.py
+++ src/cutty/filestorage/adapters/cookiecutter.py
@@ -9,7 +9,7 @@ from collections.abc import Iterator
 from typing import Optional
 
 from cutty.compat.contextlib import contextmanager
-from cutty.filestorage.adapters.disk import diskfilestorage
+from cutty.filestorage.adapters.disk import DiskFileStorage
 from cutty.filestorage.adapters.disk import FileExistsPolicy
 from cutty.filestorage.adapters.disk import temporarydiskfilestorage
 from cutty.filestorage.domain.files import File
@@ -31,6 +31,47 @@ def _runhook(hooks: dict[str, File], hook: str, *, cwd: pathlib.Path) -> None:
             storefile(hookfile)
 
 
+class CookiecutterFileStorage(DiskFileStorage):
+    """Disk-based file store with Cookiecutter hooks."""
+
+    def __init__(
+        self,
+        root: pathlib.Path,
+        *,
+        hookfiles: Iterable[File] = (),
+        overwrite_if_exists: bool = False,
+        skip_if_file_exists: bool = False,
+    ):
+        """Initialize."""
+        fileexists = (
+            FileExistsPolicy.RAISE
+            if not overwrite_if_exists
+            else FileExistsPolicy.SKIP
+            if skip_if_file_exists
+            else FileExistsPolicy.OVERWRITE
+        )
+        super().__init__(root, fileexists=fileexists)
+        self.hooks = {hook.path.stem: hook for hook in hookfiles}
+        self.project: Optional[pathlib.Path] = None
+        self.overwrite_if_exists = overwrite_if_exists
+
+    def add(self, file: File) -> None:
+        """Add file to storage."""
+        if self.project is None:
+            self.project = self.root / file.path.parts[0]
+            self.project.mkdir(
+                parents=True, exist_ok=self.fileexists is not FileExistsPolicy.RAISE
+            )
+            _runhook(self.hooks, "pre_gen_project", cwd=self.project)
+
+        super().add(file)
+
+    def commit(self) -> None:
+        """Commit the stores."""
+        if self.project is not None:
+            _runhook(self.hooks, "post_gen_project", cwd=self.project)
+
+
 @contextmanager
 def cookiecutterfilestorage(
     root: pathlib.Path,
@@ -40,29 +81,10 @@ def cookiecutterfilestorage(
     skip_if_file_exists: bool = False,
 ) -> Iterator[FileStore]:
     """File store."""
-    project: Optional[pathlib.Path] = None
-    hooks = {hook.path.stem: hook for hook in hookfiles}
-    fileexists = (
-        FileExistsPolicy.RAISE
-        if not overwrite_if_exists
-        else FileExistsPolicy.SKIP
-        if skip_if_file_exists
-        else FileExistsPolicy.OVERWRITE
-    )
-
-    with diskfilestorage(root, fileexists=fileexists) as storefile:
-
-        def _storefile(file: File) -> None:
-            nonlocal project
-
-            if project is None:
-                project = root / file.path.parts[0]
-                project.mkdir(parents=True, exist_ok=overwrite_if_exists)
-                _runhook(hooks, "pre_gen_project", cwd=project)
-
-            return storefile(file)
-
-        yield _storefile
-
-        if project is not None:
-            _runhook(hooks, "post_gen_project", cwd=project)
+    with CookiecutterFileStorage(
+        root,
+        hookfiles=hookfiles,
+        overwrite_if_exists=overwrite_if_exists,
+        skip_if_file_exists=skip_if_file_exists,
+    ) as storage:
+        yield storage.add
