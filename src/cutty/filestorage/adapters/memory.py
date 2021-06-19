3dd3c82174224e7a60521912771ec26a02736d1b
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-19 20:24:33 +0200

    :recycle: [filestorage] Add in-memory implementation

--- src/cutty/filestorage/adapters/memory.py
+++ src/cutty/filestorage/adapters/memory.py
@@ -4,10 +4,31 @@ from collections.abc import MutableSequence
 
 from cutty.compat.contextlib import contextmanager
 from cutty.filestorage.domain.files import File
+from cutty.filestorage.domain.storage import FileStorage
 from cutty.filestorage.domain.storage import FileStore
 
 
 @contextmanager
 def memoryfilestorage(files: MutableSequence[File]) -> Iterator[FileStore]:
     """Append files to a mutable sequence."""
-    yield files.append
+    with MemoryFileStorage(files) as storage:
+        yield storage.add
+
+
+class MemoryFileStorage(FileStorage):
+    """In-memory file storage for testing."""
+
+    def __init__(self, files: MutableSequence[File]) -> None:
+        """Initialize."""
+        self.files = files
+
+    def add(self, file: File) -> None:
+        """Add the file to the storage."""
+        self.files.append(file)
+
+    def commit(self) -> None:
+        """Commit all stores."""
+
+    def rollback(self) -> None:
+        """Rollback all stores."""
+        self.files.clear()
