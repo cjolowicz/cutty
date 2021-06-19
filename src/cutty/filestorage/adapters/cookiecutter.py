2f3a322087ab4746f358dc6f6fafc645de89cba5
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-19 22:41:58 +0200

    :recycle: [filestorage] Do not depend on TemporaryDiskFileStorage in cookiecutter

--- src/cutty/filestorage/adapters/cookiecutter.py
+++ src/cutty/filestorage/adapters/cookiecutter.py
@@ -4,12 +4,12 @@ import pathlib
 import platform
 import subprocess  # noqa: S404
 import sys
+import tempfile
 from collections.abc import Iterable
 from typing import Optional
 
 from cutty.filestorage.adapters.disk import DiskFileStorage
 from cutty.filestorage.adapters.disk import FileExistsPolicy
-from cutty.filestorage.adapters.disk import TemporaryDiskFileStorage
 from cutty.filestorage.domain.files import File
 
 
@@ -22,10 +22,11 @@ def _runcommand(path: pathlib.Path, *, cwd: pathlib.Path) -> None:
 def _runhook(hooks: dict[str, File], hook: str, *, cwd: pathlib.Path) -> None:
     hookfile = hooks.get(hook)
     if hookfile is not None:
-        with TemporaryDiskFileStorage(
-            onstore=functools.partial(_runcommand, cwd=cwd)
-        ) as storage:
-            storage.add(hookfile)
+        with tempfile.TemporaryDirectory() as root:
+            with DiskFileStorage(
+                pathlib.Path(root), onstore=functools.partial(_runcommand, cwd=cwd)
+            ) as storage:
+                storage.add(hookfile)
 
 
 class CookiecutterFileStorage(DiskFileStorage):
