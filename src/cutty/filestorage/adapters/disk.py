82023c0ae1c3d1bf2bee7ec89f8adf77e9bcc60b
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-19 23:52:01 +0200

    :recycle: [filestorage] Move function _storefile

--- src/cutty/filestorage/adapters/disk.py
+++ src/cutty/filestorage/adapters/disk.py
@@ -18,60 +18,6 @@ class FileExistsPolicy(enum.Enum):
     SKIP = enum.auto()
 
 
-def _storefile(
-    file: File,
-    path: pathlib.Path,
-    *,
-    undo: list[Callable[[], None]],
-    overwrite: bool = False,
-) -> None:
-    """Store the file at the given path on disk."""
-    # OVERWRITING.
-    #
-    # These operations are allowed:
-    #
-    # - Overwrite a regular file with another regular file.
-    # - Overwrite a symbolic link with a regular file.
-    # - Overwrite a symbolic link with another symbolic link.
-    #
-    # These operations raise exceptions:
-    #
-    # - Do not overwrite a directory with a regular file.
-    # - Do not overwrite a directory with a symbolic link.
-    # - Do not overwrite a regular file with a symbolic link.
-    # - Do not overwrite a regular file with a directory.
-    # - Do not overwrite a symbolic link with a directory if the link target is
-    #   not a directory. (Symbolic links in the path are followed if they point
-    #   to directories.)
-
-    if overwrite and path.is_symlink():
-        path.unlink()
-
-    for parent in reversed(path.parents):
-        if not parent.is_dir():
-            parent.mkdir()
-            undo.append(parent.rmdir)
-
-    if isinstance(file, RegularFile):
-        path.write_bytes(file.blob)
-
-        if not overwrite:
-            undo.append(path.unlink)
-
-        if isinstance(file, Executable):
-            path.chmod(path.stat().st_mode | 0o111)
-
-    elif isinstance(file, SymbolicLink):
-        target = pathlib.Path(*file.target.parts)
-        path.symlink_to(target)
-
-        if not overwrite:
-            undo.append(path.unlink)
-
-    else:
-        raise TypeError(f"cannot store file of type {type(file)}")
-
-
 class DiskFileStorage(FileStorage):
     """Disk-based file storage."""
 
@@ -91,11 +37,65 @@ class DiskFileStorage(FileStorage):
         path = self.root.joinpath(*file.path.parts)
 
         if not path.exists():
-            _storefile(file, path, undo=self.undo)
+            self._storefile(file, path, undo=self.undo)
         elif self.fileexists is FileExistsPolicy.OVERWRITE:
-            _storefile(file, path, undo=self.undo, overwrite=True)
+            self._storefile(file, path, undo=self.undo, overwrite=True)
         elif self.fileexists is FileExistsPolicy.RAISE:
             raise FileExistsError(f"{path} already exists")
+
+    def _storefile(
+        self,
+        file: File,
+        path: pathlib.Path,
+        *,
+        undo: list[Callable[[], None]],
+        overwrite: bool = False,
+    ) -> None:
+        """Store the file at the given path on disk."""
+        # OVERWRITING.
+        #
+        # These operations are allowed:
+        #
+        # - Overwrite a regular file with another regular file.
+        # - Overwrite a symbolic link with a regular file.
+        # - Overwrite a symbolic link with another symbolic link.
+        #
+        # These operations raise exceptions:
+        #
+        # - Do not overwrite a directory with a regular file.
+        # - Do not overwrite a directory with a symbolic link.
+        # - Do not overwrite a regular file with a symbolic link.
+        # - Do not overwrite a regular file with a directory.
+        # - Do not overwrite a symbolic link with a directory if the link target is
+        #   not a directory. (Symbolic links in the path are followed if they point
+        #   to directories.)
+
+        if overwrite and path.is_symlink():
+            path.unlink()
+
+        for parent in reversed(path.parents):
+            if not parent.is_dir():
+                parent.mkdir()
+                undo.append(parent.rmdir)
+
+        if isinstance(file, RegularFile):
+            path.write_bytes(file.blob)
+
+            if not overwrite:
+                undo.append(path.unlink)
+
+            if isinstance(file, Executable):
+                path.chmod(path.stat().st_mode | 0o111)
+
+        elif isinstance(file, SymbolicLink):
+            target = pathlib.Path(*file.target.parts)
+            path.symlink_to(target)
+
+            if not overwrite:
+                undo.append(path.unlink)
+
+        else:
+            raise TypeError(f"cannot store file of type {type(file)}")
 
     def rollback(self) -> None:
         """Rollback all stores."""
