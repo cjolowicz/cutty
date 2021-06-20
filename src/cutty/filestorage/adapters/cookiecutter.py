ae18281164ef5b722fdcabed781d7729a8e4c2b6
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-20 13:03:33 +0200

    :sparkles: [filestorage] Add CookiecutterFileStorageWrapper

--- src/cutty/filestorage/adapters/cookiecutter.py
+++ src/cutty/filestorage/adapters/cookiecutter.py
@@ -10,6 +10,8 @@ from typing import Optional
 from cutty.filestorage.adapters.disk import DiskFileStorage
 from cutty.filestorage.adapters.disk import FileExistsPolicy
 from cutty.filestorage.domain.files import File
+from cutty.filestorage.domain.storage import FileStorage
+from cutty.filestorage.domain.storage import FileStorageWrapper
 
 
 def _runcommand(path: pathlib.Path, *, cwd: pathlib.Path) -> None:
@@ -58,3 +60,43 @@ class CookiecutterFileStorage(DiskFileStorage):
         """Commit the stores."""
         if self.project is not None:
             _runhook(self.hooks, "post_gen_project", cwd=self.project)
+
+
+class CookiecutterFileStorageWrapper(FileStorageWrapper[DiskFileStorage]):
+    """Wrap a disk-based file store with Cookiecutter hooks."""
+
+    @classmethod
+    def wrap(
+        cls, storage: DiskFileStorage, *, hookfiles: Iterable[File] = ()
+    ) -> FileStorage:
+        """Wrap the disk storage using the given Cookiecutter hooks."""
+        # TODO: Use disk storage if there are no hooks.
+        return cls(storage, hookfiles=hookfiles)
+
+    def __init__(
+        self, storage: DiskFileStorage, *, hookfiles: Iterable[File] = ()
+    ) -> None:
+        """Initialize."""
+        super().__init__(storage)
+        self.hooks = {hook.path.stem: hook for hook in hookfiles}
+        self.project: Optional[pathlib.Path] = None
+
+    def add(self, file: File) -> None:
+        """Add file to storage."""
+        # FIXME: Hooks break rollback assumptions.
+        if self.project is None:
+            self.project = self.storage.resolve(file.path.parents[-2])
+            self.project.mkdir(
+                parents=True,
+                exist_ok=self.storage.fileexists is not FileExistsPolicy.RAISE,
+            )
+            _runhook(self.hooks, "pre_gen_project", cwd=self.project)
+
+        super().add(file)
+
+    def commit(self) -> None:
+        """Commit the stores."""
+        if self.project is not None:
+            _runhook(self.hooks, "post_gen_project", cwd=self.project)
+
+        super().commit()
