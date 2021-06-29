39ce7eeddca2eddeb61163f028268958861660a5
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-29 23:23:37 +0200

    :sparkles: [filestorage] Add observe function

--- src/cutty/filestorage/domain/storage.py
+++ src/cutty/filestorage/domain/storage.py
@@ -65,6 +65,40 @@ class FileStorageObserver:
         """A storage transaction was aborted."""
 
 
+class _ObservableFileStorage(FileStorage):
+    """Storage wrapper with an observer."""
+
+    def __init__(self, storage: FileStorage, observer: FileStorageObserver) -> None:
+        """Initialize."""
+        self.storage = storage
+        self.observer = observer
+
+    def begin(self) -> None:
+        """Begin a storage transaction."""
+        self.storage.begin()
+        self.observer.begin()
+
+    def add(self, file: File) -> None:
+        """Add the file to the storage."""
+        self.storage.add(file)
+        self.observer.add(file)
+
+    def commit(self) -> None:
+        """Commit all stores."""
+        self.storage.commit()
+        self.observer.commit()
+
+    def rollback(self) -> None:
+        """Rollback all stores."""
+        self.storage.rollback()
+        self.observer.rollback()
+
+
+def observe(storage: FileStorage, observer: FileStorageObserver) -> FileStorage:
+    """Wrap a storage with an observer."""
+    return _ObservableFileStorage(storage, observer)
+
+
 class ObservableFileStorage(FileStorage):
     """Storage wrapper with observers."""
 
