626f0611ac8733a31b8d3e6c5b6f55f3756d0f5b
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-29 23:38:23 +0200

    :recycle: [filestorage] Move class _ObservableFileStorage into observe function

--- src/cutty/filestorage/domain/observers.py
+++ src/cutty/filestorage/domain/observers.py
@@ -19,35 +19,24 @@ class FileStorageObserver:
         """A storage transaction was aborted."""
 
 
-class _ObservableFileStorage(FileStorage):
-    """Storage wrapper with an observer."""
-
-    def __init__(self, storage: FileStorage, observer: FileStorageObserver) -> None:
-        """Initialize."""
-        self.storage = storage
-        self.observer = observer
-
-    def begin(self) -> None:
-        """Begin a storage transaction."""
-        self.storage.begin()
-        self.observer.begin()
-
-    def add(self, file: File) -> None:
-        """Add the file to the storage."""
-        self.storage.add(file)
-        self.observer.add(file)
-
-    def commit(self) -> None:
-        """Commit all stores."""
-        self.storage.commit()
-        self.observer.commit()
-
-    def rollback(self) -> None:
-        """Rollback all stores."""
-        self.storage.rollback()
-        self.observer.rollback()
-
-
 def observe(storage: FileStorage, observer: FileStorageObserver) -> FileStorage:
     """Wrap a storage with an observer."""
-    return _ObservableFileStorage(storage, observer)
+
+    class _ObservableFileStorage(FileStorage):
+        def begin(self) -> None:
+            storage.begin()
+            observer.begin()
+
+        def add(self, file: File) -> None:
+            storage.add(file)
+            observer.add(file)
+
+        def commit(self) -> None:
+            storage.commit()
+            observer.commit()
+
+        def rollback(self) -> None:
+            storage.rollback()
+            observer.rollback()
+
+    return _ObservableFileStorage()
