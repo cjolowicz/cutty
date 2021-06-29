56b85c77d6185afd75d294c67e5be5ad544fcb24
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-29 17:41:23 +0200

    :bug: [filestorage] Create repository after hook execution

--- src/cutty/filestorage/adapters/git.py
+++ src/cutty/filestorage/adapters/git.py
@@ -2,11 +2,9 @@
 import contextlib
 import os
 import pathlib
-from typing import Optional
 
 import pygit2
 
-from cutty.filestorage.domain.files import File
 from cutty.filestorage.domain.storage import FileStorageObserver
 
 
@@ -26,22 +24,12 @@ class GitRepositoryObserver(FileStorageObserver):
     def __init__(self, *, project: pathlib.Path) -> None:
         """Initialize."""
         self.project = project
-        self.repository: Optional[pygit2.Repository] = None
-
-    def begin(self) -> None:
-        """A storage transaction was started."""
-        self.repository = pygit2.init_repository(self.project)
-
-    def add(self, file: File) -> None:
-        """A file was added to the transaction."""
-        path = "/".join(file.path.parts[1:])
-        assert self.repository is not None  # noqa: S101
-        self.repository.index.add(path)
 
     def commit(self) -> None:
         """A storage transaction was completed."""
-        assert self.repository is not None  # noqa: S101
-        tree = self.repository.index.write_tree()
-        self.repository.index.write()
-        signature = default_signature(self.repository)
-        self.repository.create_commit("HEAD", signature, signature, "Initial", tree, [])
+        repository = pygit2.init_repository(self.project)
+        repository.index.add_all()
+        tree = repository.index.write_tree()
+        repository.index.write()
+        signature = default_signature(repository)
+        repository.create_commit("HEAD", signature, signature, "Initial", tree, [])
