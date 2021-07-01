6a9f8a44041ec99c5f42f32103250a017f65974d
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-01 18:29:45 +0200

    :sparkles: utils: Add LazySequence

---
+++ tests/unit/util/test_lazysequence.py
@@ -0,0 +1,31 @@
+"""Unit tests for cutty.util.lazysequence."""
+import pytest
+
+from cutty.util.lazysequence import LazySequence
+
+
+def test_init() -> None:
+    """It is created from an iterable."""
+    LazySequence([])
+
+
+def test_len() -> None:
+    """It returns the number of items."""
+    assert 0 == len(LazySequence([]))
+
+
+def test_getitem() -> None:
+    """It returns the item at the given position."""
+    assert 1 == LazySequence([1])[0]
+
+
+def test_getslice() -> None:
+    """It returns the items at the given positions."""
+    [item] = LazySequence([1, 2])[1:]
+    assert 2 == item
+
+
+def test_outofrange() -> None:
+    """It raises IndexError."""
+    with pytest.raises(IndexError):
+        LazySequence([])[0]
