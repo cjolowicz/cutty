c4279736c1fd27a55fefa864b5476d4840cea5e2
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-01 17:57:59 +0200

    :recycle: [templates] Move function for locating hooks from services.create (#257)
    * :recycle: iterhooks: Copy function from services.create
    
    * :recycle: iterhooks: Rename function to `findhooks`
    
    * :recycle: services.create.iterhooks: Replace body with call to `findhooks`
    
    * :recycle: services.create.iterhooks: Inline function
    
    * :recycle: Move `findhooks` tests to `tests.unit.templates`

--- tests/unit/services/test_create.py
+++ tests/unit/services/test_create.py
@@ -1,7 +1,6 @@
 """Unit tests for cutty.services.create."""
 from cutty.filesystems.adapters.dict import DictFilesystem
 from cutty.filesystems.domain.path import Path
-from cutty.services.create import iterhooks
 from cutty.services.create import iterpaths
 from cutty.templates.domain.config import Config
 
@@ -30,42 +29,4 @@ def test_iterpaths_other_directories() -> None:
     paths = iterpaths(path, config)
 
     assert next(paths) == path / "{{ cookiecutter.project }}"
-    assert next(paths, None) is None
-
-
-def test_iterhooks_none() -> None:
-    """It does not yield if there is no hook directory."""
-    filesystem = DictFilesystem({"{{ cookiecutter.project }}": {}})
-    path = Path(filesystem=filesystem)
-    paths = iterhooks(path)
-
-    assert next(paths, None) is None
-
-
-def test_iterhooks_paths() -> None:
-    """It returns paths to hooks."""
-    filesystem = DictFilesystem(
-        {
-            "hooks": {"pre_gen_project.py": ""},
-            "{{ cookiecutter.project }}": {},
-        }
-    )
-    path = Path(filesystem=filesystem)
-    paths = iterhooks(path)
-
-    assert next(paths) == path / "hooks" / "pre_gen_project.py"
-    assert next(paths, None) is None
-
-
-def test_iterhooks_bogus_hooks() -> None:
-    """It ignores hooks with a backup extension."""
-    filesystem = DictFilesystem(
-        {
-            "hooks": {"pre_gen_project.py~": ""},
-            "{{ cookiecutter.project }}": {},
-        }
-    )
-    path = Path(filesystem=filesystem)
-    paths = iterhooks(path)
-
     assert next(paths, None) is None
--- tests/unit/templates/domain/test_config.py
+++ tests/unit/templates/domain/test_config.py
@@ -1,4 +1,7 @@
 """Unit tests for cutty.templates.domain.config."""
+from cutty.filesystems.adapters.dict import DictFilesystem
+from cutty.filesystems.domain.path import Path
+from cutty.templates.adapters.cookiecutter.config import findhooks
 from cutty.templates.domain.config import Config
 from cutty.templates.domain.variables import Variable
 
@@ -6,3 +9,41 @@ from cutty.templates.domain.variables import Variable
 def test_config(variable: Variable) -> None:
     """It can store settings and variables."""
     Config(settings={"a-setting": "value"}, variables=(variable,))
+
+
+def test_findhooks_none() -> None:
+    """It does not yield if there is no hook directory."""
+    filesystem = DictFilesystem({"{{ cookiecutter.project }}": {}})
+    path = Path(filesystem=filesystem)
+    paths = findhooks(path)
+
+    assert next(paths, None) is None
+
+
+def test_findhooks_paths() -> None:
+    """It returns paths to hooks."""
+    filesystem = DictFilesystem(
+        {
+            "hooks": {"pre_gen_project.py": ""},
+            "{{ cookiecutter.project }}": {},
+        }
+    )
+    path = Path(filesystem=filesystem)
+    paths = findhooks(path)
+
+    assert next(paths) == path / "hooks" / "pre_gen_project.py"
+    assert next(paths, None) is None
+
+
+def test_findhooks_bogus_hooks() -> None:
+    """It ignores hooks with a backup extension."""
+    filesystem = DictFilesystem(
+        {
+            "hooks": {"pre_gen_project.py~": ""},
+            "{{ cookiecutter.project }}": {},
+        }
+    )
+    path = Path(filesystem=filesystem)
+    paths = findhooks(path)
+
+    assert next(paths, None) is None
