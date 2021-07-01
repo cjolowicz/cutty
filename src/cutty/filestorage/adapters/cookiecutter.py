d6234499c3a847481280f7f1911bd91c2cad78ce
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-01 16:41:24 +0200

    :recycle: createcookiecutterstorage: Simplify conditional logic

--- src/cutty/filestorage/adapters/cookiecutter.py
+++ src/cutty/filestorage/adapters/cookiecutter.py
@@ -1,14 +1,12 @@
 """File storage for Cookiecutter projects."""
 import pathlib
 from collections.abc import Sequence
-from typing import Optional
 
 from cutty.filestorage.adapters.disk import DiskFileStorage
 from cutty.filestorage.adapters.disk import FileExistsPolicy
 from cutty.filestorage.adapters.observers.cookiecutter import CookiecutterHooksObserver
 from cutty.filestorage.adapters.observers.git import GitRepositoryObserver
 from cutty.filestorage.domain.files import File
-from cutty.filestorage.domain.observers import FileStorageObserver
 from cutty.filestorage.domain.observers import observe
 from cutty.filestorage.domain.storage import FileStorage
 from cutty.filesystems.domain.path import Path
@@ -38,14 +36,10 @@ def createcookiecutterstorage(
     fileexists = fileexistspolicy(overwrite_if_exists, skip_if_file_exists)
     storage: FileStorage = DiskFileStorage(project_dir.parent, fileexists=fileexists)
 
-    observer: Optional[FileStorageObserver] = None
-
     if hookfiles:  # pragma: no branch
         observer = CookiecutterHooksObserver(
             hookfiles=hookfiles, project=project_dir, fileexists=fileexists
         )
-
-    if observer:  # pragma: no branch
         storage = observe(storage, observer)
 
     storage = observe(storage, GitRepositoryObserver(project=project_dir))
