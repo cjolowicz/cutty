b28e4c5d90d0642159bc91dfd7e20c83a90a0773
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-01 10:36:53 +0200

    :tada: [cli] Add stub update command and functional test (#250)
    * :white_check_mark: [functional] Add functional test for `cutty update --help`
    
    * :tada: [cli] Add update command
    
    * :white_check_mark: [cli] Add functional test for `cutty update`
    
    * :white_check_mark: [functional] Mark functional test for update as XFAIL

--- src/cutty/entrypoints/cli/__init__.py
+++ src/cutty/entrypoints/cli/__init__.py
@@ -1,3 +1,4 @@
 """Command-line interface."""
 from . import create  # noqa: F401
+from . import update  # noqa: F401
 from ._main import main as main  # noqa: F401
---
+++ tests/functional/test_update.py
@@ -0,0 +1,45 @@
+"""Functional tests for the update CLI."""
+from pathlib import Path
+
+import pygit2
+import pytest
+from click.testing import CliRunner
+
+from cutty.entrypoints.cli import main
+
+
+def test_help(runner: CliRunner) -> None:
+    """It exits with a status code of zero."""
+    result = runner.invoke(main, ["update", "--help"])
+    assert result.exit_code == 0
+
+
+@pytest.mark.xfail(reason="not implemented")
+def test_update(runner: CliRunner, repository: Path) -> None:
+    """It applies changes from the template."""
+    runner.invoke(
+        main, ["create", "--no-input", str(repository)], catch_exceptions=False
+    )
+
+    # Update README.md.
+    path = repository / "{{ cookiecutter.project }}" / "README.md"
+    path.write_text(path.read_text() + "An awesome project.\n")
+
+    # Commit the changes.
+    repo = pygit2.Repository(repository)
+    signature = pygit2.Signature("you", "you@example.com")
+    repo.index.add("{{ cookiecutter.project }}/README.md")
+    tree = repo.index.write_tree()
+    repo.create_commit(
+        "HEAD",
+        signature,
+        signature,
+        "Update README.md",
+        tree,
+        [repo.head.target],
+    )
+
+    runner.invoke(main, ["update"], catch_exceptions=False)
+    assert (
+        Path("example", "README.md").read_text() == "# example\nAn awesome project.\n"
+    )
