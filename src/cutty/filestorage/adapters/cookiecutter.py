93390d8700976577d485fafbdf54c44bf7f9e518
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-01 16:27:26 +0200

    :recycle: Add function `createcookiecutterstorage` delegating to `createstorage`

--- src/cutty/filestorage/adapters/cookiecutter.py
+++ src/cutty/filestorage/adapters/cookiecutter.py
@@ -1,1 +1,22 @@
 """File storage for Cookiecutter projects."""
+import pathlib
+from collections.abc import Sequence
+
+from cutty.filestorage.domain.files import File
+from cutty.filestorage.domain.storage import FileStorage
+from cutty.filesystems.domain.path import Path
+
+
+def createcookiecutterstorage(
+    template_dir: Path,
+    project_dir: pathlib.Path,
+    overwrite_if_exists: bool,
+    skip_if_file_exists: bool,
+    hookfiles: Sequence[File],
+) -> FileStorage:
+    """Create storage for Cookiecutter project files."""
+    from cutty.services.create import createstorage
+
+    return createstorage(
+        template_dir, project_dir, overwrite_if_exists, skip_if_file_exists, hookfiles
+    )
