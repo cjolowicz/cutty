4713b2cd981f2026e5b840e05f0d2c3efff6768f
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-15 23:16:38 +0200

    :sparkles: [services] Add function for creating worktrees

--- src/cutty/services/update.py
+++ src/cutty/services/update.py
@@ -1,6 +1,11 @@
 """Update a project with changes from its Cookiecutter template."""
+import contextlib
 import json
+import tempfile
+from collections.abc import Iterator
 from pathlib import Path
+
+import pygit2
 
 from cutty.services.create import create
 
@@ -22,6 +27,20 @@ def getprojectcontext(projectdir: Path) -> dict[str, str]:
         for key, value in data.items()
         if isinstance(key, str) and isinstance(value, str)
     }
+
+
+@contextlib.contextmanager
+def createworktree(repositorypath: Path, branch: str) -> Iterator[Path]:
+    """Create a worktree for the branch in the repository."""
+    repository = pygit2.Repository(repositorypath)
+
+    with tempfile.TemporaryDirectory() as directory:
+        path = Path(directory) / branch
+        worktree = repository.add_worktree(branch, path, repository.branches[branch])
+        yield path
+
+    # Prune with `force=True` because libgit2 thinks `worktree.path` still exists.
+    worktree.prune(True)
 
 
 def update() -> None:
