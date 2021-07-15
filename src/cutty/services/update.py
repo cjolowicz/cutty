2d73a3f98dc571ee6d249af7307dee853a3a4033
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-15 13:49:10 +0200

    :sparkles: [services] Determine template location in update service

--- src/cutty/services/update.py
+++ src/cutty/services/update.py
@@ -1,4 +1,13 @@
 """Update a project with changes from its Cookiecutter template."""
+import json
+from pathlib import Path
+
+
+def getprojecttemplate(projectdir: Path) -> str:
+    """Return the location of the project template."""
+    text = (projectdir / ".cookiecutter.json").read_text()
+    data = json.loads(text)
+    return data["_template"]
 
 
 def update() -> None:
