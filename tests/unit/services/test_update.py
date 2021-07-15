80a1b423453f762424a16d291a8b9dce991c51a7
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-15 17:05:16 +0200

    :white_check_mark: [functional] Update docstring

--- tests/unit/services/test_update.py
+++ tests/unit/services/test_update.py
@@ -6,7 +6,7 @@ from cutty.services.update import getprojecttemplate
 
 
 def test_getprojecttemplate(tmp_path: Path) -> None:
-    """It does not raise."""
+    """It returns the `_template` key from .cookiecutter.json."""
     template = "https://example.com/repository.git"
     text = json.dumps({"_template": template})
     (tmp_path / ".cookiecutter.json").write_text(text)
