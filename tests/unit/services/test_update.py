3f23f7cbf3539eb0604a71c4411be07549d9a7cf
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-15 13:47:00 +0200

    :white_check_mark: [services] Add failing test for determining the template location

--- tests/unit/services/test_update.py
+++ tests/unit/services/test_update.py
@@ -1,1 +1,20 @@
 """Unit tests for cutty.services.update."""
+import json
+from pathlib import Path
+
+from cutty.services.update import getprojecttemplate
+from cutty.services.update import update
+
+
+def test_update() -> None:
+    """It does not raise."""
+    update()
+
+
+def test_getprojecttemplate(tmp_path: Path) -> None:
+    """It does not raise."""
+    template = "https://example.com/repository.git"
+    text = json.dumps({"_template": template})
+    (tmp_path / ".cookiecutter.json").write_text(text)
+
+    assert template == getprojecttemplate(tmp_path)
