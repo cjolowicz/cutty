6e0915749c8b9be81c5698764585fed79e0f1bdd
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-15 18:23:21 +0200

    :white_check_mark: [functional] Add functional test for update requiring merge

--- tests/functional/test_update.py
+++ tests/functional/test_update.py
@@ -15,7 +15,7 @@ def test_help(runner: CliRunner) -> None:
     assert result.exit_code == 0
 
 
-def test_update(runner: CliRunner, repository: Path) -> None:
+def test_update_trivial(runner: CliRunner, repository: Path) -> None:
     """It applies changes from the template."""
     runner.invoke(
         main,
@@ -34,3 +34,31 @@ def test_update(runner: CliRunner, repository: Path) -> None:
 
     runner.invoke(main, ["update"], catch_exceptions=False)
     assert Path("README.md").read_text() == "# awesome\nAn awesome project.\n"
+
+
+def test_update_merge(runner: CliRunner, repository: Path) -> None:
+    """It merges changes from the template."""
+    runner.invoke(
+        main,
+        ["create", "--no-input", str(repository), "project=awesome"],
+        catch_exceptions=False,
+    )
+
+    projectdir = Path("awesome")
+
+    # Update README.md in the project.
+    path = projectdir / "README.md"
+    path.write_text(path.read_text() + "An awesome project.\n")
+    commit(pygit2.Repository(projectdir), message="Update README.md")
+
+    # Add LICENSE in the template.
+    path = repository / "{{ cookiecutter.project }}" / "LICENSE"
+    path.touch()
+    commit(pygit2.Repository(repository), message="Add LICENSE")
+
+    # Update the project.
+    os.chdir(projectdir)
+    runner.invoke(main, ["update"], catch_exceptions=False)
+
+    assert Path("README.md").read_text() == "# awesome\nAn awesome project.\n"
+    assert Path("LICENSE").is_file()
