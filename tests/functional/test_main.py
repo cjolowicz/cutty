a2540eee2019ee90c5ef2fd7e086eb8a020ddd70
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-17 22:06:07 +0200

    :sparkles: [entrypoints] Add `cutty --version` option (#302)
    * :white_check_mark: [functional] Add test for `cutty --version`
    
    * :sparkles: [entrypoints] Add `cutty --version`
    
    * :fire: [entrypoints] Remove `cutty create --version`

--- tests/functional/test_main.py
+++ tests/functional/test_main.py
@@ -1,4 +1,6 @@
 """Functional tests for the cutty CLI."""
+from importlib.metadata import version
+
 from click.testing import CliRunner
 
 from cutty.entrypoints.cli import main
@@ -8,3 +10,9 @@ def test_help(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(main, ["--help"])
     assert result.exit_code == 0
+
+
+def test_version(runner: CliRunner) -> None:
+    """It displays the version."""
+    result = runner.invoke(main, ["--version"])
+    assert version("cutty") in result.output
