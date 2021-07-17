2bb8aef9987fc4a53f7eeb899fc282c6cc9deaba
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-17 12:45:43 +0200

    :recycle: [filestorage] Add method to replace the path of `File` objects (#298)
    * :white_check_mark: [filestorage] Add test for replacing paths of regular files
    
    * :white_check_mark: [filestorage] Add test for replacing paths of symlinks
    
    * :white_check_mark: [filestorage] Add test for replacing paths of executables
    
    * :sparkles: [filestorage] Add method to replace the path of a `File`

--- tests/unit/filestorage/domain/test_files.py
+++ tests/unit/filestorage/domain/test_files.py
@@ -1,6 +1,7 @@
 """Unit tests for cutty.filestorage.domain.files."""
 import pytest
 
+from cutty.filestorage.domain.files import Executable
 from cutty.filestorage.domain.files import loadfile
 from cutty.filestorage.domain.files import RegularFile
 from cutty.filestorage.domain.files import SymbolicLink
@@ -49,3 +50,42 @@ def test_load_directory(filesystem: Filesystem) -> None:
     path = Path(filesystem=filesystem)
     with pytest.raises(Exception):
         loadfile(path, follow_symlinks=False)
+
+
+def test_withpath_regularfile(filesystem: Filesystem) -> None:
+    """It replaces the path of a regular file."""
+    path1 = PurePath("dir", "file")
+    path2 = PurePath("file")
+
+    file1 = RegularFile(path1, b"Lorem ipsum dolor")
+    file2 = file1.withpath(path2)
+
+    assert isinstance(file2, RegularFile)
+    assert file2.path == path2
+    assert file2.blob == file1.blob
+
+
+def test_withpath_executable(filesystem: Filesystem) -> None:
+    """It replaces the path of an executable file."""
+    path1 = PurePath("dir", "exec")
+    path2 = PurePath("exec")
+
+    exec1 = Executable(path1, b":")
+    exec2 = exec1.withpath(path2)
+
+    assert isinstance(exec2, Executable)
+    assert exec2.path == path2
+    assert exec2.blob == exec1.blob
+
+
+def test_withpath_symlink(filesystem: Filesystem) -> None:
+    """It replaces the path of a symbolic link."""
+    path1 = PurePath("dir", "link")
+    path2 = PurePath("link")
+
+    link1 = SymbolicLink(path1, PurePath("file"))
+    link2 = link1.withpath(path2)
+
+    assert isinstance(link2, SymbolicLink)
+    assert link2.path == path2
+    assert link2.target == link1.target
