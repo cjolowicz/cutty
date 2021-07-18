4b4020e758729d79158cb87f61d0a2c6b385f765
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-18 22:37:59 +0200

    :sparkles: [entrypoints] Add `cutty cookiecutter <variable>=<value>`

--- src/cutty/entrypoints/cli/cookiecutter.py
+++ src/cutty/entrypoints/cli/cookiecutter.py
@@ -2,17 +2,19 @@
 import click
 
 from cutty.entrypoints.cli._main import main as _main
+from cutty.entrypoints.cli.create import extra_context_callback
 from cutty.services.create import create as service_create
 
 
 @_main.command()
 @click.argument("template")
+@click.argument("extra-context", nargs=-1, callback=extra_context_callback)
 @click.option(
     "--no-input",
     is_flag=True,
     default=False,
     help="Do not prompt for template variables.",
 )
-def cookiecutter(template: str, no_input: bool) -> None:
+def cookiecutter(template: str, extra_context: dict[str, str], no_input: bool) -> None:
     """Generate projects from Cookiecutter templates."""
-    service_create(template, no_input=no_input)
+    service_create(template, extra_context=extra_context, no_input=no_input)
