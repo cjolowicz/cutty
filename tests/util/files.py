f1c4c90ddc4a96e27064c8f5ecda18b6134f66ae
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-20 11:57:13 +0200

    :white_check_mark: [tests] Add utility for listing files recursively

---
+++ tests/util/files.py
@@ -0,0 +1,42 @@
+"""File utilities."""
+from pathlib import Path
+from typing import Iterator
+from typing import Union
+
+
+def _list_files_recursively(path: Path, *, exclude: frozenset[Path]) -> Iterator[Path]:
+    if path in exclude:
+        return
+
+    if path.is_dir():
+        for entry in path.iterdir():
+            yield from _list_files_recursively(entry, exclude=exclude)
+    else:
+        yield path
+
+
+def list_files_recursively(
+    path: Path, *, exclude: frozenset[Path] = frozenset()
+) -> frozenset[Path]:
+    """List files starting at path, descending into subdirectories."""
+    return frozenset(
+        filename.relative_to(path)
+        for filename in _list_files_recursively(path, exclude=exclude)
+    )
+
+
+def template_files(path: Union[Path, str]) -> frozenset[Path]:
+    """List the project files contained in the template repository."""
+    if isinstance(path, str):
+        path = Path(path)
+
+    path = next(entry for entry in path.iterdir() if entry.name.startswith("{{"))
+    return list_files_recursively(path)
+
+
+def project_files(path: Union[Path, str]) -> frozenset[Path]:
+    """List the project files contained in the project repository."""
+    if isinstance(path, str):
+        path = Path(path)
+
+    return list_files_recursively(path, exclude=frozenset({path / ".git"}))
