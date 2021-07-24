b2f91a5eacb6dcaef978a47cf0160bf5f7e82420
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-24 22:49:48 +0200

    :recycle: [templates] Reduce dependency on storage layout in project configuration tests (#335)
    * :recycle: [templates] Use `createprojectconfigfile` in `readprojectconfigfile` tests
    
    * :recycle: [templates] Add fixture `storage`
    
    * :recycle: [templates] Use file storage instead of I/O in `readprojectconfigfile` tests
    
    * :recycle: [templates] Add `projectconfig` fixture
    
    * :recycle: [templates] Replace inline code with `projectconfig` fixture
    
    * :fire: [templates] Remove redundant test for `readprojectconfigfile`
    
    * :fire: [templates] Remove redundant tests for `createprojectconfigfile`
    
    * :bulb: [templates] Improve docstring for `readprojectconfigfile` test
    
    * :recycle: [templates] Rename test for `readprojectconfigfile`
    
    * :recycle: [functional] Do not depend on storage layout in `projectvariable` function

--- tests/unit/templates/adapters/cookiecutter/test_projectconfig.py
+++ tests/unit/templates/adapters/cookiecutter/test_projectconfig.py
@@ -4,6 +4,7 @@ import pathlib
 
 import pytest
 
+from cutty.filestorage.adapters.disk import DiskFileStorage
 from cutty.filesystems.domain.purepath import PurePath
 from cutty.templates.adapters.cookiecutter.projectconfig import createprojectconfigfile
 from cutty.templates.adapters.cookiecutter.projectconfig import PROJECT_CONFIG_FILE
@@ -12,51 +13,29 @@ from cutty.templates.adapters.cookiecutter.projectconfig import readprojectconfi
 from cutty.templates.domain.bindings import Binding
 
 
-def test_createprojectconfigfile_bindings() -> None:
-    """It creates a JSON file with the bindings."""
+@pytest.fixture
+def projectconfig() -> ProjectConfig:
+    """Fixture for a project configuration."""
     template = "https://example.com/repository.git"
-    project = PurePath("example")
     bindings = [Binding("project", "example"), Binding("license", "MIT")]
 
-    config = ProjectConfig(template, bindings)
-    file = createprojectconfigfile(project, config)
-
-    data = json.loads(file.blob.decode())
-    assert all(binding.name in data for binding in bindings)
+    return ProjectConfig(template, bindings)
 
 
-def test_createprojectconfigfile_template() -> None:
-    """It creates a JSON file containing the template location."""
-    template = "https://example.com/repository.git"
-    project = PurePath("example")
-    bindings = [Binding("Project", "example")]
-
-    config = ProjectConfig(template, bindings)
-    file = createprojectconfigfile(project, config)
-
-    assert "_template" in json.loads(file.blob.decode())
+@pytest.fixture
+def storage(tmp_path: pathlib.Path) -> DiskFileStorage:
+    """Fixture for disk file storage."""
+    return DiskFileStorage(tmp_path / "storage")
 
 
-def test_readprojectconfigfile(tmp_path: pathlib.Path) -> None:
-    """It returns the persisted Cookiecutter context."""
-    template = "https://example.com/repository.git"
-    bindings = [Binding("project", "example")]
-    config = ProjectConfig(template, bindings)
-    file = createprojectconfigfile(PurePath(), config)
-    tmp_path.joinpath(*file.path.parts).write_bytes(file.blob)
+def test_roundtrip(storage: DiskFileStorage, projectconfig: ProjectConfig) -> None:
+    """It returns the persisted project configuration."""
+    file = createprojectconfigfile(PurePath(), projectconfig)
 
-    assert config == readprojectconfigfile(tmp_path)
+    with storage:
+        storage.add(file)
 
-
-def test_readprojectconfigfile_template(tmp_path: pathlib.Path) -> None:
-    """It returns the `_template` key from cutty.json."""
-    template = "https://example.com/repository.git"
-    text = json.dumps({"_template": template})
-    (tmp_path / PROJECT_CONFIG_FILE).write_text(text)
-
-    config = readprojectconfigfile(tmp_path)
-
-    assert template == config.template
+    assert projectconfig == readprojectconfigfile(storage.root)
 
 
 def test_readprojectconfigfile_template_typeerror(tmp_path: pathlib.Path) -> None:
