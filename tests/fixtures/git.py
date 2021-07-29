9498636cc8caea526edebed2c932fe693e6f596d
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-29 13:47:51 +0200

    :recycle: [tests] Move git-related fixtures to pytest plugin `tests.fixtures.git` (#361)
    * :tada: [tests] Add package tests.fixtures
    
    * :sparkles: [tests] Add tests.fixtures.git
    
    * :recycle: [tests] Replace fixtures with pytest plugin `tests.fixtures.git`
    
    * :recycle: [tests] Replace fixtures with pytest plugin

---
+++ tests/fixtures/git.py
@@ -0,0 +1,36 @@
+"""Git-related fixtures."""
+import string
+from collections.abc import Iterator
+from pathlib import Path
+
+import pygit2
+import pytest
+
+from tests.util.git import commit
+
+
+@pytest.fixture
+def repositorypath(tmp_path: Path) -> Path:
+    """Fixture for a repository."""
+    repositorypath = tmp_path / "repository"
+    pygit2.init_repository(repositorypath)
+    commit(repositorypath)
+    return repositorypath
+
+
+@pytest.fixture
+def repository(repositorypath: Path) -> pygit2.Repository:
+    """Fixture for a repository."""
+    return pygit2.Repository(repositorypath)
+
+
+@pytest.fixture
+def paths(repositorypath: Path) -> Iterator[Path]:
+    """Return arbitrary paths in the repository."""
+    return (repositorypath / letter for letter in string.ascii_letters)
+
+
+@pytest.fixture
+def path(paths: Iterator[Path]) -> Path:
+    """Return an arbitrary path in the repository."""
+    return next(paths)
