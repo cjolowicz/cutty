6c546a2c90da558b16e4c2a1b0e6c0956379415f
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-29 11:49:18 +0200

    createbranch: copy function

--- src/cutty/util/git.py
+++ src/cutty/util/git.py
@@ -98,3 +98,12 @@ def cherrypick(repositorypath: Path, reference: str, *, message: str) -> None:
 
     commit(repository, message=message)
     repository.state_cleanup()
+
+
+def createbranch(
+    repositorypath: Path, branch: str, *, target: str, force: bool = False
+) -> None:
+    """Create a branch pointing to the given target, another branch."""
+    repository = pygit2.Repository(repositorypath)
+    commit = repository.branches[target].peel()
+    repository.branches.create(branch, commit, force=force)
