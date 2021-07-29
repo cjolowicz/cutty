27fee08fb75d4e0e5213aeb4052616bd2462de91
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-29 13:04:40 +0200

    :recycle: [tests] Replace fixtures with pytest plugin

--- tests/unit/services/test_update.py
+++ tests/unit/services/test_update.py
@@ -1,6 +1,4 @@
 """Unit tests for cutty.services.update."""
-import string
-from collections.abc import Iterator
 from pathlib import Path
 
 import pygit2
@@ -13,37 +11,12 @@ from cutty.services.update import continueupdate
 from cutty.services.update import skipupdate
 from cutty.util.git import cherrypick
 from tests.util.files import chdir
-from tests.util.git import commit
 from tests.util.git import resolveconflicts
 from tests.util.git import Side
 from tests.util.git import updatefile
 
 
-@pytest.fixture
-def repositorypath(tmp_path: Path) -> Path:
-    """Fixture for a repository."""
-    repositorypath = tmp_path / "repository"
-    pygit2.init_repository(repositorypath)
-    commit(repositorypath)
-    return repositorypath
-
-
-@pytest.fixture
-def paths(repositorypath: Path) -> Iterator[Path]:
-    """Return arbitrary paths in the repository."""
-    return (repositorypath / letter for letter in string.ascii_letters)
-
-
-@pytest.fixture
-def path(paths: Iterator[Path]) -> Path:
-    """Return an arbitrary path in the repository."""
-    return next(paths)
-
-
-@pytest.fixture
-def repository(repositorypath: Path) -> pygit2.Repository:
-    """Fixture for a repository."""
-    return pygit2.Repository(repositorypath)
+pytest_plugins = ["tests.fixtures.git"]
 
 
 def createbranch(repository: pygit2.Repository, name: str) -> pygit2.Branch:
