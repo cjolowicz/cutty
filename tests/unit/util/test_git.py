bac8735467322f65b8f21716eb5714b64593d36b
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-30 18:08:15 +0200

    :white_check_mark: [util] Add test for `git.Repository.path`

--- tests/unit/util/test_git.py
+++ tests/unit/util/test_git.py
@@ -20,6 +20,13 @@ def test_discover_fail(tmp_path: Path) -> None:
     assert None is Repository.discover(tmp_path)
 
 
+def test_path(tmp_path: Path) -> None:
+    """It returns the path to the repository."""
+    path = tmp_path / "repository"
+    repository = Repository.init(path)
+    assert path == repository.path
+
+
 def test_commit_on_unborn_branch(tmp_path: Path) -> None:
     """It creates a commit without parents."""
     repository = Repository.init(tmp_path / "repository")
