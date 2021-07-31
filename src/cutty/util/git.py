abdfe746038fcc8cf691682a4a6914c54984ee76
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-31 23:27:05 +0200

    :sparkles: [util] Add `git.Repository.branches2`

--- src/cutty/util/git.py
+++ src/cutty/util/git.py
@@ -128,6 +128,11 @@ class Repository:
         """Return the repository branches."""
         return self._repository.branches
 
+    @property
+    def branches2(self) -> Branches:
+        """Return the repository branches."""
+        return Branches(self._repository.branches)
+
     def checkout(self, reference: pygit2.Reference) -> None:
         """Check out the given reference."""
         self._repository.checkout(reference)
