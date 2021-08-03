2141700a60c2021a1c33233da5a06b5492387c85
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-08-03 23:37:30 +0200

    :recycle: [filestorage] Inline attribute `git.Repository.branches`

--- src/cutty/filestorage/adapters/observers/git.py
+++ src/cutty/filestorage/adapters/observers/git.py
@@ -27,18 +27,16 @@ class GitRepositoryObserver(FileStorageObserver):
         except pygit2.GitError:
             repository = Repository.init(self.project)
 
-        if UPDATE_BRANCH in repository.branches:
+        if UPDATE_BRANCH in repository.heads:
             # HEAD must point to update branch if it exists.
             head = repository.head.name
             if head != UPDATE_BRANCH:
                 raise RuntimeError(f"unexpected HEAD: {head}")
 
         message = (
-            CREATE_MESSAGE
-            if LATEST_BRANCH not in repository.branches
-            else UPDATE_MESSAGE
+            CREATE_MESSAGE if LATEST_BRANCH not in repository.heads else UPDATE_MESSAGE
         )
 
         repository.commit(message=message)
 
-        repository.branches.setdefault(LATEST_BRANCH, repository.head.commit)
+        repository.heads.setdefault(LATEST_BRANCH, repository.head.commit)
