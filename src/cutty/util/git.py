d27983e2ca557056323931baab2d57b237cff8d2
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-08-03 22:41:30 +0200

    :recycle: [util] Add function `git.Repository.resetcherrypick`

--- src/cutty/util/git.py
+++ src/cutty/util/git.py
@@ -268,6 +268,33 @@ class Repository:
             return reference.peel(pygit2.Commit)
         return None
 
+    def resetcherrypick(self) -> None:
+        """Reset only files that were touched by a cherry-pick.
+
+        Emulates `git reset --merge HEAD` by performing a hard reset on the
+        files updated by CHERRY_PICK_HEAD, and resetting the index to HEAD.
+        """
+        self._repository.index.read_tree(self._repository.head.peel().tree)
+        self._repository.index.write()
+
+        commit = self.cherrypickhead
+        if not commit:
+            return
+
+        [parent] = commit.parents
+
+        diff = commit.tree.diff_to_tree(parent.tree)
+        paths = [
+            file.path
+            for delta in diff.deltas
+            for file in (delta.old_file, delta.new_file)
+        ]
+
+        self._repository.checkout(
+            strategy=pygit2.GIT_CHECKOUT_FORCE | pygit2.GIT_CHECKOUT_REMOVE_UNTRACKED,
+            paths=paths,
+        )
+
     def resetmerge(self, parent: str, cherry: str) -> None:
         """Reset only files that were touched by a cherry-pick.
 
