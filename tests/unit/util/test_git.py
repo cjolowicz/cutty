698ef1088d7c55d82bbfec87cad220b4524a0995
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-08-04 11:31:43 +0200

    :recycle: [util] Inline attribute `git.Repository.branches` in tests

--- tests/unit/util/test_git.py
+++ tests/unit/util/test_git.py
@@ -16,106 +16,106 @@ from tests.util.git import updatefiles
 pytest_plugins = ["tests.fixtures.git"]
 
 
-def test_branches_len_empty(tmp_path: Path) -> None:
-    """It returns zero if there are no branches."""
+def test_heads_len_empty(tmp_path: Path) -> None:
+    """It returns zero if there are no heads."""
     repository = Repository.init(tmp_path / "repository")
-    assert not len(repository.branches)
+    assert not len(repository.heads)
 
 
-def test_branches_len_nonzero(repository: Repository) -> None:
-    """It returns the number of branches."""
-    assert 1 == len(repository.branches)
+def test_heads_len_nonzero(repository: Repository) -> None:
+    """It returns the number of heads."""
+    assert 1 == len(repository.heads)
 
 
-def test_branches_bool_empty(tmp_path: Path) -> None:
-    """It returns False if there are no branches."""
+def test_heads_bool_empty(tmp_path: Path) -> None:
+    """It returns False if there are no heads."""
     repository = Repository.init(tmp_path / "repository")
-    assert not repository.branches
+    assert not repository.heads
 
 
-def test_branches_bool_nonzero(repository: Repository) -> None:
-    """It returns True if there are branches."""
-    assert repository.branches
+def test_heads_bool_nonzero(repository: Repository) -> None:
+    """It returns True if there is a head."""
+    assert repository.heads
 
 
-def test_branches_contains_false(repository: Repository) -> None:
-    """It returns False if the branch does not exist."""
-    assert "branch" not in repository.branches
+def test_heads_contains_false(repository: Repository) -> None:
+    """It returns False if the head does not exist."""
+    assert "branch" not in repository.heads
 
 
-def test_branches_contains_true(repository: Repository) -> None:
-    """It returns True if the branch exists."""
-    assert repository.head.name in repository.branches
+def test_heads_contains_true(repository: Repository) -> None:
+    """It returns True if the head exists."""
+    assert repository.head.name in repository.heads
 
 
-def test_branches_iter(repository: Repository) -> None:
+def test_heads_iter(repository: Repository) -> None:
     """It yields the name of each branch."""
     head = repository.head
-    assert [head.name] == list(iter(repository.branches))
+    assert [head.name] == list(iter(repository.heads))
 
 
-def test_branches_getitem_fail(repository: Repository) -> None:
+def test_heads_getitem_fail(repository: Repository) -> None:
     """It raises KeyError."""
     with pytest.raises(KeyError):
-        repository.branches["branch"]
+        repository.heads["branch"]
 
 
-def test_branches_getitem_pass(repository: Repository) -> None:
+def test_heads_getitem_pass(repository: Repository) -> None:
     """It returns the commit at the head of the branch."""
     head = repository.head
-    assert head.commit == repository.branches[head.name]
+    assert head.commit == repository.heads[head.name]
 
 
-def test_branches_setitem_new(repository: Repository) -> None:
+def test_heads_setitem_new(repository: Repository) -> None:
     """It creates a branch pointing to the given commit."""
-    repository.branches["branch"] = repository.head.commit
-    assert repository.head.commit == repository.branches["branch"]
+    repository.heads["branch"] = repository.head.commit
+    assert repository.head.commit == repository.heads["branch"]
 
 
-def test_branches_setitem_existing(repository: Repository) -> None:
+def test_heads_setitem_existing(repository: Repository) -> None:
     """It resets the branch to the given commit."""
-    head, branches = repository.head, repository.branches
-    branches["branch"] = head.commit
+    head, heads = repository.head, repository.heads
+    heads["branch"] = head.commit
     updatefile(repository.path / "file")
-    branches["branch"] = head.commit
-    assert head.commit == branches["branch"]
+    heads["branch"] = head.commit
+    assert head.commit == heads["branch"]
 
 
-def test_branches_delitem_fail(repository: Repository) -> None:
+def test_heads_delitem_fail(repository: Repository) -> None:
     """It raises KeyError."""
     with pytest.raises(KeyError):
-        del repository.branches["branch"]
+        del repository.heads["branch"]
 
 
-def test_branches_delitem_pass(repository: Repository) -> None:
+def test_heads_delitem_pass(repository: Repository) -> None:
     """It removes the branch."""
-    head, branches = repository.head, repository.branches
-    branches["branch"] = head.commit
-    del branches["branch"]
-    assert "branch" not in branches
+    head, heads = repository.head, repository.heads
+    heads["branch"] = head.commit
+    del heads["branch"]
+    assert "branch" not in heads
 
 
-def test_branches_keys(repository: Repository) -> None:
+def test_heads_keys(repository: Repository) -> None:
     """It returns the branch names."""
-    branches = repository.branches
-    [branch] = branches.keys()
+    heads = repository.heads
+    [branch] = heads.keys()
     assert repository.head.name == branch
 
 
-def test_branches_pop_returns_commit(repository: Repository) -> None:
+def test_heads_pop_returns_commit(repository: Repository) -> None:
     """It removes the branch and returns the commit at its head."""
-    branches = repository.branches
-    branches["branch"] = repository.head.commit
-    commit = branches.pop("branch")
+    heads = repository.heads
+    heads["branch"] = repository.head.commit
+    commit = heads.pop("branch")
     assert repository.head.commit == commit
 
 
-def test_branches_pop_removes_branch(repository: Repository) -> None:
+def test_heads_pop_removes_branch(repository: Repository) -> None:
     """It removes the branch and returns the commit at its head."""
-    branches = repository.branches
-    branches["branch"] = repository.head.commit
-    branches.pop("branch")
-    assert "branch" not in branches
+    heads = repository.heads
+    heads["branch"] = repository.head.commit
+    heads.pop("branch")
+    assert "branch" not in heads
 
 
 def test_branch_fail(repository: Repository) -> None:
@@ -124,63 +124,63 @@ def test_branch_fail(repository: Repository) -> None:
         repository.branch("branch")
 
 
-def test_branches_create_new_branch_name(repository: Repository) -> None:
+def test_heads_create_new_branch_name(repository: Repository) -> None:
     """It creates the branch with the given name."""
-    branches = repository.branches
-    branch = branches.create("branch", repository.head.commit)
+    heads = repository.heads
+    branch = heads.create("branch", repository.head.commit)
     assert "branch" == branch.name
 
 
-def test_branches_create_new_branch_commit(repository: Repository) -> None:
+def test_heads_create_new_branch_commit(repository: Repository) -> None:
     """It creates the branch at the given commit."""
-    branches = repository.branches
-    branch = branches.create("branch", repository.head.commit)
+    heads = repository.heads
+    branch = heads.create("branch", repository.head.commit)
     assert repository.head.commit == branch.commit
 
 
-def test_branches_create_new_branch_at_ancestor(repository: Repository) -> None:
+def test_heads_create_new_branch_at_ancestor(repository: Repository) -> None:
     """It creates the branch at an earlier commit."""
     parent = repository.head.commit
     updatefile(repository.path / "a")
-    branch = repository.branches.create("branch", parent)
+    branch = repository.heads.create("branch", parent)
     assert parent == branch.commit
 
 
-def test_branches_create_new_branch_at_another_branch(repository: Repository) -> None:
+def test_heads_create_new_branch_at_another_branch(repository: Repository) -> None:
     """It creates the branch at a commit on another branch."""
-    branches = repository.branches
+    heads = repository.heads
     main = repository.head
-    branch1 = branches.create("branch1")
+    branch1 = heads.create("branch1")
 
     repository.checkout(branch1)
     repository.commit()
 
     repository.checkout(main)
-    branch2 = branches.create("branch2", branch1.commit)
+    branch2 = heads.create("branch2", branch1.commit)
 
     assert branch1.commit == branch2.commit
 
 
-def test_branches_create_existing_branch(repository: Repository) -> None:
+def test_heads_create_existing_branch(repository: Repository) -> None:
     """It raises an exception if the branch already exists."""
-    branches = repository.branches
-    branch = branches.create("branch", repository.head.commit)
+    heads = repository.heads
+    branch = heads.create("branch", repository.head.commit)
     with pytest.raises(pygit2.AlreadyExistsError):
-        branches.create(branch.name, branch.commit)
+        heads.create(branch.name, branch.commit)
 
 
-def test_branches_create_existing_branch_force(repository: Repository) -> None:
+def test_heads_create_existing_branch_force(repository: Repository) -> None:
     """It updates the branch head if the branch already exists."""
-    head, branches = repository.head, repository.branches
-    branch = branches.create("branch", head.commit)
+    head, heads = repository.head, repository.heads
+    branch = heads.create("branch", head.commit)
     updatefile(repository.path / "a")
-    branches.create(branch.name, head.commit, force=True)
+    heads.create(branch.name, head.commit, force=True)
     assert head.commit == branch.commit
 
 
-def test_branches_create_default_commit(repository: Repository) -> None:
+def test_heads_create_default_commit(repository: Repository) -> None:
     """It creates the branch at the commit referenced by HEAD."""
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
     assert branch.commit == repository.head.commit
 
 
@@ -213,15 +213,15 @@ def test_branch_name_set(repository: Repository) -> None:
 
 def test_branch_commit_get(repository: Repository) -> None:
     """It returns the commit at the head of the branch."""
-    branches = repository.branches
+    heads = repository.heads
     branch = repository.head
-    assert branches[branch.name] == branch.commit
+    assert heads[branch.name] == branch.commit
 
 
 def test_branch_commit_set(repository: Repository) -> None:
     """It resets the branch to the given commit."""
-    head, branches = repository.head, repository.branches
-    branches["branch"] = head.commit
+    head, heads = repository.head, repository.heads
+    heads["branch"] = head.commit
     updatefile(repository.path / "a")
     branch = repository.branch("branch")
     branch.commit = head.commit
@@ -311,7 +311,7 @@ def createconflict(
 
 def test_worktree_creates_worktree(repository: Repository) -> None:
     """It creates a worktree."""
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
 
     with repository.worktree(branch) as worktree:
         assert (worktree / ".git").is_file()
@@ -319,7 +319,7 @@ def test_worktree_creates_worktree(repository: Repository) -> None:
 
 def test_worktree_removes_worktree_on_exit(repository: Repository) -> None:
     """It removes the worktree on exit."""
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
 
     with repository.worktree(branch) as worktree:
         pass
@@ -330,7 +330,7 @@ def test_worktree_removes_worktree_on_exit(repository: Repository) -> None:
 def test_worktree_does_checkout(repository: Repository, path: Path) -> None:
     """It checks out a working tree."""
     updatefile(path)
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
 
     with repository.worktree(branch) as worktree:
         assert (worktree / path.name).is_file()
@@ -339,7 +339,7 @@ def test_worktree_does_checkout(repository: Repository, path: Path) -> None:
 def test_worktree_no_checkout(repository: Repository, path: Path) -> None:
     """It creates a worktree without checking out the files."""
     updatefile(path)
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
 
     with repository.worktree(branch, checkout=False) as worktree:
         assert not (worktree / path.name).is_file()
@@ -348,7 +348,7 @@ def test_worktree_no_checkout(repository: Repository, path: Path) -> None:
 def test_cherrypick_adds_file(repository: Repository, path: Path) -> None:
     """It cherry-picks the commit onto the current branch."""
     main = repository.head
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
 
     repository.checkout(branch)
     updatefile(path)
@@ -363,7 +363,7 @@ def test_cherrypick_adds_file(repository: Repository, path: Path) -> None:
 def test_cherrypick_message(repository: Repository, path: Path) -> None:
     """It uses the original commit message."""
     main = repository.head
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
 
     repository.checkout(branch)
     updatefile(path)
@@ -378,7 +378,7 @@ def test_cherrypick_message(repository: Repository, path: Path) -> None:
 def test_cherrypick_author(repository: Repository) -> None:
     """It uses the original commit author."""
     main = repository.head
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
     author = pygit2.Signature("author", "author@example.com")
 
     repository.checkout(branch)
@@ -396,7 +396,7 @@ def test_cherrypick_committer(
 ) -> None:
     """It creates its own committer signature."""
     main = repository.head
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
     committer = pygit2.Signature("committer", "committer@example.com")
 
     repository.checkout(branch)
@@ -416,7 +416,7 @@ def test_cherrypick_committer(
 def test_cherrypick_conflict_edit(repository: Repository, path: Path) -> None:
     """It raises an exception when both sides modified the file."""
     main = repository.head
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
 
     repository.checkout(branch)
     updatefile(path, "a")
@@ -433,7 +433,7 @@ def test_cherrypick_conflict_deletion(repository: Repository, path: Path) -> Non
     updatefile(path, "a")
 
     main = repository.head
-    branch = repository.branches.create("branch")
+    branch = repository.heads.create("branch")
 
     repository.checkout(branch)
     updatefile(path, "b")
@@ -454,7 +454,7 @@ def test_cherrypickhead_progress(repository: Repository, path: Path) -> None:
     """It returns the commit being cherry-picked."""
     createconflict(repository, path, ours="a", theirs="b")
 
-    assert repository.cherrypickhead == repository.branches["update"]
+    assert repository.cherrypickhead == repository.heads["update"]
 
 
 def test_resetcherrypick_restores_files_with_conflicts(
