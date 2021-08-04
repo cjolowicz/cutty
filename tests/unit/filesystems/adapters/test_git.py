827226f4045e121204c77423f48eb27cf82afc1f
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-08-04 13:38:35 +0200

    :recycle: [filesystems] Refactor tests for `GitFilesystem` (#410)
    * :recycle: [filesystems] Do not create repository in `tmp_path` fixture
    
    * :recycle: [filesystems] Add utility class `TreeBuilder` for git tests
    
    * :recycle: [filesystems] Simplify git tests using `git.Repository`

--- tests/unit/filesystems/adapters/test_git.py
+++ tests/unit/filesystems/adapters/test_git.py
@@ -1,4 +1,6 @@
 """Unit tests for cutty.filesystems.adapters.git."""
+from __future__ import annotations
+
 from pathlib import Path
 
 import pygit2
@@ -7,52 +9,81 @@ import pytest
 from cutty.filesystems.adapters.git import GitFilesystem
 from cutty.filesystems.domain.filesystem import Access
 from cutty.filesystems.domain.purepath import PurePath
+from cutty.util.git import Repository
+
+
+class TreeBuilder:
+    """A helper class forming a wrapper around pygit2.TreeBuilder."""
+
+    def __init__(self, repository: Repository) -> None:
+        """Initialize."""
+        self.repository = repository
+        self.builder = self.repository._repository.TreeBuilder()
+        self.children: dict[str, TreeBuilder] = {}
+
+    def _child(self, name: str) -> TreeBuilder:
+        return self.children.setdefault(name, TreeBuilder(self.repository))
+
+    def _descendant(self, path: PurePath) -> TreeBuilder:
+        builder = self
+        for name in path.parts:
+            builder = builder._child(name)
+        return builder
+
+    def file(self, path: PurePath, text: str) -> None:
+        """Create a regular file."""
+        builder = self._descendant(path.parent)
+        builder._blob(path.name, text, pygit2.GIT_FILEMODE_BLOB)
+
+    def exec(self, path: PurePath, text: str) -> None:
+        """Create an executable file."""
+        builder = self._descendant(path.parent)
+        builder._blob(path.name, text, pygit2.GIT_FILEMODE_BLOB_EXECUTABLE)
+
+    def link(self, path: PurePath, target: str) -> None:
+        """Create a symbolic link."""
+        builder = self._descendant(path.parent)
+        builder._blob(path.name, target, pygit2.GIT_FILEMODE_LINK)
+
+    def _tree(self, name: str, builder: TreeBuilder) -> None:
+        oid = builder.write()
+        self.builder.insert(name, oid, pygit2.GIT_FILEMODE_TREE)
+
+    def _blob(self, name: str, text: str, attr: int) -> None:
+        oid = self.repository._repository.create_blob(text.encode())
+        self.builder.insert(name, oid, attr)
+
+    def write(self) -> pygit2.Oid:
+        """Write the tree to the object database."""
+        for name, child in self.children.items():
+            self._tree(name, child)
+
+        return self.builder.write()
 
 
 @pytest.fixture
 def filesystem(tmp_path: Path) -> GitFilesystem:
     """Fixture for a git filesystem."""
+    repository = Repository.init(tmp_path / "repository")
 
-    def _blob(builder: pygit2.TreeBuilder, name: str, text: str, attr: int) -> None:
-        builder.insert(name, repository.create_blob(text.encode()), attr)
+    builder = TreeBuilder(repository)
+    builder.file(PurePath("file"), "lorem ipsum dolor\n")
+    builder.exec(PurePath("dir", "script.py"), "#!/usr/bin/env python\n")
+    builder.link(PurePath("dir", "link"), "../file")
+    builder.file(PurePath("dir", "subdir", ".keep"), "")
+    builder.link(PurePath("sh"), "/bin/sh")
 
-    def _file(builder: pygit2.TreeBuilder, name: str, text: str) -> None:
-        _blob(builder, name, text, pygit2.GIT_FILEMODE_BLOB)
-
-    def _exec(builder: pygit2.TreeBuilder, name: str, text: str) -> None:
-        _blob(builder, name, text, pygit2.GIT_FILEMODE_BLOB_EXECUTABLE)
-
-    def _link(builder: pygit2.TreeBuilder, name: str, target: str) -> None:
-        _blob(builder, name, target, pygit2.GIT_FILEMODE_LINK)
-
-    def _tree(builder: pygit2.TreeBuilder, name: str, tree: pygit2.TreeBuilder) -> None:
-        builder.insert(name, tree.write(), pygit2.GIT_FILEMODE_TREE)
-
-    signature = pygit2.Signature("you", "you@example.com")
-    repository = pygit2.init_repository(tmp_path)
-
-    root = repository.TreeBuilder()
-    root_dir = repository.TreeBuilder()
-    root_dir_subdir = repository.TreeBuilder()
-
-    _file(root, "file", "lorem ipsum dolor\n")
-    _exec(root_dir, "script.py", "#!/usr/bin/env python\n")
-    _link(root_dir, "link", "../file")
-    _file(root_dir_subdir, ".keep", "")
-    _tree(root_dir, "subdir", root_dir_subdir)
-    _tree(root, "dir", root_dir)
-    _link(root, "sh", "/bin/sh")
-
-    repository.create_commit(
+    author = committer = repository.default_signature
+    repository._repository.create_commit(
         "HEAD",
-        signature,
-        signature,
+        author,
+        committer,
         "Initial",
-        root.write(),
+        builder.write(),
         [],
     )
 
-    return GitFilesystem(tmp_path)
+    return GitFilesystem(repository.path)
 
 
 @pytest.mark.parametrize(
