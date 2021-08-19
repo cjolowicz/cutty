e9bb34b7fcd3441ede1bbaed1b80b608c0db6c9f
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2021-08-19 21:53:46 +0200

    :sparkles: [entrypoints] Exit with an error message on `CuttyError`

--- src/cutty/entrypoints/cli/__init__.py
+++ src/cutty/entrypoints/cli/__init__.py
@@ -2,14 +2,15 @@
 from cutty.entrypoints.cli._main import main
 from cutty.entrypoints.cli.cookiecutter import cookiecutter
 from cutty.entrypoints.cli.create import create
+from cutty.entrypoints.cli.errors import fatal
 from cutty.entrypoints.cli.update import update
 
 
 registercommand = main.command()
 
-registercommand(create)
-registercommand(update)
-registercommand(cookiecutter)
+registercommand(fatal(create))
+registercommand(fatal(update))
+registercommand(fatal(cookiecutter))
 
 
 __all__ = ["main"]
