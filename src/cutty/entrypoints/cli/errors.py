9864f6a875aa323db99ad504ce78827566352a79
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-08-26 15:18:23 +0200

    :recycle: [entrypoints] Add handler for UnknownLocationError

--- src/cutty/entrypoints/cli/errors.py
+++ src/cutty/entrypoints/cli/errors.py
@@ -2,7 +2,13 @@
 from typing import NoReturn
 
 from cutty.errors import CuttyError
+from cutty.repositories.domain.providers import UnknownLocationError
 from cutty.util.exceptionhandlers import exceptionhandler
+
+
+@exceptionhandler
+def _unknownlocation(error: UnknownLocationError) -> NoReturn:
+    raise SystemExit(f"fatal: {error}")
 
 
 @exceptionhandler
