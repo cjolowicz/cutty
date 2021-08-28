a43879de0a165f29d2dc25b221f694ac47f71546
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-08-28 19:30:11 +0200

    :recycle: [repositories] Do not return `tmp_path` from `store` fixture

--- tests/fixtures/repositories/domain/stores.py
+++ tests/fixtures/repositories/domain/stores.py
@@ -10,8 +10,10 @@ from cutty.repositories.domain.stores import Store
 @pytest.fixture
 def store(tmp_path: pathlib.Path) -> Store:
     """Fixture for a store."""
+    path = tmp_path / "store"
+    path.mkdir()
 
     def _(url: URL) -> pathlib.Path:
-        return tmp_path
+        return path
 
     return _
