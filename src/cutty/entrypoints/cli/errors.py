6bb0d349bef24418dcad3be17063a4a4755e4b89
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-01 22:04:38 +0200

    :recycle: [entrypoints] Refactor command-line error handlers (#454)
    * :white_check_mark: [entrypoints] Add tests for `fatal` with `UnknownLocationError`
    
    * :fire: [functional] Reduce to single functional test for error handling
    
    * :children_crossing: [entrypoints] Use "error:" as the prefix for all error messages
    
    * :recycle: [entrypoints] Extract function `_die`
    
    * :fire: [entrypoints] Remove error handler for `CuttyError` base class
    
    * :white_check_mark: [entrypoints] Extend test to check for an error message

--- src/cutty/entrypoints/cli/errors.py
+++ src/cutty/entrypoints/cli/errors.py
@@ -2,22 +2,20 @@
 import pathlib
 from typing import NoReturn
 
-from cutty.errors import CuttyError
 from cutty.repositories.domain.registry import UnknownLocationError
 from cutty.util.exceptionhandlers import exceptionhandler
+
+
+def _die(message: str) -> NoReturn:
+    raise SystemExit(f"error: {message}")
 
 
 @exceptionhandler
 def _unknownlocation(error: UnknownLocationError) -> NoReturn:
     if isinstance(error.location, pathlib.Path) and not error.location.exists():
-        raise SystemExit(f"no such file or directory: {error.location}")
+        _die(f"no such file or directory: {error.location}")
 
-    raise SystemExit(f"unknown location {error.location}")
-
-
-@exceptionhandler
-def _fatal(error: CuttyError) -> NoReturn:
-    raise SystemExit(f"fatal: {error}")
+    _die(f"unknown location {error.location}")
 
 
-fatal = _unknownlocation >> _fatal
+fatal = _unknownlocation
--- tests/unit/entrypoints/cli/test_errors.py
+++ tests/unit/entrypoints/cli/test_errors.py
@@ -1,12 +1,23 @@
 """Unit tests for cutty.entrypoints.cli.errors."""
+import pathlib
+
 import pytest
+from yarl import URL
 
 from cutty.entrypoints.cli.errors import fatal
 from cutty.errors import CuttyError
+from cutty.repositories.domain.registry import UnknownLocationError
 
 
-def test_fatal() -> None:
-    """It raises SystemExit with the exception message."""
-    with pytest.raises(SystemExit, match="fatal: Boom"):
+@pytest.mark.parametrize(
+    "error",
+    [
+        UnknownLocationError(URL("invalid://location")),
+        UnknownLocationError(pathlib.Path("/no/such/file/or/directory")),
+    ],
+)
+def test_errors(error: CuttyError) -> None:
+    """It exits with an error message."""
+    with pytest.raises(SystemExit, match="error: "):
         with fatal:
-            raise CuttyError("Boom")
+            raise error
