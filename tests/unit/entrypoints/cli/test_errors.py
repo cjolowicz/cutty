9a62be8289568cefab010b9f32375c5113128cef
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-01 15:23:49 +0200

    :white_check_mark: [entrypoints] Add tests for `fatal` with `UnknownLocationError`

--- tests/unit/entrypoints/cli/test_errors.py
+++ tests/unit/entrypoints/cli/test_errors.py
@@ -1,8 +1,12 @@
 """Unit tests for cutty.entrypoints.cli.errors."""
+import pathlib
+
 import pytest
+from yarl import URL
 
 from cutty.entrypoints.cli.errors import fatal
 from cutty.errors import CuttyError
+from cutty.repositories.domain.registry import UnknownLocationError
 
 
 def test_fatal() -> None:
@@ -10,3 +14,17 @@ def test_fatal() -> None:
     with pytest.raises(SystemExit, match="fatal: Boom"):
         with fatal:
             raise CuttyError("Boom")
+
+
+@pytest.mark.parametrize(
+    "error",
+    [
+        UnknownLocationError(URL("invalid://location")),
+        UnknownLocationError(pathlib.Path("/no/such/file/or/directory")),
+    ],
+)
+def test_unknown_location_invalid_url(error: CuttyError) -> None:
+    """It raises SystemExit instead of the original exception."""
+    with pytest.raises(SystemExit):
+        with fatal:
+            raise error
