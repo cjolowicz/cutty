008e3727c8bc4672c7601aeb3b34bd2af25cd65c
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-01 15:36:28 +0200

    :recycle: [entrypoints] Extract function `_die`

--- src/cutty/entrypoints/cli/errors.py
+++ src/cutty/entrypoints/cli/errors.py
@@ -7,17 +7,21 @@ from cutty.repositories.domain.registry import UnknownLocationError
 from cutty.util.exceptionhandlers import exceptionhandler
 
 
+def _die(message: str) -> NoReturn:
+    raise SystemExit(f"error: {message}")
+
+
 @exceptionhandler
 def _unknownlocation(error: UnknownLocationError) -> NoReturn:
     if isinstance(error.location, pathlib.Path) and not error.location.exists():
-        raise SystemExit(f"error: no such file or directory: {error.location}")
+        _die(f"no such file or directory: {error.location}")
 
-    raise SystemExit(f"error: unknown location {error.location}")
+    _die(f"unknown location {error.location}")
 
 
 @exceptionhandler
 def _fatal(error: CuttyError) -> NoReturn:
-    raise SystemExit(f"error: {error}")
+    _die(str(error))
 
 
 fatal = _unknownlocation >> _fatal
