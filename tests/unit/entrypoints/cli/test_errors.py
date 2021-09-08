57e0901c7c50642bee1acc17187a55ae9a971520
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-08 10:09:07 +0200

    :white_check_mark: [entrypoints] Add test for FileFetcherError

--- tests/unit/entrypoints/cli/test_errors.py
+++ tests/unit/entrypoints/cli/test_errors.py
@@ -6,6 +6,7 @@ from yarl import URL
 
 from cutty.entrypoints.cli.errors import fatal
 from cutty.errors import CuttyError
+from cutty.repositories.adapters.fetchers.file import FileFetcherError
 from cutty.repositories.adapters.fetchers.git import GitFetcherError
 from cutty.repositories.adapters.fetchers.mercurial import HgError
 from cutty.repositories.adapters.fetchers.mercurial import HgNotFoundError
@@ -29,6 +30,9 @@ from cutty.repositories.domain.registry import UnknownLocationError
             None,
         ),
         HgError(("/usr/bin/hg",), "", "", 1, pathlib.Path("/home/user")),
+        FileFetcherError(
+            FileNotFoundError(2, "No such file or directory", "/no/such/file")
+        ),
         UnsupportedRevisionError("v1.0.0"),
         UnknownLocationError(URL("invalid://location")),
         UnknownLocationError(pathlib.Path("/no/such/file/or/directory")),
