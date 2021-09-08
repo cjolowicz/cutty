9fc7230524af5a6a3da38cf69eb77178a16a1d82
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-08 21:25:16 +0200

    :sparkles: [entrypoints] Improve error message when `filefetcher` fails (#462)
    * :white_check_mark: [repositories] Add test for `filefetcher` with inexistent path
    
    * :recycle: [repositories] Wrap `OSError` in `CuttyError` in `filefetcher`
    
    * :recycle: [repositories] Wrap `OSError` in `FileFetcherError`
    
    * :white_check_mark: [entrypoints] Add test for FileFetcherError
    
    * :sparkles: [entrypoints] Improve error message when `filefetcher` fails

--- src/cutty/repositories/adapters/fetchers/file.py
+++ src/cutty/repositories/adapters/fetchers/file.py
@@ -1,17 +1,34 @@
 """Fetch a repository from the filesystem."""
 import pathlib
 import shutil
+from dataclasses import dataclass
+from typing import NoReturn
 from typing import Optional
 
 from yarl import URL
 
+from cutty.errors import CuttyError
 from cutty.repositories.domain.fetchers import fetcher
 from cutty.repositories.domain.locations import aspath
 from cutty.repositories.domain.matchers import scheme
 from cutty.repositories.domain.revisions import Revision
+from cutty.util.exceptionhandlers import exceptionhandler
+
+
+@dataclass
+class FileFetcherError(CuttyError):
+    """The file or directory could not be fetched."""
+
+    error: OSError
+
+
+@exceptionhandler
+def _errorhandler(error: OSError) -> NoReturn:
+    raise FileFetcherError(error)
 
 
 @fetcher(match=scheme("file"))
+@_errorhandler
 def filefetcher(
     url: URL, destination: pathlib.Path, revision: Optional[Revision]
 ) -> None:
