bf629a6bdbe872fa71c6d1e3d5ea72c20a86e484
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-09 10:07:13 +0200

    :recycle: [repositories] Wrap `httpx.HTTPError` in `HTTPFetcherError`

--- src/cutty/repositories/adapters/fetchers/http.py
+++ src/cutty/repositories/adapters/fetchers/http.py
@@ -1,16 +1,33 @@
 """Fetch a repository via HTTP."""
+from dataclasses import dataclass
 from pathlib import Path
+from typing import NoReturn
 from typing import Optional
 
 import httpx
 from yarl import URL
 
+from cutty.errors import CuttyError
 from cutty.repositories.domain.fetchers import fetcher
 from cutty.repositories.domain.matchers import scheme
 from cutty.repositories.domain.revisions import Revision
+from cutty.util.exceptionhandlers import exceptionhandler
+
+
+@dataclass
+class HTTPFetcherError(CuttyError):
+    """The HTTP resource could not be fetched."""
+
+    error: httpx.HTTPError
+
+
+@exceptionhandler
+def _errorhandler(error: httpx.HTTPError) -> NoReturn:
+    raise HTTPFetcherError(error)
 
 
 @fetcher(match=scheme("http", "https"))
+@_errorhandler
 def httpfetcher(url: URL, destination: Path, revision: Optional[Revision]) -> None:
     """Fetch via HTTP."""
     with httpx.stream("GET", str(url)) as response:
