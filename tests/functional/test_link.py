d5565a7a80dd56afe63e42ee23afa716a8939a04
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-12 11:17:52 +0200

    :white_check_mark: [functional] Add test for `cutty link` adding cutty.json

--- tests/functional/test_link.py
+++ tests/functional/test_link.py
@@ -1,7 +1,24 @@
 """Functional tests for `cutty link`."""
+from pathlib import Path
+
+from cutty.util.git import Repository
 from tests.functional.conftest import RunCutty
+from tests.util.files import chdir
 
 
 def test_help(runcutty: RunCutty) -> None:
     """It exits with a status code of zero."""
     runcutty("link", "--help")
+
+
+def test_project_config(runcutty: RunCutty, template: Path) -> None:
+    """It adds a cutty.json to the project."""
+    runcutty("cookiecutter", str(template))
+
+    project = Repository.init(Path("example"))
+    project.commit(message="Initial")
+
+    with chdir(project.path):
+        runcutty("link", str(template))
+
+    assert (project.path / "cutty.json").is_file()
