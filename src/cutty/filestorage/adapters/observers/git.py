c4aaf80bdce4af3320ad7c6c1b718c2232446f3d
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-19 14:55:59 +0200

    ♻ [filestorage] Remove parameter `self`

--- src/cutty/filestorage/adapters/observers/git.py
+++ src/cutty/filestorage/adapters/observers/git.py
@@ -41,16 +41,14 @@ class GitRepositoryObserver(FileStorageObserver):
                 raise RuntimeError(f"unexpected HEAD: {head}")
 
         message = _commitmessage(
-            self, self.template, self.revision, update=LATEST_BRANCH in repository.heads
+            self.template, self.revision, update=LATEST_BRANCH in repository.heads
         )
 
         repository.commit(message=message)
         repository.heads.setdefault(LATEST_BRANCH, repository.head.commit)
 
 
-def _commitmessage(
-    self: GitRepositoryObserver, template: str, revision: Optional[str], update: bool
-) -> str:
+def _commitmessage(template: str, revision: Optional[str], update: bool) -> str:
     if update and revision:
         return f"Update {template} to {revision}"
 
