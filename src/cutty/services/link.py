57e274f1b5b25d14bfa3feb990b622f0b2160a81
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-23 23:35:54 +0200

    ✨ [services] Do not silently ignore empty templates in `cutty link`

--- src/cutty/services/link.py
+++ src/cutty/services/link.py
@@ -6,7 +6,6 @@ from typing import Optional
 
 from cutty.errors import CuttyError
 from cutty.services.create import create
-from cutty.services.create import EmptyTemplateError
 from cutty.services.git import creategitrepository
 from cutty.services.git import LATEST_BRANCH
 from cutty.services.git import UPDATE_BRANCH
@@ -89,19 +88,16 @@ def link(
         update = _create_orphan_branch(project, UPDATE_BRANCH)
 
     with project.worktree(update, checkout=False) as worktree:
-        try:
-            project_dir, template2 = create(
-                template,
-                outputdir=worktree,
-                outputdirisproject=True,
-                extrabindings=extrabindings,
-                no_input=no_input,
-                checkout=checkout,
-                directory=directory,
-            )
-            creategitrepository(project_dir, template2.name, template2.revision)
-        except EmptyTemplateError:  # pragma: no cover
-            pass
+        project_dir, template2 = create(
+            template,
+            outputdir=worktree,
+            outputdirisproject=True,
+            extrabindings=extrabindings,
+            no_input=no_input,
+            checkout=checkout,
+            directory=directory,
+        )
+        creategitrepository(project_dir, template2.name, template2.revision)
 
     if latest is None:
         # Squash the empty initial commit.
