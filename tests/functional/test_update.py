e266cfa943b4593e7a7638f8c014e5865487f652
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-24 14:15:02 +0200

    🔨 [functional] Replace inline code in `update` test with `emptytemplate` fixture

--- tests/functional/test_update.py
+++ tests/functional/test_update.py
@@ -351,18 +351,13 @@ def test_skip(runcutty: RunCutty, templateproject: Path, project: Path) -> None:
     assert (project / "INSTALL").is_file()
 
 
-def test_empty_template(tmp_path: Path, runcutty: RunCutty) -> None:
+def test_empty_template(emptytemplate: Path, runcutty: RunCutty) -> None:
     """It prints an error message."""
-    template = tmp_path / "template"
-    template.mkdir()
+    (emptytemplate / "{{ cookiecutter.project }}" / "marker").touch()
 
-    (template / "cookiecutter.json").write_text('{"project": "project"}')
-    (template / "{{ cookiecutter.project }}").mkdir()
-    (template / "{{ cookiecutter.project }}" / "marker").touch()
+    runcutty("create", str(emptytemplate))
 
-    runcutty("create", str(template))
-
-    (template / "{{ cookiecutter.project }}" / "marker").unlink()
+    (emptytemplate / "{{ cookiecutter.project }}" / "marker").unlink()
 
     with pytest.raises(RunCuttyError):
         runcutty("update", "--cwd=project")
