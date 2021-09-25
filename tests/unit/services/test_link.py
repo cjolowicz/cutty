912ad0c7b33dce4fe05967c0325d03ea29d7d857
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-25 23:00:53 +0200

    ✅ [services] Add test for `linkproject` creating a commit

---
+++ tests/unit/services/test_link.py
@@ -0,0 +1,47 @@
+"""Unit tests for cutty.services.link."""
+from pathlib import Path
+
+import pytest
+
+from cutty.filesystems.adapters.dict import DictFilesystem
+from cutty.filesystems.domain.path import Path as VirtualPath
+from cutty.repositories.domain.repository import Repository as Template
+from cutty.services.link import CreateProject
+from cutty.services.link import linkproject
+from cutty.util.git import Repository
+
+
+pytest_plugins = ["tests.fixtures.git"]
+
+
+@pytest.fixture
+def project(repository: Repository) -> Repository:
+    """Fixture for a project repository."""
+    return repository
+
+
+@pytest.fixture
+def template() -> Template:
+    """Fixture for a `Template` instance."""
+    templatepath = VirtualPath(filesystem=DictFilesystem({}))
+    return Template("template", templatepath, None)
+
+
+@pytest.fixture
+def createproject(template: Template) -> CreateProject:
+    """Fixture for a `createproject` function."""
+
+    def _(project: Path) -> Template:
+        (project / "cutty.json").touch()
+        return template
+
+    return _
+
+
+def test_linkproject_commit(project: Repository, createproject: CreateProject) -> None:
+    """It creates a commit on the current branch."""
+    tip = project.head.commit
+
+    linkproject(project, createproject)
+
+    assert [tip] == project.head.commit.parents
