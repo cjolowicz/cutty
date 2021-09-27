5056a10ffea07b37a71f24f1d76b3b4e07b354ac
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-27 21:07:01 +0200

    🔨 [services] Inline function `creategitrepository` in `test_create`

--- tests/unit/services/test_create.py
+++ tests/unit/services/test_create.py
@@ -1,4 +1,5 @@
 """Unit tests for cutty.services.create."""
+import dataclasses
 import pathlib
 
 import pygit2
@@ -11,7 +12,7 @@ from cutty.filesystems.adapters.dict import DictFilesystem
 from cutty.filesystems.domain.path import Path as VirtualPath
 from cutty.filesystems.domain.purepath import PurePath
 from cutty.repositories.domain.repository import Repository as Template
-from cutty.services.create import creategitrepository
+from cutty.services.create import creategitrepository2
 from cutty.services.create import LATEST_BRANCH
 from cutty.util.git import Repository
 
@@ -59,37 +60,36 @@ def template() -> Template:
     return Template("template", templatepath, None)
 
 
-def test_repository(project2: pathlib.Path) -> None:
+def test_repository(project2: pathlib.Path, template: Template) -> None:
     """It creates a repository."""
-    creategitrepository(project2, "template", None)
+    creategitrepository2(project2, template)
 
     Repository.open(project2)  # does not raise
 
 
-def test_commit(project2: pathlib.Path) -> None:
+def test_commit(project2: pathlib.Path, template: Template) -> None:
     """It creates a commit."""
-    creategitrepository(project2, "template", None)
+    creategitrepository2(project2, template)
 
     repository = Repository.open(project2)
     repository.head.commit  # does not raise
 
 
-def test_commit_message_template(project2: pathlib.Path) -> None:
+def test_commit_message_template(project2: pathlib.Path, template: Template) -> None:
     """It includes the template name in the commit message."""
-    template = "awesome-template"
-    creategitrepository(project2, template, None)
+    creategitrepository2(project2, template)
 
     repository = Repository.open(project2)
-    assert template in repository.head.commit.message
+    assert template.name in repository.head.commit.message
 
 
-def test_commit_message_revision(project2: pathlib.Path) -> None:
+def test_commit_message_revision(project2: pathlib.Path, template: Template) -> None:
     """It includes the revision in the commit message."""
-    revision = "1.0.0"
-    creategitrepository(project2, "template", revision)
+    template = dataclasses.replace(template, revision="1.0.0")
+    creategitrepository2(project2, template)
 
     repository = Repository.open(project2)
-    assert revision in repository.head.commit.message
+    assert template.revision in repository.head.commit.message
 
 
 def tree(repository: Repository) -> pygit2.Tree:
@@ -98,7 +98,10 @@ def tree(repository: Repository) -> pygit2.Tree:
 
 
 def test_existing_repository(
-    storage: FileStorage, file: RegularFile, projectpath: pathlib.Path
+    storage: FileStorage,
+    file: RegularFile,
+    projectpath: pathlib.Path,
+    template: Template,
 ) -> None:
     """It creates the commit in an existing repository."""
     repository = Repository.init(projectpath)
@@ -107,22 +110,22 @@ def test_existing_repository(
     with storage:
         storage.add(file)
 
-    creategitrepository(projectpath, "template", None)
+    creategitrepository2(projectpath, template)
 
     assert file.path.name in tree(repository)
 
 
-def test_branch(project2: pathlib.Path) -> None:
+def test_branch(project2: pathlib.Path, template: Template) -> None:
     """It creates a branch pointing to the initial commit."""
-    creategitrepository(project2, "template", None)
+    creategitrepository2(project2, template)
 
     repository = Repository.open(project2)
     assert repository.head.commit == repository.heads[LATEST_BRANCH]
 
 
-def test_branch_not_checked_out(project2: pathlib.Path) -> None:
+def test_branch_not_checked_out(project2: pathlib.Path, template: Template) -> None:
     """It does not check out the `latest` branch."""
-    creategitrepository(project2, "template", None)
+    creategitrepository2(project2, template)
 
     repository = Repository.open(project2)
     assert repository.head.name != LATEST_BRANCH
