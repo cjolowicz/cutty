115e76118e3a5e2e6651de0b5f6515cc22e168cf
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-30 13:47:45 +0200

    🔨 [projects] Inline function `updateproject2`

--- src/cutty/projects/update.py
+++ src/cutty/projects/update.py
@@ -1,12 +1,10 @@
 """Updating projects with changes from their templates."""
 from pathlib import Path
 
-from cutty.projects.common import CreateProject
 from cutty.projects.common import CreateProject2
 from cutty.projects.common import LATEST_BRANCH
 from cutty.projects.common import UPDATE_BRANCH
 from cutty.projects.common import updatecommitmessage
-from cutty.repositories.domain.repository import Repository as Repository2
 from cutty.services.loadtemplate import Template
 from cutty.util.git import Repository
 
@@ -15,25 +13,13 @@ def updateproject(
     projectdir: Path, createproject: CreateProject2, template: Template
 ) -> None:
     """Update a project by applying changes between the generated trees."""
-
-    def createproject2(projectdir: Path) -> Repository2:
-        createproject(projectdir)
-        return template.repository
-
-    updateproject2(projectdir, createproject2, template)
-
-
-def updateproject2(
-    projectdir: Path, createproject: CreateProject, template: Template
-) -> None:
-    """Update a project by applying changes between the generated trees."""
     project = Repository.open(projectdir)
 
     latestbranch = project.branch(LATEST_BRANCH)
     updatebranch = project.heads.create(UPDATE_BRANCH, latestbranch.commit, force=True)
 
     with project.worktree(updatebranch, checkout=False) as worktree:
-        _ = createproject(worktree)
+        createproject(worktree)
         Repository.open(worktree).commit(
             message=updatecommitmessage(template.repository)
         )
