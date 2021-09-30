a9907caa1d7ebaf5350f6756a79f1abddfd02e7d
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-09-30 09:53:26 +0200

    🔨 [services] Add classes `Template` and `TemplateMetadata`

--- src/cutty/services/loadtemplate.py
+++ src/cutty/services/loadtemplate.py
@@ -1,17 +1,39 @@
 """Loading templates."""
 import pathlib
+from dataclasses import dataclass
 from typing import Optional
 
 import platformdirs
 
+from cutty.filesystems.domain.path import Path
 from cutty.filesystems.domain.purepath import PurePath
 from cutty.repositories.adapters.storage import getdefaultrepositoryprovider
-from cutty.repositories.domain.repository import Repository as Template
+from cutty.repositories.domain.repository import Repository
+from cutty.repositories.domain.revisions import Revision
+
+
+@dataclass
+class TemplateMetadata:
+    """Metadata for a project template."""
+
+    location: str
+    checkout: Optional[str]
+    directory: Optional[pathlib.PurePosixPath]
+    name: str
+    revision: Optional[Revision]
+
+
+@dataclass
+class Template:
+    """Project template."""
+
+    metadata: TemplateMetadata
+    root: Path
 
 
 def loadtemplate(
     template: str, checkout: Optional[str], directory: Optional[pathlib.PurePosixPath]
-) -> Template:
+) -> Repository:
     """Load a template repository."""
     cachedir = pathlib.Path(platformdirs.user_cache_dir("cutty"))
     repositoryprovider = getdefaultrepositoryprovider(cachedir)
