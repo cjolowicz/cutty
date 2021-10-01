2296579c5abf89b6a8958d5eccdbb3f0fa153439
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-01 22:22:57 +0200

    🔨 [services] Inline function `generate` in `cookiecutter`

--- src/cutty/services/cookiecutter.py
+++ src/cutty/services/cookiecutter.py
@@ -3,7 +3,8 @@ import pathlib
 from collections.abc import Sequence
 from typing import Optional
 
-from cutty.projects.generate import generate
+from cutty.projects.generate import fileexistspolicy
+from cutty.projects.generate import generate2
 from cutty.projects.template import Template
 from cutty.templates.domain.bindings import Binding
 
@@ -21,14 +22,14 @@ def createproject(
 ) -> None:
     """Generate projects from Cookiecutter templates."""
     template = Template.load(location, checkout, directory)
+    fileexists = fileexistspolicy(overwrite_if_exists, skip_if_file_exists)
 
-    generate(
+    generate2(
         template,
         outputdir,
         extrabindings=extrabindings,
         no_input=no_input,
-        overwrite_if_exists=overwrite_if_exists,
-        skip_if_file_exists=skip_if_file_exists,
+        fileexists=fileexists,
         outputdirisproject=False,
         createconfigfile=False,
     )
