e87708311b3ca7ffc74a9f730de8f340f42fb4ba
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-01 22:57:47 +0200

    🔨 [services] Extract function `createproject2` with `FileExistsPolicy`

--- src/cutty/services/create.py
+++ src/cutty/services/create.py
@@ -3,6 +3,7 @@ import pathlib
 from collections.abc import Sequence
 from typing import Optional
 
+from cutty.filestorage.adapters.disk import FileExistsPolicy
 from cutty.projects.generate import fileexistspolicy
 from cutty.projects.generate import generate
 from cutty.projects.repository import ProjectRepository
@@ -23,9 +24,33 @@ def createproject(
     in_place: bool,
 ) -> None:
     """Generate projects from Cookiecutter templates."""
+    fileexists = fileexistspolicy(overwrite_if_exists, skip_if_file_exists)
+    createproject2(
+        location,
+        outputdir,
+        extrabindings=extrabindings,
+        no_input=no_input,
+        checkout=checkout,
+        directory=directory,
+        fileexists=fileexists,
+        in_place=in_place,
+    )
+
+
+def createproject2(
+    location: str,
+    outputdir: pathlib.Path,
+    *,
+    extrabindings: Sequence[Binding],
+    no_input: bool,
+    checkout: Optional[str],
+    directory: Optional[pathlib.PurePosixPath],
+    fileexists: FileExistsPolicy,
+    in_place: bool,
+) -> None:
+    """Generate projects from Cookiecutter templates."""
     template = Template.load(location, checkout, directory)
 
-    fileexists = fileexistspolicy(overwrite_if_exists, skip_if_file_exists)
     projectdir = generate(
         template,
         outputdir,
