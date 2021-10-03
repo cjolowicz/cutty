5b28260eac81208dcf980b985b9895f36742c45a
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-03 13:54:42 +0200

    🔥 [projects] Remove function `generate`

--- src/cutty/projects/generate.py
+++ src/cutty/projects/generate.py
@@ -1,40 +1,11 @@
 """Generating projects from templates."""
-import pathlib
 from collections.abc import Sequence
 
-from cutty.filestorage.adapters.disk import FileExistsPolicy
 from cutty.projects.generator import ProjectGenerator
 from cutty.projects.project import Project
-from cutty.projects.store import storeproject
 from cutty.projects.template import Template
 from cutty.templates.adapters.cookiecutter.binders import bindcookiecuttervariables
 from cutty.templates.domain.bindings import Binding
-
-
-def generate(
-    template: Template,
-    outputdir: pathlib.Path,
-    *,
-    extrabindings: Sequence[Binding],
-    no_input: bool,
-    fileexists: FileExistsPolicy,
-    outputdirisproject: bool,
-    createconfigfile: bool,
-) -> pathlib.Path:
-    """Generate a project from a project template."""
-    project = generate2(
-        template,
-        extrabindings=extrabindings,
-        no_input=no_input,
-        createconfigfile=createconfigfile,
-    )
-
-    return storeproject(
-        project,
-        outputdir,
-        outputdirisproject,
-        fileexists,
-    )
 
 
 def generate2(
