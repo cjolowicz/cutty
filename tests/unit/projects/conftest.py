00d7fefe599b326b28fbb7412ca88a81b0076898
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-04 09:22:55 +0200

    💡 [projects] Update stale docstring for `template` fixture

--- tests/unit/projects/conftest.py
+++ tests/unit/projects/conftest.py
@@ -6,6 +6,6 @@ from cutty.projects.template import Template
 
 @pytest.fixture
 def template() -> Template.Metadata:
-    """Fixture for a `Template` instance."""
+    """Fixture for a `Template.Metadata` instance."""
     location = "https://example.com/template"
     return Template.Metadata(location, None, None, "template", None)
