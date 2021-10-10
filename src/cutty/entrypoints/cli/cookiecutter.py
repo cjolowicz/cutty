b782c5d1d379e6ce281874c66ee54d1f5dd5b047
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-10 13:30:09 +0200

    🔨 [services] Rename function `createproject2`

--- src/cutty/entrypoints/cli/cookiecutter.py
+++ src/cutty/entrypoints/cli/cookiecutter.py
@@ -6,7 +6,7 @@ from typing import Optional
 import click
 
 from cutty.filestorage.adapters.disk import FileExistsPolicy
-from cutty.services.cookiecutter import createproject2
+from cutty.services.cookiecutter import createproject
 from cutty.templates.domain.bindings import Binding
 
 
@@ -104,7 +104,7 @@ def cookiecutter(
 
     fileexists = fileexistspolicy(overwrite_if_exists, skip_if_file_exists)
 
-    createproject2(
+    createproject(
         location,
         output_dir,
         extrabindings=extrabindings,
