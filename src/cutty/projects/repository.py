1e98e379e99b0c9b0ee9e2a5a8f25ecdc8f21719
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-15 06:37:28 +0200

    🔨 [projects] Use empty commit message

--- src/cutty/projects/repository.py
+++ src/cutty/projects/repository.py
@@ -55,7 +55,7 @@ class ProjectRepository:
             None,
             author,
             committer,
-            "initial",
+            "",
             repository.TreeBuilder().write(),
             [],
         )
