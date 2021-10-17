e43ff5c022fa0cdf51f25e760ba8480183418d40
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-17 10:42:18 +0200

    🔨 [projects] Derive attribute `ProjectBuilder.path`

--- src/cutty/projects/repository.py
+++ src/cutty/projects/repository.py
@@ -29,7 +29,7 @@ class ProjectBuilder:
     @property
     def path(self) -> Path:
         """Return the project directory."""
-        return self._path
+        return self._worktree.path
 
     def commit2(self, message: str) -> str:
         """Commit the project."""
