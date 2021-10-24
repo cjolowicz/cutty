cjolowicz/cutty

commit db22b4b0c3063707fc408f6ba59d698d2d25ba94

✨ [projects] Do not commit untracked files in `cutty create` (#585)
* 🔨 [projects] Extract function `ProjectRepository.create2`

* 🔨 [projects] Move commit creation to `ProjectRepository.create`

* 🔨 [projects] Inline function `ProjectRepository.create`

* 🔨 [projects] Rename function `ProjectRepository.create2`

* 🔨 [services] Slide statement

* ✨ [services] Use cherry-picking in `cutty create`

* ✅ [functional] Remove XFAIL marker for `cutty create` with existing files

src/cutty/services/create.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 from cutty.filestorage.adapters.disk import FileExistsPolicy
 from cutty.projects.generate import generate
+from cutty.projects.repository import createcommitmessage
 from cutty.projects.repository import ProjectRepository
 from cutty.projects.store import storeproject
 from cutty.projects.template import Template
@@ -26,10 +27,11 @@ def createproject(
     template = Template.load(location, revision, directory)
 
     project = generate(template, extrabindings, interactive=interactive)
-
     projectdir = outputdir if in_place else outputdir / project.name
-    storeproject(
-        project, projectdir, outputdirisproject=in_place, fileexists=fileexists
-    )
+    repository = ProjectRepository.create(projectdir)
 
-    ProjectRepository.create(projectdir, template.metadata)
+    with repository.build(parent=repository.root) as builder:
+        storeproject(project, builder.path, fileexists=fileexists)
+        commit = builder.commit(message=createcommitmessage(template.metadata))
+
+    repository.import_(commit)

tests/functional/test_create.py
@@ -128,7 +128,6 @@ def test_no_branches(runcutty: RunCutty, template: Path) -> None:
     assert branches == list(project.heads)
 
 
-@pytest.mark.xfail(reason="TODO")
 def test_existing_files(runcutty: RunCutty, template: Path) -> None:
     """It does not commit existing files."""
     existing = Path("example", "do-not-commit-this-file")

tests/unit/projects/test_create.py
@@ -8,6 +8,7 @@ from cutty.filestorage.adapters.disk import DiskFileStorage
 from cutty.filestorage.domain.files import RegularFile
 from cutty.filestorage.domain.storage import FileStorage
 from cutty.filesystems.domain.purepath import PurePath
+from cutty.projects.repository import createcommitmessage
 from cutty.projects.repository import ProjectRepository
 from cutty.projects.template import Template
 from cutty.util.git import Repository
@@ -15,7 +16,8 @@ from cutty.util.git import Repository
 
 def creategitrepository(projectdir: pathlib.Path, template: Template.Metadata) -> None:
     """Initialize the git repository for a project."""
-    ProjectRepository.create(projectdir, template)
+    repository = ProjectRepository.create(projectdir)
+    repository.project.commit(message=createcommitmessage(template))
 
 
 @pytest.fixture

src/cutty/projects/repository.py
@@ -1,4 +1,6 @@
 """Project repositories."""
+from __future__ import annotations
+
 from collections.abc import Iterator
 from dataclasses import dataclass
 from pathlib import Path
@@ -45,7 +47,7 @@ class ProjectRepository:
         self.project = Repository.open(path)
 
     @classmethod
-    def create(cls, projectdir: Path, template: Template.Metadata) -> None:
+    def create(cls, projectdir: Path) -> ProjectRepository:
         """Initialize the git repository for a project."""
         try:
             repository = cls(projectdir)
@@ -56,7 +58,7 @@ class ProjectRepository:
         if repository.project._repository.head_is_unborn:
             repository.createroot()
 
-        repository.project.commit(message=createcommitmessage(template))
+        return repository
 
     @property
     def root(self) -> str:

commit 93ee12edc08c2d7d3685c41b8c03af8502f99019

💥 [entrypoints] Remove options `cutty create --skip-if-file-exists` and `--overwrite-if-exists` (#600)
* ✅ [functional] Add test for `cutty create` with untracked project files

* ✅ [functional] Add test for `cutty create` with existing project files

* ✅ [functional] Add test for `cutty create` with conflicting changes

* 💥 [entrypoints] Remove option `cutty create --overwrite-if-exists`

This option was broken in #585 when `cutty create` was switched to use
cherry-picking to introduce changes. As project creation currently always stores
the project in a git repository, it appears more practical to rely on git to
manage existing files.

We might eventually reintroduce this functionality if we ever add pure
disk-based storage for `cutty create`. In that case, we'd likely switch to a
different CLI, such as `--file-exists={error,keep,overwrite}`.

* 💥 [entrypoints] Remove option `cutty create --skip-if-file-exists`

* 🔨 [services] Remove parameter `fileexists` from `createproject`

* 🔨 [services] Rename functions `{create,cookiecutter}.create{project => }`

src/cutty/entrypoints/cli/cookiecutter.py
@@ -6,7 +6,7 @@ from typing import Optional
 import click
 
 from cutty.filestorage.adapters.disk import FileExistsPolicy
-from cutty.services.cookiecutter import createproject
+from cutty.services.cookiecutter import create
 from cutty.templates.domain.bindings import Binding
 
 
@@ -104,7 +104,7 @@ def cookiecutter(
 
     fileexists = fileexistspolicy(overwrite_if_exists, skip_if_file_exists)
 
-    createproject(
+    create(
         location,
         output_dir,
         extrabindings=extrabindings,

src/cutty/entrypoints/cli/create.py
@@ -5,8 +5,7 @@ from typing import Optional
 import click
 
 from cutty.entrypoints.cli.cookiecutter import extra_context_callback
-from cutty.entrypoints.cli.cookiecutter import fileexistspolicy
-from cutty.services.create import createproject
+from cutty.services.create import create as service_create
 from cutty.templates.domain.bindings import Binding
 
 
@@ -40,20 +39,6 @@ from cutty.templates.domain.bindings import Binding
     ),
 )
 @click.option(
-    "-f",
-    "--overwrite-if-exists",
-    is_flag=True,
-    default=False,
-    help="Overwrite the contents of the output directory if it already exists.",
-)
-@click.option(
-    "-s",
-    "--skip-if-file-exists",
-    is_flag=True,
-    default=False,
-    help="Skip the files in the corresponding directories if they already exist.",
-)
-@click.option(
     "-i",
     "--in-place",
     is_flag=True,
@@ -67,8 +52,6 @@ def create(
     revision: Optional[str],
     cwd: Optional[pathlib.Path],
     template_directory: Optional[pathlib.Path],
-    overwrite_if_exists: bool,
-    skip_if_file_exists: bool,
     in_place: bool,
 ) -> None:
     """Generate projects from Cookiecutter templates."""
@@ -77,14 +60,12 @@ def create(
     if cwd is None:
         cwd = pathlib.Path.cwd()
 
-    fileexists = fileexistspolicy(overwrite_if_exists, skip_if_file_exists)
-    createproject(
+    service_create(
         template,
         cwd,
         extrabindings=extrabindings,
         interactive=not non_interactive,
         revision=revision,
         directory=template_directory,
-        fileexists=fileexists,
         in_place=in_place,
     )

src/cutty/services/cookiecutter.py
@@ -10,7 +10,7 @@ from cutty.projects.template import Template
 from cutty.templates.domain.bindings import Binding
 
 
-def createproject(
+def create(
     location: str,
     outputdir: pathlib.Path,
     *,

src/cutty/services/create.py
@@ -3,7 +3,6 @@ import pathlib
 from collections.abc import Sequence
 from typing import Optional
 
-from cutty.filestorage.adapters.disk import FileExistsPolicy
 from cutty.projects.generate import generate
 from cutty.projects.messages import createcommitmessage
 from cutty.projects.repository import ProjectRepository
@@ -12,7 +11,7 @@ from cutty.projects.template import Template
 from cutty.templates.domain.bindings import Binding
 
 
-def createproject(
+def create(
     location: str,
     outputdir: pathlib.Path,
     *,
@@ -20,7 +19,6 @@ def createproject(
     interactive: bool,
     revision: Optional[str],
     directory: Optional[pathlib.Path],
-    fileexists: FileExistsPolicy,
     in_place: bool,
 ) -> None:
     """Generate projects from Cookiecutter templates."""
@@ -31,7 +29,7 @@ def createproject(
     repository = ProjectRepository.create(projectdir, message="Initial commit")
 
     with repository.build() as builder:
-        storeproject(project, builder.path, fileexists=fileexists)
+        storeproject(project, builder.path)
         commit = builder.commit(message=createcommitmessage(template.metadata))
 
     repository.import_(commit)

tests/functional/test_create.py
@@ -150,3 +150,43 @@ def test_untracked_files(runcutty: RunCutty, template: Path) -> None:
     runcutty("create", str(template))
 
     assert untracked.name not in project.head.commit.tree
+
+
+def test_untracked_project_files(runcutty: RunCutty, template: Path) -> None:
+    """It bails out to avoid overwriting uncommitted changes."""
+    project = Repository.init(Path("example"))
+
+    untracked = project.path / "cutty.json"
+    untracked.touch()
+
+    with pytest.raises(Exception, match="uncommitted change"):
+        runcutty("create", str(template))
+
+    assert {untracked.relative_to(project.path)} == project_files(project.path)
+
+
+def test_existing_project_files(runcutty: RunCutty, template: Path) -> None:
+    """It does not overwrite existing files."""
+    project = Path("example")
+    project.mkdir()
+
+    existing = project / "cutty.json"
+    existing.touch()
+
+    with pytest.raises(Exception, match="uncommitted change"):
+        runcutty("create", str(template))
+
+    assert {existing.relative_to(project)} == project_files(project)
+
+
+def test_conflict(runcutty: RunCutty, template: Path) -> None:
+    """It produces conflict markers if files have conflicting changes."""
+    project = Repository.init(Path("example"))
+    conflicting = project.path / "cutty.json"
+
+    updatefile(conflicting, "null")
+
+    with pytest.raises(Exception, match="conflict"):
+        runcutty("create", str(template))
+
+    assert ">>>>" in conflicting.read_text()