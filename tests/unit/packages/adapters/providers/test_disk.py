802a9f24cb8988c1a8bb8b9e246789f551a9d0a4
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-26 06:21:02 +0200

    🔨 [packages] Rename fixture `repository{ => path}` in `test_disk`

--- tests/unit/packages/adapters/providers/test_disk.py
+++ tests/unit/packages/adapters/providers/test_disk.py
@@ -8,17 +8,17 @@ from cutty.packages.domain.locations import asurl
 
 
 @pytest.fixture
-def repository(tmp_path: Path) -> Path:
-    """Fixture for a repository."""
+def repositorypath(tmp_path: Path) -> Path:
+    """Fixture for a package repository."""
     path = tmp_path / "repository"
     path.mkdir()
     (path / "marker").write_text("Lorem")
     return path
 
 
-def test_happy(repository: Path) -> None:
+def test_happy(repositorypath: Path) -> None:
     """It provides a repository from a local directory."""
-    url = asurl(repository)
+    url = asurl(repositorypath)
     package = diskprovider(url)
     assert package is not None
 
@@ -26,8 +26,8 @@ def test_happy(repository: Path) -> None:
     assert text == "Lorem"
 
 
-def test_revision(repository: Path) -> None:
+def test_revision(repositorypath: Path) -> None:
     """It raises an exception when passed a revision."""
-    url = asurl(repository)
+    url = asurl(repositorypath)
     with pytest.raises(Exception):
         diskprovider(url, "v1.0")
