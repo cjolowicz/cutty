bf65413620c6d7f5fe7181cdb5ee7f2367e0e9f5
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-27 06:54:05 +0200

    🔨 [packages] Pass `mount2` in disk provider

--- src/cutty/packages/adapters/providers/disk.py
+++ src/cutty/packages/adapters/providers/disk.py
@@ -1,5 +1,6 @@
 """Package provider for a local directory."""
 from cutty.filesystems.adapters.disk import DiskFilesystem
+from cutty.packages.domain.mounters import asmounter2
 from cutty.packages.domain.mounters import unversioned_mounter
 from cutty.packages.domain.providers import LocalProvider
 
@@ -7,5 +8,5 @@ from cutty.packages.domain.providers import LocalProvider
 diskprovider = LocalProvider(
     "local",
     match=lambda path: path.is_dir(),
-    mount=unversioned_mounter(DiskFilesystem),
+    mount2=asmounter2(unversioned_mounter(DiskFilesystem)),
 )
