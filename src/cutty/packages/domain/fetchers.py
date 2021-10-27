276b9cb7c7b4decf64115732d16ab60222349e6f
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-27 13:56:53 +0200

    🔨 [packages] Remove class `Fetcher2`

--- src/cutty/packages/domain/fetchers.py
+++ src/cutty/packages/domain/fetchers.py
@@ -46,23 +46,6 @@ class AbstractFetcher(abc.ABC):
         """Retrieve the package repository at the URL into local storage."""
 
 
-class Fetcher2(AbstractFetcher):
-    """A fetcher retrieves a package repository from a URL into storage."""
-
-    def __init__(self, fetcher: Fetcher):
-        """Initialize."""
-        self.fetcher = fetcher
-
-    def fetch(
-        self,
-        url: URL,
-        store: Store,
-        mode: FetchMode = FetchMode.ALWAYS,
-    ) -> Optional[pathlib.Path]:
-        """Retrieve the package repository at the URL into local storage."""
-        return self.fetcher(url, store, mode)
-
-
 FetchFunction = Callable[[URL, pathlib.Path], None]
 FetchDecorator = Callable[[FetchFunction], Fetcher]
 FetchDecorator2 = Callable[[FetchFunction], AbstractFetcher]
