f8f512628f556c551f7298e40a6670b48317038e
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-27 13:18:19 +0200

    🔨 [packages] Inline function `filefetcher`

--- src/cutty/packages/adapters/fetchers/file.py
+++ src/cutty/packages/adapters/fetchers/file.py
@@ -7,8 +7,7 @@ from typing import NoReturn
 from yarl import URL
 
 from cutty.errors import CuttyError
-from cutty.packages.domain.fetchers import fetcher
-from cutty.packages.domain.fetchers import Fetcher2
+from cutty.packages.domain.fetchers import fetcher2
 from cutty.packages.domain.locations import aspath
 from cutty.packages.domain.matchers import scheme
 from cutty.util.exceptionhandlers import exceptionhandler
@@ -26,9 +25,9 @@ def _errorhandler(error: OSError) -> NoReturn:
     raise FileFetcherError(error)
 
 
-@fetcher(match=scheme("file"))
+@fetcher2(match=scheme("file"))
 @_errorhandler
-def filefetcher(url: URL, destination: pathlib.Path) -> None:
+def filefetcher2(url: URL, destination: pathlib.Path) -> None:
     """Copy a file or directory."""
     if destination.is_dir():
         shutil.rmtree(destination)
@@ -41,6 +40,3 @@ def filefetcher(url: URL, destination: pathlib.Path) -> None:
         shutil.copytree(source, destination, symlinks=True)
     else:
         shutil.copy2(source, destination, follow_symlinks=False)
-
-
-filefetcher2 = Fetcher2(filefetcher)
