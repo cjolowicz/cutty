5de53534787c759cd9266378e9a44591a9146241
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-27 06:55:30 +0200

    🔨 [packages] Pass `mount2` in hg provider

--- src/cutty/packages/adapters/providers/mercurial.py
+++ src/cutty/packages/adapters/providers/mercurial.py
@@ -6,6 +6,7 @@ from cutty.filesystems.adapters.disk import DiskFilesystem
 from cutty.filesystems.domain.filesystem import Filesystem
 from cutty.packages.adapters.fetchers.mercurial import findhg
 from cutty.packages.adapters.fetchers.mercurial import hgfetcher
+from cutty.packages.domain.mounters import asmounter2
 from cutty.packages.domain.providers import RemoteProviderFactory
 from cutty.packages.domain.revisions import Revision
 
@@ -37,5 +38,5 @@ def mount(path: pathlib.Path, revision: Optional[Revision]) -> Filesystem:
 
 
 hgproviderfactory = RemoteProviderFactory(
-    "hg", fetch=[hgfetcher], getrevision=getrevision, mount=mount
+    "hg", fetch=[hgfetcher], getrevision=getrevision, mount2=asmounter2(mount)
 )
