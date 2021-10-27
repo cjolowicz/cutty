780e5e331c27e270435fe1dc4164f5d5044c3664
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-27 09:07:17 +0200

    🔨 [packages] Inline fixture `jsonmounter`

--- tests/fixtures/packages/domain/mounters.py
+++ tests/fixtures/packages/domain/mounters.py
@@ -1,14 +1,15 @@
 """Fixtures for cutty.packages.domain.mounters."""
 import json
 import pathlib
+from collections.abc import Iterator
 from typing import Optional
 
 import pytest
 
+from cutty.compat.contextlib import contextmanager
 from cutty.filesystems.adapters.dict import DictFilesystem
 from cutty.filesystems.adapters.disk import DiskFilesystem
 from cutty.filesystems.domain.filesystem import Filesystem
-from cutty.packages.domain.mounters import asmounter2
 from cutty.packages.domain.mounters import Mounter
 from cutty.packages.domain.mounters import Mounter2
 from cutty.packages.domain.mounters import unversioned_mounter
@@ -29,18 +30,13 @@ def diskmounter2() -> Mounter2:
 
 
 @pytest.fixture
-def jsonmounter() -> Mounter:
+def jsonmounter2() -> Mounter2:
     """Mount a versioned dict filesystem read from JSON."""
 
-    def _(path: pathlib.Path, revision: Optional[Revision]) -> Filesystem:
+    @contextmanager
+    def _(path: pathlib.Path, revision: Optional[Revision]) -> Iterator[Filesystem]:
         text = path.read_text()
         data = json.loads(text)
-        return DictFilesystem(data[revision] if revision is not None else data)
+        yield DictFilesystem(data[revision] if revision is not None else data)
 
     return _
-
-
-@pytest.fixture
-def jsonmounter2(jsonmounter: Mounter) -> Mounter2:
-    """Mount a versioned dict filesystem read from JSON."""
-    return asmounter2(jsonmounter)
