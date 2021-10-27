0618cf98ef7c1e4f85d2b52863c4727cb02b506f
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-27 13:20:53 +0200

    🔨 [packages] Use `ftpfetcher2` in `test_ftp`

--- tests/unit/packages/adapters/fetchers/test_ftp.py
+++ tests/unit/packages/adapters/fetchers/test_ftp.py
@@ -11,7 +11,7 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 from yarl import URL
 
-from cutty.packages.adapters.fetchers.ftp import ftpfetcher
+from cutty.packages.adapters.fetchers.ftp import ftpfetcher2
 from cutty.packages.domain.stores import Store
 
 
@@ -62,7 +62,7 @@ def server(repository: Path) -> Iterator[URL]:
 
 def test_happy(server: URL, store: Store, repository: Path) -> None:
     """It downloads the file."""
-    path = ftpfetcher(server, store)
+    path = ftpfetcher2.fetch(server, store)
     assert path is not None
     assert path.read_text() == repository.read_text()
 
@@ -70,21 +70,21 @@ def test_happy(server: URL, store: Store, repository: Path) -> None:
 def test_not_matched(store: Store) -> None:
     """It returns None if the URL does not use the ftp scheme."""
     url = URL("file:///")
-    path = ftpfetcher(url, store)
+    path = ftpfetcher2.fetch(url, store)
     assert path is None
 
 
 def test_not_found(server: URL, store: Store) -> None:
     """It raises an exception if the server responds with an error."""
     with pytest.raises(Exception):
-        ftpfetcher(server.with_name("bogus"), store)
+        ftpfetcher2.fetch(server.with_name("bogus"), store)
 
 
 def test_update(server: URL, store: Store, repository: Path) -> None:
     """It updates a file from a previous fetch."""
-    ftpfetcher(server, store)
+    ftpfetcher2.fetch(server, store)
     repository.write_text("ipsum")
-    path = ftpfetcher(server, store)
+    path = ftpfetcher2.fetch(server, store)
 
     assert path is not None
     assert path.read_text() == repository.read_text()
