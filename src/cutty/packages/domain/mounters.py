a5fab7ed6f3f83cda0c2c6898b868d945fb99b4e
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-27 05:57:03 +0200

    🔨 [packages] Add function `asmounter2` to convert to new mounter signature

--- src/cutty/packages/domain/mounters.py
+++ src/cutty/packages/domain/mounters.py
@@ -1,10 +1,12 @@
 """Mounting filesystems."""
 import pathlib
 from collections.abc import Callable
+from collections.abc import Iterator
 from contextlib import AbstractContextManager
 from dataclasses import dataclass
 from typing import Optional
 
+from cutty.compat.contextlib import contextmanager
 from cutty.errors import CuttyError
 from cutty.filesystems.domain.filesystem import Filesystem
 from cutty.packages.domain.revisions import Revision
@@ -32,3 +34,13 @@ def unversioned_mounter(filesystem: Callable[[pathlib.Path], Filesystem]) -> Mou
         return filesystem(storage)
 
     return _mount
+
+
+def asmounter2(mount: Mounter) -> Mounter2:
+    """Convert `Mounter` to `Mounter2`."""
+
+    @contextmanager
+    def _(path: pathlib.Path, revision: Optional[Revision]) -> Iterator[Filesystem]:
+        yield mount(path, revision)
+
+    return _
