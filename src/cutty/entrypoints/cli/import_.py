680f6765d91363285bc16572e4b65e1d772e836e
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-28 13:46:51 +0200

    ✨ [entrypoints] Implement `cutty import` using `services.update`

--- src/cutty/entrypoints/cli/import_.py
+++ src/cutty/entrypoints/cli/import_.py
@@ -1,10 +1,16 @@
 """Command-line interface for importing changesets into projects."""
+from pathlib import Path
+
 import click
 
 from cutty.entrypoints.cli.errors import fatal
+from cutty.services.update import update
 
 
 @click.command("import")
 @fatal
 def import_() -> None:
     """Import changesets from templates into projects."""
+    update(
+        Path.cwd(), extrabindings=(), interactive=True, revision=None, directory=None
+    )
