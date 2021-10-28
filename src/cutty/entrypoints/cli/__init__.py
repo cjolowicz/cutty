c14729983c34f1935a3e5d9fce47b953e8836ba0
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-28 13:07:02 +0200

    ✨ [entrypoints] Add command `cutty import`

--- src/cutty/entrypoints/cli/__init__.py
+++ src/cutty/entrypoints/cli/__init__.py
@@ -3,6 +3,7 @@ from cutty.entrypoints.cli._main import main
 from cutty.entrypoints.cli.cookiecutter import cookiecutter
 from cutty.entrypoints.cli.create import create
 from cutty.entrypoints.cli.errors import fatal
+from cutty.entrypoints.cli.import_ import import_
 from cutty.entrypoints.cli.link import link
 from cutty.entrypoints.cli.update import update
 
@@ -12,5 +13,6 @@ registercommand = main.command()
 for command in [create, update, link, cookiecutter]:
     registercommand(fatal(command))
 
+main.command("import")(import_)
 
 __all__ = ["main"]
---
+++ src/cutty/entrypoints/cli/import_.py
@@ -0,0 +1,5 @@
+"""Command-line interface for importing changesets into projects."""
+
+
+def import_() -> None:
+    """Import changesets from templates into projects."""
