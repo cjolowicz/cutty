3de25c00e5383321d2cb3c4e0fe5bf733bb7e7a0
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-10-29 06:42:52 +0200

    🔨 [services] Inline function `update` in `import_`

--- src/cutty/services/import_.py
+++ src/cutty/services/import_.py
@@ -2,15 +2,40 @@
 from pathlib import Path
 from typing import Optional
 
-from cutty.services.update import update
+from cutty.projects.build import buildproject
+from cutty.projects.config import ProjectConfig
+from cutty.projects.config import readprojectconfigfile
+from cutty.projects.messages import updatecommitmessage
+from cutty.projects.repository import ProjectRepository
 
 
 def import_(projectdir: Path, *, revision: Optional[str]) -> None:
     """Import changes from a template into a project."""
-    update(
-        projectdir,
-        extrabindings=(),
-        interactive=True,
-        revision=revision,
-        directory=None,
+    config1 = readprojectconfigfile(projectdir)
+
+    config2 = ProjectConfig(
+        config1.template,
+        config1.bindings,
+        revision,
+        config1.directory,
     )
+
+    repository = ProjectRepository(projectdir)
+
+    parent = buildproject(
+        repository,
+        config1,
+        interactive=True,
+        commitmessage=updatecommitmessage,
+    )
+
+    commit = buildproject(
+        repository,
+        config2,
+        interactive=True,
+        commitmessage=updatecommitmessage,
+        parent=parent,
+    )
+
+    if commit != parent:
+        repository.import_(commit)
