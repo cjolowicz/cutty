4453beba602b783927a2227115c026ec75961e61
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-11-03 06:13:13 +0100

    🔨 [functional] Replace inline code with function call

--- tests/functional/test_update.py
+++ tests/functional/test_update.py
@@ -18,6 +18,7 @@ from tests.util.git import resolveconflicts
 from tests.util.git import Side
 from tests.util.git import updatefile
 from tests.util.variables import projectvariable
+from tests.util.variables import updatetemplatevariable
 
 
 def test_help(runcutty: RunCutty) -> None:
@@ -40,14 +41,6 @@ def templatevariable(template: Path, name: str) -> Any:
     path = template / "cookiecutter.json"
     data = json.loads(path.read_text())
     return data[name]
-
-
-def updatetemplatevariable(template: Path, name: str, value: Any) -> None:
-    """Add or update a template variable."""
-    path = template / "cookiecutter.json"
-    data = json.loads(path.read_text())
-    data[name] = value
-    updatefile(path, json.dumps(data))
 
 
 @pytest.fixture
