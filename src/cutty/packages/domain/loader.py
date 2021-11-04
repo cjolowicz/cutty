3995071c8d4eb793756a2bcb0fd34b6e5d346b3b
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-11-04 09:02:17 +0100

    🔨 [packages] Add class `MountedPackageRepositoryLoader`

--- src/cutty/packages/domain/loader.py
+++ src/cutty/packages/domain/loader.py
@@ -2,6 +2,8 @@
 import abc
 import pathlib
 
+from cutty.packages.domain.mounters import Mounter
+from cutty.packages.domain.repository import DefaultPackageRepository
 from cutty.packages.domain.repository import PackageRepository
 
 
@@ -11,3 +13,15 @@ class PackageRepositoryLoader(abc.ABC):
     @abc.abstractmethod
     def load(self, name: str, path: pathlib.Path) -> PackageRepository:
         """Load a package repository from disk."""
+
+
+class MountedPackageRepositoryLoader(PackageRepositoryLoader):
+    """Loader for repositories of mounted packages."""
+
+    def __init__(self, mount: Mounter) -> None:
+        """Initialize."""
+        self.mount = mount
+
+    def load(self, name: str, path: pathlib.Path) -> PackageRepository:
+        """Load a package repository from disk."""
+        return DefaultPackageRepository(name, path, mount=self.mount)
