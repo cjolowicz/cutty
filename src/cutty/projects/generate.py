06f7dd3f0a7d40c2597b8b33b1d2b7a75d719fbc
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-11-07 16:18:42 +0100

    🚚 [variables] Move module `prompts` to `adapters`

--- src/cutty/projects/generate.py
+++ src/cutty/projects/generate.py
@@ -5,10 +5,10 @@ from cutty.projects.generator import ProjectGenerator
 from cutty.projects.project import Project
 from cutty.projects.template import Template
 from cutty.templates.domain.bindvariables import bindvariables
+from cutty.variables.adapters.prompts import createprompt
 from cutty.variables.domain.binders import binddefault
 from cutty.variables.domain.binders import Binder
 from cutty.variables.domain.bindings import Binding
-from cutty.variables.prompts import createprompt
 
 
 def generate(
